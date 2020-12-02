87752a31d3285e0b10f5c6103abf46245df2246c
Simon Brookes <2011834+smabuk@users.noreply.github.com>
2020-12-02 06:03:56 +0000

    Day02 Solved

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -9,6 +9,9 @@ namespace AdventOfCode
 			Console.WriteLine("DAY  1");
 			Console.WriteLine($"Part 1: {Day01.Day01_Part1()}");
 			Console.WriteLine($"Part 2: {Day01.Day01_Part2()}");
+			Console.WriteLine("DAY  2");
+			Console.WriteLine($"Part 1: {Day02.Day02_Part1()}");
+			Console.WriteLine($"Part 2: {Day02.Day02_Part2()}");
 		}
 	}
 }
