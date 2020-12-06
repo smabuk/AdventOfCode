992c9c9c3bae808f71bab0c62030a1dc47d5fbea
Simon Brookes <2011834+smabuk@users.noreply.github.com>
2020-12-06 23:37:55 +0000

    2015 Day 03 Solved

--- Solutions/2015/Day03.cs
+++ Solutions/2015/Day03.cs
@@ -8,23 +8,55 @@ using AdventOfCode.Solutions.Helpers;
 
 namespace AdventOfCode.Solutions {
 	/// <summary>
-	/// Day XX: Title
-	/// https://adventofcode.com/2015/day/XX
+	/// Day 03: Perfectly Spherical Houses in a Vacuum
+	/// https://adventofcode.com/2015/day/3
 	/// </summary>
-	public class Solution_2015_xx {
-		private static string Solution1(string[] input) {
-			List<string> inputs = input.ToList();
-			//inputs.Add("");
-			return "Error: No solution yet";
+	public class Solution_2015_03 {
+		private static int Solution1(string[] input) {
+			List<(int x, int y)> houses = new();
+			(int x, int y) santa = (0, 0);
+			houses.Add(santa);
+			foreach (char c in input[0]) {
+				_ = c switch {
+					'^' => santa.y++,
+					'v' => santa.y--,
+					'>' => santa.x++,
+					'<' => santa.x--,
+					_ => throw new NotImplementedException()
+				};
+				houses.Add(santa);
+			}
+			return houses.Distinct().Count();
 		}
 
-		private static string Solution2(string[] input) {
-			List<string> inputs = input.ToList();
-			inputs.Add("");
-			return "Error: No solution yet";
+		private static int Solution2(string[] input) {
+			List<(int x, int y)> houses = new();
+			(int x, int y) santa = (0, 0);
+			(int x, int y) roboSanta = (0, 0);
+			houses.Add(santa);
+			houses.Add(roboSanta);
+			int count = 0;
+			foreach (char c in input[0]) {
+				bool santasTurn = (count++ % 2) == 0;
+				_ = (santasTurn, c) switch {
+					(true, '^') => santa.y++,
+					(true, 'v') => santa.y--,
+					(true, '>') => santa.x++,
+					(true, '<') => santa.x--,
+					(false, '^') => roboSanta.y++,
+					(false, 'v') => roboSanta.y--,
+					(false, '>') => roboSanta.x++,
+					(false, '<') => roboSanta.x--,
+					_ => throw new NotImplementedException()
+				};
+				if (santasTurn) {
+					houses.Add(santa);
+				} else {
+					houses.Add(roboSanta);
+				}
+			}
+			return houses.Distinct().Count();
 		}
-
-
 
 		public static string Part1(string[]? input) {
 			if (input is null) { return "Error: No data provided"; }
@@ -37,5 +69,6 @@ namespace AdventOfCode.Solutions {
 			input = input.StripTrailingBlankLineOrDefault();
 			return Solution2(input).ToString();
 		}
+
 	}
 }
