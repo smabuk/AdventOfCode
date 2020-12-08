de581c1a96717a906baaba7d8c699ae377f9918f
Simon Brookes <2011834+smabuk@users.noreply.github.com>
2020-12-08 08:03:40 +0000

    InputData now reading filename properly

--- Tests/Helpers/InputData.cs
+++ Tests/Helpers/InputData.cs
@@ -19,11 +19,11 @@ namespace AdventOfCode.Tests
 				filename = $"{year}_{day:D2}_{username}.txt";
 			}
 
-			string fullFilename = Path.GetFullPath(Path.Combine(".", $"{year}_{day:D2}.txt"));
+			string fullFilename = Path.GetFullPath(Path.Combine(".", filename));
 
 			// Start: Fix for xUnit test project
 			if (Path.GetFileName(Path.GetDirectoryName(fullFilename))?.StartsWith("net") ?? false) {
-				fullFilename = Path.GetFullPath(Path.Combine("..", "..", "..", "..", "Data", $"{year}_{day:D2}.txt"));
+				fullFilename = Path.GetFullPath(Path.Combine("..", "..", "..", "..", "Data", filename));
 			}
 			// End: Fix for xUnit test project
 
