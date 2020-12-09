11af964c43da4ab5214978589be21a26d24e9217
Simon Brookes <2011834+smabuk@users.noreply.github.com>
2020-12-09 14:11:11 +0000

    2020 Day 09 Solved

--- Solutions/2020/Day09.cs
+++ Solutions/2020/Day09.cs
@@ -28,9 +28,7 @@ namespace AdventOfCode.Solutions.Year2020 {
 				}
 				i++;
 			}
-
-			return 0;
-
+			throw new ArgumentOutOfRangeException();
 		}
 
 		private static bool ValidXmas(long code, IEnumerable<long> values) {
@@ -50,18 +48,29 @@ namespace AdventOfCode.Solutions.Year2020 {
 			int PreambleNo = 25;
 			long[] codes = inputs.Select(x => long.Parse(x)).ToArray();
 
+			int foundPos = 0;
 			int i = 0;
 			long[] skipped = codes.Skip(PreambleNo).ToArray();
 			foreach (long code in skipped) {
 				if (!ValidXmas(code, codes[i..(i + PreambleNo)])) {
-					return code;
+					foundPos = i + PreambleNo;
 				}
 				i++;
 			}
 
-			return 0;
+			return ValidSumHighPlusLow(codes[foundPos], codes);
 		}
 
+		private static long ValidSumHighPlusLow(long code, long[] values) {
+			for (int i = 0; i <= values.Count(); i++) {
+				for (int j = i + 1; j <= values.Count(); j++) {
+					if (values[i..j].Sum() == code) {
+						return values[i..j].LowestValue() + values[i..j].HighestValue();
+					}
+				}
+			}
+			return 0;
+		}
 
 
 
