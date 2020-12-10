e04b2add8aeec78c8f73cd5060aa2282f0da5687
Simon Brookes <2011834+smabuk@users.noreply.github.com>
2020-12-10 18:53:29 +0000

    Added new github user

--- SharedUI/Services/GithubClient/GithubHttpClient.cs
+++ SharedUI/Services/GithubClient/GithubHttpClient.cs
@@ -15,7 +15,7 @@ namespace AdventOfCode.Services {
 		}
 
 		public List<string> KnownUsers => new() { 
-			"smabuk" , "CopperBeardy", "encse", "KevinSjoberg", "Andriamanitra", "Bassel-T" };
+			"smabuk" , "CopperBeardy", "Rollerss", "encse", "KevinSjoberg", "Andriamanitra", "Bassel-T" };
 
 		public string GetSolutionHref(int year, int day, string username) {
 			const string GITHUB = "https://github.com/";
@@ -28,7 +28,9 @@ namespace AdventOfCode.Services {
 				"bassel-t" => $"{GITHUB}Bassel-T/AdventOfCode{year}-CS/blob/main/AdventOfCode{year}/Day{day}.cs",
 				"copperbeardy" => $"{GITHUB}CopperBeardy/AdventOfCode{year}/blob/main/AdventOfCode{year}/AdventOfCode{year}/Days/Day{day}.cs",
 				"encse" => $"{GITHUB}encse/adventofcode/blob/master/{year}/Day{day:D2}/Solution.cs",
+				//"ians-au" => $"{GITHUB}ians-au/AdventOfCode{year}/blob/main/day{day:D2}.cs",
 				"kevinsjoberg" => $"{GITHUB}KevinSjoberg/aoc-{year}/blob/main/{day:D2}/day{day:D2}.cr",
+				"rollerss" => $"{GITHUB}Rollerss/AOC_{year}/blob/master/AOC/Day{day:D2}.cs",
 				"smabuk" => $"{GITHUB}smabuk/AdventOfCode/tree/master/Solutions/{year}/Day{day:D2}.cs",
 				_ => "",
 			};
@@ -45,7 +47,9 @@ namespace AdventOfCode.Services {
 				"bassel-t" => "",
 				"copperbeardy" => $"CopperBeardy/AdventOfCode{year}/main/AdventOfCode{year}/AdventOfCode{year}/DayInputs/Day{day}.txt",
 				"encse" => $"encse/adventofcode/master/{year}/Day{day:D2}/input.in",
+				//"ians-au" => $"",
 				"kevinsjoberg" => $"KevinSjoberg/aoc-{year}/main/{day:D2}/input.txt",
+				"rollerss" => $"Rollerss/AOC_{year}/master/AOC/InputData/AOCDay{day:D2}.txt",
 				"smabuk" => $"smabuk/AdventOfCode/master/Data/{year}_{day:D2}.txt",
 				_ => "",
 			};
