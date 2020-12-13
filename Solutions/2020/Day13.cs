bda7aafa8f95756d1ad0afa5d28c8b0963f9b84c
Simon Brookes <2011834+smabuk@users.noreply.github.com>
2020-12-13 17:58:37 +0000

    2020 Day13 Solved

--- Solutions/2020/Day13.cs
+++ Solutions/2020/Day13.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -15,8 +16,7 @@ namespace AdventOfCode.Solutions.Year2020 {
 	/// https://adventofcode.com/2020/day/13
 	/// </summary>
 	public class Day13 {
-
-		record RecordType(string Name, int Value);
+		record Bus(string BusNo, int Value, int Offset);
 
 		private static long Solution1(string[] input) {
 			int arrivalTime = int.Parse(input[0]);
@@ -39,43 +39,41 @@ namespace AdventOfCode.Solutions.Year2020 {
 				currentTime++;
 			} while (lookingForBus);
 
-
 			int timeToWait = currentTime - arrivalTime;
 			return timeToWait * busiD;
 		}
 
 		private static long Solution2(string[] input) {
-			int[] buses = input[0]
+			Bus[] buses = input[1]
 				.Split(',')
-				.Select(i => i == "x" ? 1 : int.Parse(i))
+				.Select((busNo, i) => new Bus(busNo, 9999,  i))
+				.Where(bus =>bus.BusNo != "x")
+				.Select(bus => bus with { Value = int.Parse(bus.BusNo) })
 				.ToArray();
 
-			int noOfBuses = buses.Length;
-			long step = buses[0];
-			//for (int i = 0; i < buses.Length; i++) {
-			//	int bus = buses[i];
-			//	step *= (buses[i] - i);
-			//}
-			long currentTime = step;
-			while (Enumerable.Range(0, noOfBuses).Any(x => (currentTime + x) % (buses[x]) != 0)) {
-				currentTime += step;
-			}
+			long step = buses[0].Value;
+			long time = step;
+			int iteration = 1;
+			do {
+				time += step;
+				if(Enumerable.Range(0, iteration + 1).All(x => (time + buses[x].Offset) % (buses[x].Value) == 0)) {
+					step *= buses[iteration].Value;
+					iteration++;
+				}
+			} while (iteration < buses.Length);
 
-			return currentTime;
+			return time;
 		}
-
 
 
 		#region Problem initialisation
 		public static string Part1(string[]? input, params object[]? args) {
 			if (input is null) { return "Error: No data provided"; }
-			// int arg1 = GetArgument(args, 1, 25);
 			input = input.StripTrailingBlankLineOrDefault();
 			return Solution1(input).ToString();
 		}
 		public static string Part2(string[]? input, params object[]? args) {
 			if (input is null) { return "Error: No data provided"; }
-			// int arg1 = GetArgument(args, 1, 25);
 			input = input.StripTrailingBlankLineOrDefault();
 			return Solution2(input).ToString();
 		}
--- Tests/2020/Tests_13.cs
+++ Tests/2020/Tests_13.cs
@@ -20,24 +20,39 @@ namespace AdventOfCode.Tests.Year2020 {
 
 		[Theory]
 		[InlineData(new string[] {
+			"1004098",
+			"13, 17"
+		}, 169)]
+		[InlineData(new string[] {
+			"1004098",
+			"3, 7, 5"
+		}, 48)]
+		[InlineData(new string[] {
+			"1004098",
 			"17,x,13,19"
 		}, 3417)]
 		[InlineData(new string[] {
+			"1004098",
 			"7,13,x,x,59,x,31,19"
 		}, 1068781)]
 		[InlineData(new string[] {
+			"1004098",
 			"67,7,59,61"
 		}, 754018)]
 		[InlineData(new string[] {
+			"1004098",
 			"67,7,x,59,61"
 		}, 1261476)]
 		[InlineData(new string[] {
+			"1004098",
 			"1789,37,47,1889"
 		}, 1202161486)]
 		[InlineData(new string[] {
+			"1004098",
 			"67,x,7,59,61"
 		}, 779210)]
 		[InlineData(new string[] {
+			"1004098",
 			"23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,509,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29,x,401,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,19"
 		}, 0)]
 		public void Part2(string[] input, long expected) {
