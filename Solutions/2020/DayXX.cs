8d76fcfcde760e8cf5362036700c8fcdbc96e3ef
Simon Brookes <2011834+smabuk@users.noreply.github.com>
2020-12-14 22:12:41 +0000

    2015 Day21 Part1 Solved

--- Solutions/2020/DayXX.cs
+++ Solutions/2020/DayXX.cs
@@ -48,12 +48,16 @@ namespace AdventOfCode.Solutions.Year2020 {
 		#region Problem initialisation
 		public static string Part1(string[]? input, params object[]? args) {
 			if (input is null) { return "Error: No data provided"; }
+			bool testing = GetArgument(args, 1, false);
+			if (testing is false) { return "** Solution not written yet **"; }
 			// int arg1 = GetArgument(args, 1, 25);
 			input = input.StripTrailingBlankLineOrDefault();
 			return Solution1(input).ToString();
 		}
 		public static string Part2(string[]? input, params object[]? args) {
 			if (input is null) { return "Error: No data provided"; }
+			bool testing = GetArgument(args, 1, false);
+			if (testing is false) { return "** Solution not written yet **"; }
 			// int arg1 = GetArgument(args, 1, 25);
 			input = input.StripTrailingBlankLineOrDefault();
 			return Solution2(input).ToString();
