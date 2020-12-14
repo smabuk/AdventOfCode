d7ee7af12a111659deab52bbb29d98d2a40ea279
Simon Brookes <2011834+smabuk@users.noreply.github.com>
2020-12-14 22:18:16 +0000

    2015 Day21 Solved

--- Tests/2015/Tests_21.cs
+++ Tests/2015/Tests_21.cs
@@ -22,15 +22,10 @@ namespace AdventOfCode.Tests.Year2015 {
 
 		[Theory]
 		[InlineData(new string[] {
-			"Hit Points: 12",
-			"Damage: 7",
-			"Armor: 2"
-		}, 9999)]
-		[InlineData(new string[] {
 			"Hit Points: 104",
 			"Damage: 8",
 			"Armor: 1"
-		}, 9999)]
+		}, 148)]
 		public void Part2(string[] input, int expected) {
 			_ = int.TryParse(SolutionRouter.SolveProblem(2015, 21, 2, input), out int actual);
 			Assert.Equal(expected, actual);
