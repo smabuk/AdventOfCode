66a22cc452a4cdc0a9ecf00b72f2d1b81f5a1dc7
Simon Brookes <2011834+smabuk@users.noreply.github.com>
2020-12-16 08:01:57 +0000

    2020 Day16 Solved

--- Solutions/2020/Day16.cs
+++ Solutions/2020/Day16.cs
@@ -37,9 +37,57 @@ namespace AdventOfCode.Solutions.Year2020 {
 			return ticketScanningErrorRate;
 		}
 
-		private static string Solution2(string[] input) {
+		private static long Solution2(string[] input) {
 			Input instructions = Parse(input);
-			return "** Solution not written yet **";
+
+			List<int> allowedValues = instructions
+				.TicketFields.SelectMany(i => i.AllowedValues)
+				.ToList();
+
+			List<Ticket> validTickets = new();
+			foreach (var ticket in instructions.NearbyTickets) {
+				if (ticket.Values.All(t => allowedValues.Contains(t))) {
+					validTickets.Add(ticket);
+				}				
+			}
+
+			int noOfFields = instructions.TicketFields.Count();
+
+			List<(string name, int position)> answers = new();
+			foreach (TicketField tf in instructions.TicketFields) {
+				for (int i = 1; i <= noOfFields; i++) {
+					int[] values = validTickets.Select(x => x.Values[i - 1]).ToArray();
+					if (values.All(x => tf.AllowedValues.Contains(x))) {
+						answers.Add((tf.Name, i));
+					}
+				}
+			}
+
+			List<int> departurePositions = new();
+			for (int i = 1; i <= noOfFields; i++) {
+				var names = answers
+					.GroupBy(x => x.name)
+					.Select(group => new { Name = group.Key, Count = group.Count() })
+					.Where(g => g.Count == 1);
+				foreach (var item in names) {
+					int position = answers.
+						Where(x => x.name == item.Name)
+						.SingleOrDefault().position;
+					answers.RemoveAll(a => a.name == item.Name);
+					answers.RemoveAll(a => a.position == position);
+					if (item.Name.StartsWith("departure")) {
+						departurePositions.Add(position);
+					}
+				}
+			}
+
+			long value = 1L;
+
+			foreach (int pos in departurePositions) {
+				value *= instructions.YourTicket.Values[pos - 1];
+			}
+
+			return value;
 		}
 
 
@@ -70,7 +118,6 @@ namespace AdventOfCode.Solutions.Year2020 {
 			for (i += 3; i < input.Length; i++) {
 				nearbyTickets.Add(new(input[i].Split(",").Select( i => int.Parse(i)).ToArray()));
 			}
-
 
 			Input theInput = new(ticketFields, yourTicket, nearbyTickets);
 			return theInput;
--- Tests/2020/Tests_16.cs
+++ Tests/2020/Tests_16.cs
@@ -26,19 +26,18 @@ namespace AdventOfCode.Tests.Year2020 {
 
 		[Theory]
 		[InlineData(new string[] {
-			"class: 1-3 or 5-7",
-			"row: 6-11 or 33-44",
-			"seat: 13-40 or 45-50",
+			"departure class: 0-1 or 4-19",
+			"row: 0-5 or 8-19",
+			"seat: 0-13 or 16-19",
 			"",
 			"your ticket:",
-			"7,1,14",
+			"11,12,13",
 			"",
 			"nearby tickets:",
-			"7,3,47",
-			"40,4,50",
-			"55,2,20",
-			"38,6,12",
-		}, 9999)]
+			"3,9,18",
+			"15,1,5",
+			"5,14,9",
+		}, 12)]
 		public void Part2(string[] input, long expected) {
 			_ = long.TryParse(SolutionRouter.SolveProblem(2020, 16, 2, input), out long actual);
 			Assert.Equal(expected, actual);
