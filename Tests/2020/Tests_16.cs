0e0149c8b6db7b0f03264a7b2b1b424946692c19
Simon Brookes <2011834+smabuk@users.noreply.github.com>
2020-12-16 06:00:26 +0000

    2020 Day16 Part1 Solved

--- Tests/2020/Tests_16.cs
+++ Tests/2020/Tests_16.cs
@@ -3,11 +3,22 @@
 using Xunit;
 
 namespace AdventOfCode.Tests.Year2020 {
-	public class Tests_16 {
+	public class Tests_16_Ticket_Translation {
 		[Theory]
 		[InlineData(new string[] {
-			"0,3,6"
-		}, 436)]
+			"class: 1-3 or 5-7",
+			"row: 6-11 or 33-44",
+			"seat: 13-40 or 45-50",
+			"",
+			"your ticket:",
+			"7,1,14",
+			"",
+			"nearby tickets:",
+			"7,3,47",
+			"40,4,50",
+			"55,2,20",
+			"38,6,12",
+		}, 71)]
 		public void Part1(string[] input, int expected) {
 			_ = int.TryParse(SolutionRouter.SolveProblem(2020, 16, 1, input), out int actual);
 			Assert.Equal(expected, actual);
@@ -15,8 +26,19 @@ namespace AdventOfCode.Tests.Year2020 {
 
 		[Theory]
 		[InlineData(new string[] {
-			"9,12,1,4,17,0,18"
-		}, 1407)]
+			"class: 1-3 or 5-7",
+			"row: 6-11 or 33-44",
+			"seat: 13-40 or 45-50",
+			"",
+			"your ticket:",
+			"7,1,14",
+			"",
+			"nearby tickets:",
+			"7,3,47",
+			"40,4,50",
+			"55,2,20",
+			"38,6,12",
+		}, 9999)]
 		public void Part2(string[] input, long expected) {
 			_ = long.TryParse(SolutionRouter.SolveProblem(2020, 16, 2, input), out long actual);
 			Assert.Equal(expected, actual);
