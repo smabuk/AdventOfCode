6fec7ca1223a4037d01e5355432e80a592cba48e
Simon Brookes <2011834+smabuk@users.noreply.github.com>
2020-12-17 20:59:34 +0000

    2020 Day17 refactored a bit more

--- Solutions/2020/Day17.cs
+++ Solutions/2020/Day17.cs
@@ -33,6 +33,12 @@ namespace AdventOfCode.Solutions.Year2020 {
 			Dictionary<Point, Cube> cubes = ParseInput(input, 3);
 			Dictionary<Point, Cube> nextCubes = new();
 
+			Point[] directions = (from dx in new[] { -1, 0, 1 }
+								  from dy in new[] { -1, 0, 1 }
+								  from dz in new[] { -1, 0, 1 }
+								  where dx != 0 || dy != 0 || dz != 0
+								  select new CubePoint(dx, dy, dz)).ToArray();
+
 			int countOn = 0;
 			for (int i = 0; i < noOfIterations; i++) {
 
@@ -49,22 +55,13 @@ namespace AdventOfCode.Solutions.Year2020 {
 					for (int y = minY; y <= maxY; y++) {
 						for (int x = minX; x <= maxX; x++) {
 							CubePoint p = new(x, y, z);
-							Cube current;
-							if (cubes.ContainsKey(p)) {
-								current = cubes[p];
-							} else {
-								current = new(p, OFF_STATE);
-							}
+							Cube current = cubes.GetValueOrDefault(p) ?? new(p, OFF_STATE);
 							Cube next = current;
-							List<Cube> adjacent = GetAdjacentCubes(current, cubes, 3);
-							if (current.State == OFF_STATE) {
-								if (adjacent.Count(s => s.State == ON_STATE) == 3) {
-									next = next with { State = ON_STATE };
-								}
-							} else if (current.State == true) {
-								if (adjacent.Count(s => s.State == ON_STATE) is not 2 and not 3) {
-									next = next with { State = OFF_STATE };
-								}
+							int adjacentCount = GetAdjacentCubesCount(current, cubes, directions);
+							if (current.State == OFF_STATE && adjacentCount == 3) {
+								next = next with { State = ON_STATE };
+							} else if (current.State == ON_STATE && (adjacentCount is not 2 and not 3)) {
+								next = next with { State = OFF_STATE };
 							}
 							if (next.State == ON_STATE) {
 								nextCubes[p] = next;
@@ -79,43 +76,18 @@ namespace AdventOfCode.Solutions.Year2020 {
 			return countOn;
 		}
 
-		private static List<Cube> GetAdjacentCubes(Cube cube, Dictionary<Point, Cube> cubes, int noOfDimensions) {
-			List<Cube> adjacent = new();
-			int minW = -1, maxW = 1;
-			if (noOfDimensions == 3) {
-				minW = 0;
-				maxW = 0;
-			}
-
-			for (int z = -1; z <= 1; z++) {
-				for (int y = -1; y <= 1; y++) {
-					for (int x = -1; x <= 1; x++) {
-						for (int w = minW; w <= maxW; w++) {
-							if (w == 0 && x == 0 && y == 0 && z == 0) {
-								continue;
-							}
-							Point p = cube.Point switch {
-								HyperPoint hp => new HyperPoint(hp.W + w, hp.X + x, hp.Y + y, hp.Z + z),
-								CubePoint cp => new CubePoint(cp.X + x, cp.Y + y, cp.Z + z),
-								_ => new Point(cube.Point.X + x, cube.Point.Y + y)
-							};
-							bool state;
-							if (cubes.ContainsKey(p)) {
-								state = cubes[p].State;
-								adjacent.Add(new(p, state));
-							}
-						}
-					}
-				}
-			}
-			return adjacent;
-		}
-
 		private static int Solution2(string[] input) {
 			int noOfIterations = 6;
 
 			Dictionary<Point, Cube> cubes = ParseInput(input, 4);
 			Dictionary<Point, Cube> nextCubes = new();
+
+			Point[] directions = (from dw in new[] { -1, 0, 1 }
+								  from dx in new[] { -1, 0, 1 }
+								  from dy in new[] { -1, 0, 1 }
+								  from dz in new[] { -1, 0, 1 }
+								  where dw != 0 || dx != 0 || dy != 0 || dz != 0
+								  select new HyperPoint(dw, dx, dy, dz)).ToArray();
 
 			int countOn = 0;
 			for (int i = 0; i < noOfIterations; i++) {
@@ -136,22 +108,13 @@ namespace AdventOfCode.Solutions.Year2020 {
 						for (int x = minX; x <= maxX; x++) {
 							for (int w = minW; w <= maxW; w++) {
 							HyperPoint p = new(w, x, y, z);
-								Cube current;
-								if (cubes.ContainsKey(p)) {
-									current = cubes[p];
-								} else {
-									current = new(p, OFF_STATE);
-								}
+								Cube current = cubes.GetValueOrDefault(p) ?? new(p, OFF_STATE);
 								Cube next = current;
-								List<Cube> adjacent = GetAdjacentCubes(current, cubes, 4);
-								if (current.State == OFF_STATE) {
-									if (adjacent.Count(s => s.State == ON_STATE) == 3) {
-										next = next with { State = ON_STATE };
-									}
-								} else if (current.State == true) {
-									if (adjacent.Count(s => s.State == ON_STATE) is not 2 and not 3) {
-										next = next with { State = OFF_STATE };
-									}
+								int adjacentCount = GetAdjacentCubesCount(current, cubes, directions);
+								if (current.State == OFF_STATE && adjacentCount == 3) {
+									next = next with { State = ON_STATE };
+								} else if (current.State == ON_STATE && (adjacentCount is not 2 and not 3)) {
+									next = next with { State = OFF_STATE };
 								}
 								if (next.State == ON_STATE) {
 									nextCubes[p] = next;
@@ -165,6 +128,21 @@ namespace AdventOfCode.Solutions.Year2020 {
 			}
 
 			return countOn;
+		}
+
+		private static int GetAdjacentCubesCount(Cube cube, Dictionary<Point, Cube> cubes, Point[] directions) {
+			int count = 0;
+			foreach (Point dir in directions) {
+				Point p = cube.Point switch {
+					HyperPoint hp when dir is HyperPoint d => new HyperPoint(hp.W + d.W, hp.X + d.X, hp.Y + d.Y, hp.Z + d.Z),
+					CubePoint cp when dir is CubePoint d => new CubePoint(cp.X + d.X, cp.Y + d.Y, cp.Z + d.Z),
+					_ => new Point(cube.Point.X + dir.X, cube.Point.Y + dir.Y)
+				};
+				if (cubes.GetValueOrDefault(p)?.State == ON_STATE) {
+					count++;
+				}
+			}
+			return count;
 		}
 
 		private static Dictionary<Point, Cube> ParseInput(string[] input, int noOfDimensions) {
