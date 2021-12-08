b86afee12da24045c9d012f2cfc737d285ff3ca1
smabuk <2011834+smabuk@users.noreply.github.com>
2021-12-08 18:48:33 +0000

    2021 Day08 minor refactoring

--- Solutions/2021/Day08.cs
+++ Solutions/2021/Day08.cs
@@ -10,81 +10,52 @@ public class Day08 {
 	record FourDigitDisplay(List<string> SignalPatterns, List<string> OutputValues) {
 
 		public int GetFourDigitDisplay() {
-			string[] patterns = new string[10];
-			Dictionary<string, int> numbers = new();
-
 			List<string> signalPatterns = SignalPatterns;
 
-			foreach (string sp in signalPatterns) {
-				numbers[sp] = -1;
-			}
-
-			foreach (string signalPattern in signalPatterns) {
-				if (signalPattern.Length == 2) {
-					patterns[1] = signalPattern;
-					numbers[signalPattern] = 1;
-				} else if (signalPattern.Length == 3) {
-					patterns[7] = signalPattern;
-					numbers[signalPattern] = 7;
-				} else if (signalPattern.Length == 4) {
-					patterns[4] = signalPattern;
-					numbers[signalPattern] = 4;
-				} else if (signalPattern.Length == 7) {
-					patterns[8] = signalPattern;
-					numbers[signalPattern] = 8;
-				}
-			}
-
+			Dictionary<string, int>? numbers = signalPatterns
+				.ToDictionary(sp => sp, _ => -1);
+			string[] patterns = new string[10];
+			
+			SetPatternNumber(signalPatterns.Where(sp => sp.Length == 2).Single(), 1);
+			SetPatternNumber(signalPatterns.Where(sp => sp.Length == 3).Single(), 7);
+			SetPatternNumber(signalPatterns.Where(sp => sp.Length == 4).Single(), 4);
+			SetPatternNumber(signalPatterns.Where(sp => sp.Length == 7).Single(), 8);
 			signalPatterns.RemoveAll(s => patterns.Contains(s));
-			foreach (string signalPattern in signalPatterns) {
-				if (signalPattern.Length == 6 && ContainsAllWires(signalPattern, patterns[4])) {
-					patterns[9] = signalPattern;
-					numbers[signalPattern] = 9;
-				} else if (signalPattern.Length == 6 && ContainsAllWires(signalPattern, patterns[7])) {
-					patterns[0] = signalPattern;
-					numbers[signalPattern] = 0;
-				} else if (signalPattern.Length == 6) {
-					patterns[6] = signalPattern;
-					numbers[signalPattern] = 6;
+
+			foreach (string signalPattern in signalPatterns.Where(sp => sp.Length == 6)) {
+				if (signalPattern.ContainsAllWires(patterns[4])) {
+					SetPatternNumber(signalPattern, 9);
+				} else if (signalPattern.ContainsAllWires(patterns[7])) {
+					SetPatternNumber(signalPattern, 0);
+				} else {
+					SetPatternNumber(signalPattern, 6);
 				}
 			}
-
 			signalPatterns.RemoveAll(s => patterns.Contains(s));
+
 			foreach (string signalPattern in signalPatterns) {
-				if (signalPattern.Length == 5 && ContainsAllWires(signalPattern, patterns[1])) {
-					patterns[3] = signalPattern;
-					numbers[signalPattern] = 3;
-				} else if (signalPattern.Length == 5 && ContainsAllWires(patterns[6],signalPattern)) {
-					patterns[5] = signalPattern;
-					numbers[signalPattern] = 5;
-				} else if (signalPattern.Length == 5) {
-					patterns[2] = signalPattern;
-					numbers[signalPattern] = 2;
+				if (signalPattern.ContainsAllWires(patterns[1])) {
+					SetPatternNumber(signalPattern, 3);
+				} else if (patterns[6].ContainsAllWires(signalPattern)) {
+					SetPatternNumber(signalPattern, 5);
+				} else {
+					SetPatternNumber(signalPattern, 2);
 				}
 			}
 
-			int multiplier = 1000;
-			int value = 0;
-			foreach (string output in OutputValues) {
-				value += numbers[output] * multiplier;
-				multiplier /= 10;
-			}
+			return int.Parse(String.Join("", OutputValues.Select(o => numbers[o])));
 
-			return value;
+
+
+
+			void SetPatternNumber(string signalPattern, int number) {
+				patterns[number] = signalPattern;
+				numbers[signalPattern] = number;
+			}
 		}
 
-		private bool ContainsAllWires(string larger, string smaller) {
-			foreach (char c in smaller)
-			{
-				if (larger.Contains(c) == false) {
-					return false;
-				}
-			}
-			return true;
-		}
-
-		public int OneFourSevenEightTotal => OutputValues
-			.Count(o => o.Length == 2 || o.Length == 4 || o.Length == 3 || o.Length == 7);
+		public int OneFourSevenEightTotal => 
+			OutputValues.Count(o => new int[] { 2, 3, 4, 7 }.Contains(o.Length));
 
 	}
 	private static int Solution1(string[] input) {
@@ -133,4 +104,15 @@ public class Day08 {
 	}
 	#endregion
 
+}
+
+internal static class Day08_StringExtensions {
+	internal static bool ContainsAllWires(this string longer, string shorter) {
+		foreach (char c in shorter) {
+			if (longer.Contains(c) == false) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
