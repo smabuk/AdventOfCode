b42fa90431a8a231529efd9f11613fd30a12a6a6
smabuk <2011834+smabuk@users.noreply.github.com>
2021-12-09 08:07:13 +0000

    2019 Day09 refactored

--- Solutions/2021/Day09.cs
+++ Solutions/2021/Day09.cs
@@ -2,19 +2,21 @@
 
 /// <summary>
 /// Day 09: Smoke Basin
-/// https://adventofcode.com/2021/day/XX
+/// https://adventofcode.com/2021/day/9
 /// </summary>
 [Description("Smoke Basin")]
 public class Day09 {
 
-	record Vector(int DX, int DY);
+	private static readonly List<(int dX, int dY)> DIRECTIONS = new()
+		{ (0, -1), (0, 1), (-1, 0), (1, 0) };
+
 	record Point(int X, int Y);
+
 	record Basin(Point LowPoint, List<Point> Locations) {
 		public int Size => Locations.Count;
 	};
 
 	private static int Solution1(string[] input) {
-		List<Vector> vectors = new() { new(0, -1), new Vector(0, 1), new Vector(-1, 0), new Vector(1, 0) };
 		int[,] heatMap = input.Select(i => ParseLine(i)).SelectMany(i => i).AsArray(input[0].Length);
 
 		int cols = heatMap.GetUpperBound(0);
@@ -24,35 +26,33 @@ public class Day09 {
 
 		for (int y = 0; y <= rows; y++) {
 			for (int x = 0; x <= cols; x++) {
-				if (IsLowPoint(x, y)) {
+				if (IsLowPoint(x, y, heatMap)) {
 					lowPoints.Add(heatMap[x, y]);
 				}
 			}
 		}
 
 		return lowPoints.Sum(lp => lp + 1);
-
-		bool IsLowPoint(int col, int row) {
-			bool lowPoint = true;
-			int value = heatMap[col, row];
-			foreach (Vector v in vectors) {
-				int newX = col + v.DX;
-				int newY = row + v.DY;
-				if (newX >= 0 && newX <= cols) {
-					if (newY >= 0 && newY <= rows) {
-						if (heatMap[newX, newY] <= value) {
-							return false;
-						}
-					}
-				}
-			}
-			return lowPoint;
-		}
 	}
 
+	static bool IsLowPoint(int col, int row, int[,] array) {
+		bool lowPoint = true;
+		int cols = array.GetUpperBound(0);
+		int rows = array.GetUpperBound(1);
+		int value = array[col, row];
+		foreach ((int dX, int dY) in DIRECTIONS) {
+			int newX = col + dX;
+			int newY = row + dY;
+			if (newX >= 0 && newX <= cols && newY >= 0 && newY <= rows) {
+				if (array[newX, newY] <= value) {
+					return false;
+				}
+			}
+		}
+		return lowPoint;
+	}
 
 	private static long Solution2(string[] input) {
-		List<Vector> vectors = new() { new(0, -1), new Vector(0, 1), new Vector(-1, 0), new Vector(1, 0) };
 		int[,] heatMap = input.Select(i => ParseLine(i)).SelectMany(i => i).AsArray(input[0].Length);
 
 		int cols = heatMap.GetUpperBound(0);
@@ -62,61 +62,38 @@ public class Day09 {
 
 		for (int y = 0; y <= rows; y++) {
 			for (int x = 0; x <= cols; x++) {
-				if (IsLowPoint(x, y)) {
+				if (IsLowPoint(x, y, heatMap)) {
 					Point lowPoint = new(x, y);
-					Basin b = new(lowPoint, new());
-					basins.Add(b);
+					Basin basin = new(lowPoint, GetAjacentBasinPoints(lowPoint, heatMap, new() { lowPoint }));
+					basins.Add(basin);
 				}
 			}
 		}
 
-		// Build basins
-		foreach (Basin basin in basins) {
-			List<Point> points = GetAjacentBasinPoints(basin.LowPoint, new() { basin.LowPoint });
-			basin.Locations.AddRange(points);
-		}
+		return basins
+			.OrderByDescending(b => b.Size)
+			.Take(3)
+			.Select(b => b.Size)
+			.Aggregate((a, b) => a * b);
+	}
 
-		// multiply sizes of largest 3
-		var biggest = basins.OrderByDescending(b => b.Size).Take(3).ToArray();
-		return biggest[0].Size * biggest[1].Size * biggest[2].Size;
-
-
-		List<Point> GetAjacentBasinPoints(Point p, List<Point> knownPoints) {
-			int value = heatMap[p.X, p.Y];
-			foreach (Vector v in vectors) {
-				int newX = p.X + v.DX;
-				int newY = p.Y + v.DY;
-				Point newP = new Point(newX, newY);
-				if (!knownPoints.Contains(newP)) {
-					if (newX >= 0 && newX <= cols) {
-						if (newY >= 0 && newY <= rows) {
-							if (heatMap[newX, newY] < 9) {
-								knownPoints.Add(new Point(newX, newY));
-								knownPoints = GetAjacentBasinPoints(newP, knownPoints);
-							}
-						}
+	static List<Point> GetAjacentBasinPoints(Point p, int[,] array, List<Point> knownPoints) {
+		int cols = array.GetUpperBound(0);
+		int rows = array.GetUpperBound(1);
+		foreach ((int dX, int dY) in DIRECTIONS) {
+			int newX = p.X + dX;
+			int newY = p.Y + dY;
+			Point newP = new(newX, newY);
+			if (!knownPoints.Contains(newP)) {
+				if (newX >= 0 && newX <= cols && newY >= 0 && newY <= rows) {
+					if (array[newX, newY] < 9) {
+						knownPoints.Add(newP);
+						knownPoints = GetAjacentBasinPoints(newP, array, knownPoints);
 					}
 				}
 			}
-			return knownPoints;
 		}
-
-		bool IsLowPoint(int col, int row) {
-			bool lowPoint = true;
-			int value = heatMap[col, row];
-			foreach (Vector v in vectors) {
-				int newX = col + v.DX;
-				int newY = row + v.DY;
-				if (newX >= 0 && newX <= cols) {
-					if (newY >= 0 && newY <= rows) {
-						if (heatMap[newX, newY] <= value) {
-							return false;
-						}
-					}
-				}
-			}
-			return lowPoint;
-		}
+		return knownPoints;
 	}
 
 
@@ -133,13 +110,11 @@ public class Day09 {
 	#region Problem initialisation
 	public static string Part1(string[]? input, params object[]? args) {
 		if (input is null) { return "Error: No data provided"; }
-		// int arg1 = GetArgument(args, 1, 25);
 		input = input.StripTrailingBlankLineOrDefault();
 		return Solution1(input).ToString();
 	}
 	public static string Part2(string[]? input, params object[]? args) {
 		if (input is null) { return "Error: No data provided"; }
-		// int arg1 = GetArgument(args, 1, 25);
 		input = input.StripTrailingBlankLineOrDefault();
 		return Solution2(input).ToString();
 	}
