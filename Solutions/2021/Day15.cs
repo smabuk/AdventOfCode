f2bdd95f32883d7cec5909fafc2e1e1e454ec075
smabuk <2011834+smabuk@users.noreply.github.com>
2021-12-15 09:28:51 +0000

    2021 Day15 refactored with some comments to explain Dijkstra's algorithm

--- Solutions/2021/Day15.cs
+++ Solutions/2021/Day15.cs
@@ -16,28 +16,7 @@ public class Day15 {
 		Point start = new(0, 0);
 		Point end = new(new(grid.GetUpperBound(0), grid.GetUpperBound(1)));
 
-		// https://en.wikipedia.org/wiki/Dijkstra's_algorithm
-
-		PriorityQueue<Cell<int>, int> priorityQueue = new();
-		priorityQueue.Enqueue(new(start, 0), 0);
-		Dictionary<Point, int> costs = new();
-		costs.Add(start, 0);
-
-		while (priorityQueue.Count > 0) {
-			Cell<int> cell = priorityQueue.Dequeue();
-
-			foreach ((int x, int y, int value) in grid.GetAdjacentCells(cell.Index)) {
-				Cell<int> neighbour = new(x, y, value);
-				if (!costs.ContainsKey(neighbour.Index)) {
-					int cost = costs[cell.Index] + neighbour.Value;
-					costs[neighbour.Index] = cost;
-					if (neighbour.Index == end) {
-						break;
-					}
-					priorityQueue.Enqueue(neighbour, cost);
-				}
-			}
-		}
+		Dictionary<Point, int> costs = Dijkstras(grid, start, end);
 
 		return costs[end];
 	}
@@ -45,10 +24,8 @@ public class Day15 {
 	private static int Solution2(string[] input) {
 		int[,] inputGrid = input.SelectMany(i => i.AsDigits()).To2dArray(input[0].Length);
 
-		Point start = new(0, 0);
 		int inputGridWidth = inputGrid.GetLength(0);
 		int inputGridHeight = inputGrid.GetLength(1);
-
 
 		int[,] grid = new int[inputGridWidth * 5, inputGridHeight * 5];
 
@@ -67,9 +44,49 @@ public class Day15 {
 			}
 		}
 
+		Point start = new(0, 0);
 		Point end = new(new(grid.GetUpperBound(0), grid.GetUpperBound(1)));
 
-		// https://en.wikipedia.org/wiki/Dijkstra's_algorithm
+		Dictionary<Point, int> costs = Dijkstras(grid, start, end);
+
+		return costs[end];
+	}
+
+	/// <summary>
+	///  Dijkstra's algorithm
+	///  https://en.wikipedia.org/wiki/Dijkstra's_algorithm
+	/// 
+	/// PseudoCode:
+	///  function Dijkstra(Graph, source):
+	///      dist[source] ← 0                           // Initialization
+	/// 
+	///      create vertex priority queue Q
+	/// 
+	///      for each vertex v in Graph:          
+	///          if v ≠ source
+	///              dist[v] ← INFINITY                 // Unknown distance from source to v
+	///              prev[v] ← UNDEFINED                // Predecessor of v
+	/// 
+	///          Q.add_with_priority(v, dist[v])
+	/// 
+	/// 
+	///      while Q is not empty:                      // The main loop
+	///          u ← Q.extract_min()                    // Remove and return best vertex
+	///          for each neighbor v of u:              // only v that are still in Q
+	///              alt ← dist[u] + length(u, v)
+	///              if alt < dist[v]
+	///                  dist[v] ← alt
+	///                  prev[v] ← u
+	///                  Q.decrease_priority(v, alt)
+	/// 
+	///      return dist, prev
+	/// 
+	/// </summary>
+	/// <param name="grid"></param>
+	/// <param name="start"></param>
+	/// <param name="end"></param>
+	/// <returns>costs</returns>
+	private static Dictionary<Point, int> Dijkstras(int[,] grid, Point start, Point end) {
 
 		PriorityQueue<Cell<int>, int> priorityQueue = new();
 		priorityQueue.Enqueue(new(start, 0), 0);
@@ -92,6 +109,6 @@ public class Day15 {
 			}
 		}
 
-		return costs[end];
+		return costs;
 	}
 }
