deeba21f25053b6717ca8f77cb38197a5370eacb
smabuk <2011834+smabuk@users.noreply.github.com>
2021-12-21 06:39:16 +0000

    2021 Day21 Part1 Solved

---
+++ Solutions/2021/Day21.cs
@@ -0,0 +1,82 @@
+﻿namespace AdventOfCode.Solutions.Year2021;
+
+/// <summary>
+/// Day 21: Dirac Dice
+/// https://adventofcode.com/2021/day/21
+/// </summary>
+[Description("Dirac Dice")]
+public class Day21 {
+
+	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+
+	record Player(string Name) {
+
+		public int Position { get; set; }
+		public int DieRolls { get; set; }
+		public int Points { get; set; }
+		public long Score => Points * DieRolls;
+
+		public Player(string name, int position) : this(name) {
+			Position = position;
+		}
+
+		public void Move(int sumOfDieRolls) {
+			Position += sumOfDieRolls;
+			Position = (Position - 1) % 10 + 1;
+			Points += Position;
+			DieRolls += 3;
+		}
+
+	};
+
+	private static long Solution1(string[] input) {
+		List<Player> players = input.Select(i => ParseLine(i)).ToList();
+
+		int deterministicDie = 0;
+
+		int turn = 0;
+		int dieRolls = 0;
+		while (players.Where(p => p.Points >= 1000).Any() is false) {
+			int dieScore = 0;
+			dieScore += (++deterministicDie - 1) % 100 + 1;
+			dieScore += (++deterministicDie - 1) % 100 + 1;
+			dieScore += (++deterministicDie - 1) % 100 + 1;
+			dieRolls += 3;
+
+			Player player = players[turn++ % 2];
+			player.Move(dieScore);
+		}
+
+		return players.Where(p => p.Points < 1000).Single().Points * dieRolls;
+	}
+
+	private static long Solution2(string[] input) {
+		List<Player> players = input.Select(i => ParseLine(i)).ToList();
+
+		int deterministicDie = 0;
+
+		int turn = 0;
+		int dieRolls = 0;
+		while (players.Where(p => p.Points > 21).Any() is false) {
+			int dieScore = 0;
+			dieScore += (++deterministicDie - 1) % 100 + 1;
+			dieScore += (++deterministicDie - 1) % 100 + 1;
+			dieScore += (++deterministicDie - 1) % 100 + 1;
+			dieRolls += 3;
+
+			Player player = players[turn++ % 2];
+			player.Move(dieScore);
+		}
+
+		return players.Where(p => p.Points < 1000).Single().Points * dieRolls;
+	}
+
+	private static Player ParseLine(string input) {
+		Match match = Regex.Match(input, @"(?<name>.*) starting position: (?<start>\d+)");
+		if (match.Success) {
+			return new(match.Groups["name"].Value, int.Parse(match.Groups["start"].Value));
+		}
+		throw new NotSupportedException();
+	}
+}
---
+++ Tests/2021/Tests_21.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode.Tests.Year2021;
+
+public class Tests_21_Dirac_Dice {
+	[Theory]
+	[InlineData(new string[] {
+		"Player 1 starting position: 4",
+		"Player 2 starting position: 8",
+	}, 739785)]
+	public void Part1(string[] input, int expected) {
+		_ = long.TryParse(SolutionRouter.SolveProblem(2021, 21, 1, input), out long actual);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData(new string[] {
+		"Player 1 starting position: 4",
+		"Player 2 starting position: 8",
+	}, 739785)]
+	public void Part2(string[] input, int expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2021, 20, 1, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+}
