e51457c0a024930440e3b2876427a399c80fb24b
smabuk <2011834+smabuk@users.noreply.github.com>
2021-12-22 23:49:38 +0000

    2021 Day23 Part2 WIP2021 Day23 Part1 Solved very manually

---
+++ Tests/2021/Tests_23.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode.Tests.Year2021;
+
+public class Tests_23_Amphipod {
+	[Theory]
+	[InlineData(new string[] {
+		"#############",
+		"#...........#",
+		"###B#C#B#D###",
+		"  #A#D#C#A#  ",
+		"  #########  ",
+	}, 12521)]
+	public void Part1(string[] input, int expected) {
+		_ = long.TryParse(SolutionRouter.SolveProblem(2021, 23, 1, input, true), out long actual);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory(Skip = "This problem needs more thought")]
+	[InlineData(new string[] {
+		"#############",
+		"#...........#",
+		"###B#C#B#D###",
+		"  #A#D#C#A#  ",
+		"  #########  ",
+	}, 44169)]
+	public void Part2(string[] input, long expected) {
+		_ = long.TryParse(SolutionRouter.SolveProblem(2021, 23, 2, input), out long actual);
+		Assert.Equal(expected, actual);
+	}
+}
