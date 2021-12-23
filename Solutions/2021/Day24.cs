11e50e1e9354046db968966af0af83496b128530
smabuk <2011834+smabuk@users.noreply.github.com>
2021-12-23 22:39:28 +0000

    2021 Day24 WIP

---
+++ Solutions/2021/Day24.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode.Solutions.Year2021;
+
+/// <summary>
+/// Day 24: Arithmetic Logic Unit
+/// https://adventofcode.com/2021/day/24
+/// </summary>
+[Description("Arithmetic Logic Unit")]
+public class Day24 {
+
+	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+
+	record RecordType(string Name, int Value);
+
+	private static string Solution1(string[] input) {
+		//string inputLine = input[0];
+		//List<string> inputs = input.ToList();
+		List<RecordType> instructions = input.Select(i => ParseLine(i)).ToList();
+		return "** Solution not written yet **";
+	}
+
+	private static string Solution2(string[] input) {
+		//string inputLine = input[0];
+		//List<string> inputs = input.ToList();
+		List<RecordType> instructions = input.Select(i => ParseLine(i)).ToList();
+		return "** Solution not written yet **";
+	}
+
+	private static RecordType ParseLine(string input) {
+		//MatchCollection match = Regex.Matches(input, @"(opt1|opt2|opt3) ([\+\-]\d+)");
+		Match match = Regex.Match(input, @"(opt1|opt2|opt3) ([\+\-]\d+)");
+		if (match.Success) {
+			return new(match.Groups[1].Value, int.Parse(match.Groups[2].Value));
+		}
+		return null!;
+	}
+}
---
+++ Tests/2021/Tests_24.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode.Tests.Year2021;
+
+public class Tests_24_Arithmetic_Logic_Unit {
+	[Theory(Skip = "This problem needs more thought, solved by hand for the star")]
+	[InlineData(new string[] {
+		"#############",
+		"#...........#",
+		"###B#C#B#D###",
+		"  #A#D#C#A#  ",
+		"  #########  ",
+	}, 12521)]
+	public void Part1(string[] input, int expected) {
+		_ = long.TryParse(SolutionRouter.SolveProblem(2021, 23, 1, input, true), out long actual);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory(Skip = "This problem needs more thought, solved by hand for the star")]
+	[InlineData(new string[] {
+		"#############",
+		"#...........#",
+		"###B#C#B#D###",
+		"  #A#D#C#A#  ",
+		"  #########  ",
+	}, 44169)]
+	public void Part2(string[] input, long expected) {
+		_ = long.TryParse(SolutionRouter.SolveProblem(2021, 23, 2, input), out long actual);
+		Assert.Equal(expected, actual);
+	}
+}
