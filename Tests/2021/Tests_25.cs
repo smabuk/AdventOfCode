7b3f14ec543430af98dc4919d892341bd3f6da73
smabuk <2011834+smabuk@users.noreply.github.com>
2021-12-25 06:42:48 +0000

    2021 Day25 Solved

---
+++ Tests/2021/Tests_25.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode.Tests.Year2021;
+
+public class Tests_25_Sea_Cucumber {
+	[Theory]
+	[InlineData(new string[] {
+		"v...>>.vv>",
+		".vv>>.vv..",
+		">>.>v>...v",
+		">>v>>.>.v.",
+		"v>v.vv.v..",
+		">.>>..v...",
+		".vv..>.>v.",
+		"v.v..>>v.v",
+		"....v..v.>",
+	}, 58)]
+	public void Part1(string[] input, int expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2021, 25, 1, input, true), out int actual);
+		Assert.Equal(expected, actual);
+	}
+
+}
