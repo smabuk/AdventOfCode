250d32b3edbe42af382ea3f26675db93081920a1
smabuk <2011834+smabuk@users.noreply.github.com>
2022-11-27 19:30:08 +0000

    2015 Day24 Part1 Solved

---
+++ Solutions/2015/Day24.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+namespace AdventOfCode.Solutions._2015;
+
+/// <summary>
+/// Day 24: It Hangs in the Balance
+/// https://adventofcode.com/2015/day/24
+/// </summary>
+[Description("It Hangs in the Balance")]
+public sealed partial class Day24 {
+
+	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+
+	private static long Solution1(string[] input) {
+		List<int> packageWeights = input.AsInts().ToList();
+		int targetWeight = packageWeights.Sum() / 3;
+		int maxI = packageWeights.Count;
+		long minQE = long.MaxValue;
+
+		for (int i = 1; i < maxI; i++) {
+			foreach (var passengerWeights in packageWeights.Combinations(i).Where(x => x.Sum() == targetWeight)) {
+				long qe = passengerWeights.Select(l => Convert.ToInt64(l)).Aggregate((total, next) => total * next);
+				if (qe >= minQE) {
+					break;
+				}
+
+				List<int> remainingWeights = packageWeights.Except(passengerWeights).ToList();
+				int j = 1;
+				bool found = false;
+				while (j < remainingWeights.Count && found == false) {
+					foreach (var weights in remainingWeights.Combinations(j).Where(x => x.Sum() == targetWeight)) {
+						maxI = i + 1;
+						minQE = Math.Min(minQE, qe);
+						found = true;
+						break;
+					}
+					j++;
+				}
+			}
+		}
+
+		return minQE;
+	}
+
+	private static string Solution2(string[] input) {
+		return "** Solution not written yet **";
+	}
+
+}
---
+++ Tests/2015/Tests_24.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode.Tests._2015;
+
+public class Tests_24_It_Hangs_in_the_Balance {
+	[Theory]
+	[InlineData("""
+		1
+		2
+		3
+		4
+		5
+		7
+		8
+		9
+		10
+		11
+		"""
+		, 99)]
+	[InlineData("""
+		1
+		3
+		5
+		11
+		13
+		17
+		19
+		23
+		29
+		31
+		41
+		43
+		47
+		53
+		59
+		61
+		67
+		71
+		73
+		79
+		83
+		89
+		97
+		101
+		103
+		107
+		109
+		113
+		"""
+		, 11266889531)]
+	public void Part1(string input, long expected) {
+		_ = long.TryParse(SolutionRouter.SolveProblem(2015, 24, 1, input), out long actual);
+		Assert.Equal(expected, actual);
+	}
+}
