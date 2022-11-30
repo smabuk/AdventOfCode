4f21de5d185db5feb4b0766e966d0e94d4b416d0
smabuk <2011834+smabuk@users.noreply.github.com>
2022-11-30 14:34:41 +0000

    2015 Day25 Solved

---
+++ Solutions/2015/Day25.cs
@@ -0,0 +1,46 @@
+﻿namespace AdventOfCode.Solutions._2015;
+
+/// <summary>
+/// Day 25: Let It Snow
+/// https://adventofcode.com/2015/day/25
+/// </summary>
+[Description("Let It Snow")]
+public sealed partial class Day25 {
+
+	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+
+	record Position(int Row, int Column);
+
+	private static long Solution1(string[] input) {
+		Position targetPosition = ParseLine(input[0]);
+		long code = 20151125;
+
+		for (int i = 2; i <= int.MaxValue; i++) {
+			for (int row = i, col = 1; row > 0; row--, col++) {
+				code = (code * 252533) % 33554393;
+				if (targetPosition == new Position(row, col)) {
+					return code;
+				}
+			}
+		}
+
+		return 0;
+
+	}
+
+	private static string Solution2(string[] input) {
+		return "** CONGRATULATIONS **";
+	}
+
+	private static Position ParseLine(string input) {
+		Match match = PositionRegex().Match(input);
+		if (match.Success) {
+			return new(int.Parse(match.Groups["row"].Value), int.Parse(match.Groups["col"].Value));
+		}
+		return null!;
+	}
+
+	[GeneratedRegex("""To continue, please consult the code grid in the manual.  Enter the code at row (?<row>\d+), column (?<col>\d+).""")]
+	private static partial Regex PositionRegex();
+}
---
+++ Tests/2015/Tests_25.cs
@@ -0,0 +1,13 @@
+﻿namespace AdventOfCode.Tests._2015;
+
+public class Tests_25_It_Let_It_Snow {
+	[Theory]
+	[InlineData("To continue, please consult the code grid in the manual.  Enter the code at row 2, column 1."
+		, 31916031)]
+	[InlineData("To continue, please consult the code grid in the manual.  Enter the code at row 5, column 6."
+		, 31663883)]
+	public void Part1(string input, long expected) {
+		_ = long.TryParse(SolutionRouter.SolveProblem(2015, 25, 1, input), out long actual);
+		Assert.Equal(expected, actual);
+	}
+}
