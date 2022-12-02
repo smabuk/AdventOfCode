3823a7404cbb3963d69176e8215431f12792d7ac
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-02 05:31:51 +0000

    2022 Day02 Part1 Solved

---
+++ Solutions/2022/Day02.cs
@@ -0,0 +1,61 @@
+﻿namespace AdventOfCode.Solutions._2022;
+
+/// <summary>
+/// Day 02: Rock Paper Scissors
+/// https://adventofcode.com/2022/day/2
+/// </summary>
+[Description("Rock Paper Scissors")]
+public partial class Day02 {
+
+	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+
+	record Round(string OpponentChoice, string MyChoice) {
+		public int MyChoiceScore => MyChoice switch {
+			"X" => 1,
+			"Y" => 2,
+			"Z" => 3,
+			_ => 0,
+		};
+
+		public int MyResultScore {
+			get {
+				if (OpponentChoice == "A" && MyChoice == "X"
+					|| OpponentChoice == "B" && MyChoice == "Y"
+					|| OpponentChoice == "C" && MyChoice == "Z") {
+					return MyChoiceScore + 3;
+				} else if (OpponentChoice == "A" && MyChoice == "Y"
+					|| OpponentChoice == "B" && MyChoice == "Z"
+					|| OpponentChoice == "C" && MyChoice == "X") {
+					return MyChoiceScore + 6;
+				}
+				return MyChoiceScore;
+			}
+		}
+	};
+
+	private static int Solution1(string[] input) {
+		List<Round> round = input.Select(i => ParseLine(i)).ToList();
+		int totalScore = round.Sum(x => x.MyResultScore);
+		return totalScore;
+	}
+
+	private static string Solution2(string[] input) {
+		//string inputLine = input[0];
+		//List<string> inputs = input.ToList();
+		List<Round> instructions = input.Select(i => ParseLine(i)).ToList();
+		return "** Solution not written yet **";
+	}
+
+	private static Round ParseLine(string input) {
+		//MatchCollection match = InputRegEx().Matches(input);
+		Match match = InputRegEx().Match(input);
+		if (match.Success) {
+			return new(match.Groups["opp"].Value, match.Groups["me"].Value);
+		}
+		return null!;
+	}
+
+	[GeneratedRegex("""(?<opp>A|B|C) (?<me>\D)""")]
+	private static partial Regex InputRegEx();
+}
---
+++ Tests/2022/Tests_02.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode.Tests._2022;
+
+public class Tests_02_Rock_Paper_Scissors {
+	[Theory]
+	[InlineData("""
+		A Y
+		B X
+		C Z
+		"""
+		, 15)]
+	public void Part1(string input, long expected) {
+		_ = long.TryParse(SolutionRouter.SolveProblem(2022, 2, 1, input), out long actual);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData("""
+		A Y
+		B X
+		C Z
+		"""
+		, 9999)]
+	public void Part2(string input, long expected) {
+		_ = long.TryParse(SolutionRouter.SolveProblem(2022, 2, 2, input), out long actual);
+		Assert.Equal(expected, actual);
+	}
+}
