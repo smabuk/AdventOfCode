678bdab8790a7743d7f19d3b7bf7a24340779066
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-03 11:51:53 +0000

    2022 Day03 Solved

---
+++ Tests/2022/Tests_03.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.Tests._2022;
+
+public class Tests_03_Rucksack_Reorganization {
+	[Theory]
+	[InlineData("""
+		vJrwpWtwJgWrhcsFMMfFFhFp
+		jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+		PmmdzqPrVvPwwTWBwg
+		wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+		ttgJtRGJQctTZtZT
+		CrZsJsPPZsGzwwsLwLmpwMDw
+		"""
+		, 157)]
+	public void Part1(string input, int expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2022, 3, 1, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData("""
+		vJrwpWtwJgWrhcsFMMfFFhFp
+		jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+		PmmdzqPrVvPwwTWBwg
+		wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+		ttgJtRGJQctTZtZT
+		CrZsJsPPZsGzwwsLwLmpwMDw
+		"""
+		, 70)]
+	public void Part2(string input, int expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2022, 3, 2, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+}
