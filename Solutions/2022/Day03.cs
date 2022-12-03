ac33539134f878fab64b2543d83ab4cb97da984c
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-03 12:01:44 +0000

    2022 Day03 removed unused code

--- Solutions/2022/Day03.cs
+++ Solutions/2022/Day03.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode.Solutions._2022;
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Solutions._2022;
 
 /// <summary>
 /// Day 03: Rucksack Reorganization
@@ -10,8 +12,6 @@ public sealed partial class Day03 {
 	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
 	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
 
-	record RecordType(string Name, int Value);
-
 	private static int Solution1(string[] input) {
 		int prioritiesSum = 0;
 
@@ -19,24 +19,23 @@ public sealed partial class Day03 {
 			int compartmentSize = input[i].Length / 2;
 			ReadOnlySpan<char> compartment1 = input[i][..compartmentSize].AsSpan();
 			ReadOnlySpan<char> compartment2 = input[i][compartmentSize..].AsSpan();
-			for (int j = 0; j < compartmentSize; j++) {
-				char item = compartment1[j];
+			foreach (char item in compartment1) {
 				if (compartment2.Contains(item)) {
 					prioritiesSum += GetPriority(item);
 					break;
 				}
 			}
 		}
+
 		return prioritiesSum;
 	}
 
-
 	private static int Solution2(string[] input) {
 		const int GroupSize = 3;
+
 		int prioritiesSum = 0;
 
 		for (int i = 0; i < input.Length; i+=GroupSize) {
-			int compartmentSize = input[i].Length / 2;
 			ReadOnlySpan<char> group1 = input[i].AsSpan();
 			ReadOnlySpan<char> group2 = input[i+1].AsSpan();
 			ReadOnlySpan<char> group3 = input[i+2].AsSpan();
@@ -47,8 +46,9 @@ public sealed partial class Day03 {
 				}
 			}
 		}
+
 		return prioritiesSum;
 	}
 
-	private static int GetPriority(char item) => (item < 'a') ? item - 64 + 26 : item - 96;
+	private static int GetPriority(char item) => (item < 'a') ? item - 'A' + 27 : item - 'a' + 1;
 }
