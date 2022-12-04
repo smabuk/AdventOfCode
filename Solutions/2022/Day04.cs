a254fc69d5a38ddc2e73c8bf5deb0d6bf433aa51
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-04 05:20:13 +0000

    2022 Day04 Solved

---
+++ Solutions/2022/Day04.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode.Solutions._2022;
+
+/// <summary>
+/// Day 04: Camp Cleanup
+/// https://adventofcode.com/2022/day/4
+/// </summary>
+[Description("Camp Cleanup")]
+public sealed partial class Day04 {
+
+	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+
+	record ElfPair(int Elf1Start, int Elf1End, int Elf2Start, int Elf2End);
+
+	private static int Solution1(string[] input) {
+		List<ElfPair> elfPairs = input.Select(i => ParseLine(i)).ToList();
+		int count = 0;
+		foreach (ElfPair elfPair in elfPairs) {
+			if (elfPair.Elf1Start >= elfPair.Elf2Start && elfPair.Elf1End <= elfPair.Elf2End
+				|| elfPair.Elf2Start >= elfPair.Elf1Start && elfPair.Elf2End <= elfPair.Elf1End) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	private static int Solution2(string[] input) {
+		List<ElfPair> elfPairs = input.Select(i => ParseLine(i)).ToList();
+		int count = 0;
+		foreach (ElfPair elfPair in elfPairs) {
+			for (int i = elfPair.Elf1Start; i <= elfPair.Elf1End; i++) {
+				if (i >= elfPair.Elf2Start && i <= elfPair.Elf2End) {
+					count++;
+					break;
+				}
+			}
+		}
+		return count;
+	}
+
+	private static ElfPair ParseLine(string input) {
+		Match match = InputRegEx().Match(input);
+		if (match.Success) {
+			return new(int.Parse(match.Groups["Elf1Start"].Value), int.Parse(match.Groups["Elf1End"].Value)
+					 , int.Parse(match.Groups["Elf2Start"].Value), int.Parse(match.Groups["Elf2End"].Value));
+		}
+		return null!;
+	}
+
+	[GeneratedRegex("""(?<Elf1Start>\d+)-(?<Elf1End>\d+),(?<Elf2Start>\d+)-(?<Elf2End>\d+)""")]
+	private static partial Regex InputRegEx();
+}
---
+++ Tests/2022/Tests_04.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.Tests._2022;
+
+public class Tests_04_Camp_Cleanup {
+	[Theory]
+	[InlineData("""
+		2-4,6-8
+		2-3,4-5
+		5-7,7-9
+		2-8,3-7
+		6-6,4-6
+		2-6,4-8
+		"""
+		, 2)]
+	public void Part1(string input, int expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2022, 4, 1, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData("""
+		2-4,6-8
+		2-3,4-5
+		5-7,7-9
+		2-8,3-7
+		6-6,4-6
+		2-6,4-8
+		"""
+		, 4)]
+	public void Part2(string input, int expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2022, 4, 2, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+}
