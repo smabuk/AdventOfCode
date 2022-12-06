ab58dfc185269db6e34798fc060150711c3ec799
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-06 05:11:44 +0000

    2022 Day06 Part1 Solved

---
+++ Solutions/2022/Day06.cs
@@ -0,0 +1,48 @@
+﻿namespace AdventOfCode.Solutions._2022;
+
+/// <summary>
+/// Day 06: Tuning Trouble
+/// https://adventofcode.com/2022/day/6
+/// </summary>
+[Description("Tuning Trouble")]
+public sealed partial class Day06 {
+
+	public static string Part1(string input, params object[]? _) => Solution1(input).ToString();
+	public static string Part2(string input, params object[]? _) => Solution2(input).ToString();
+
+	private record struct RecordType(string Name, int Value);
+
+	private static int Solution1(string input) {
+		//string inputLine = input[0];
+		//List<string> inputs = input.ToList();
+		//List<RecordType> instructions = input.Select(i => ParseLine(i)).ToList();
+		int startOfPacketMarker = 0;
+		for (int i = 0; i < input.Length - 4; i++) {
+			string marker = input[i..(i + 4)];
+			if (marker.ToCharArray().Distinct().Count() == 4) {
+				startOfPacketMarker = i + 4;
+				break;
+			}
+		}
+		return startOfPacketMarker;
+	}
+
+	private static string Solution2(string input) {
+		//string inputLine = input[0];
+		//List<string> inputs = input.ToList();
+		//List<RecordType> instructions = input.Select(i => ParseLine(i)).ToList();
+		return "** Solution not written yet **";
+	}
+
+	private static RecordType ParseLine(string input) {
+		//MatchCollection match = InputRegEx().Matches(input);
+		Match match = InputRegEx().Match(input);
+		if (match.Success) {
+			return new(match.Groups["opts"].Value, int.Parse(match.Groups["number"].Value));
+		}
+		return new();
+	}
+
+	[GeneratedRegex("""(?<opts>opt1|opt2|opt3) (?<number>[\+\-]\d+)""")]
+	private static partial Regex InputRegEx();
+}
---
+++ Tests/2022/Tests_06.cs
@@ -0,0 +1,24 @@
+﻿namespace AdventOfCode.Tests._2022;
+
+public class Tests_06_Tuning_Trouble {
+	[Theory]
+	[InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+	[InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+	[InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+	[InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+	[InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+	public void Part1(string input, int expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2022, 6, 1, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData("""
+		a
+		"""
+		, 9999)]
+	public void Part2(string input, int expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2022, 6, 2, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+}
