fef6082a9def51a6bab7f1acf83b9ad9b58dd5c1
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-08 07:06:08 +0000

    2022 Day08 refactored

--- Solutions/2022/Day08.cs
+++ Solutions/2022/Day08.cs
@@ -18,13 +18,38 @@ public sealed partial class Day08 {
 
 		for (int y = 0; y < gridHeight; y++) {
 			for (int x = 0; x < gridWidth; x++) {
-				if (IsVisible(x, y, grid)) {
+				if (IsVisible(x, y)) {
 					visibleTrees++;
 				}
 			}
 		}
 
 		return visibleTrees;
+
+		bool IsVisible(int x, int y) {
+			if (x == 0 || y == 0 || x == gridWidth - 1 || y == gridHeight - 1) {
+				return true;
+			}
+
+			int tree = grid[x, y];
+
+			return IsVisibleInDirection(-1, 0)
+				|| IsVisibleInDirection(1, 0)
+				|| IsVisibleInDirection(0, -1)
+				|| IsVisibleInDirection(0, 1);
+
+			bool IsVisibleInDirection(int dX, int dY) {
+				int x1 = x + dX, y1 = y + dY;
+				while (x1 != -1 && y1 != -1 && x1 != gridWidth && y1 != gridHeight) {
+					if (grid[x1, y1] >= tree) {
+						return false;
+					}
+					x1 += dX;
+					y1 += dY;
+				}
+				return true;
+			}
+		}
 	}
 
 	private static long Solution2(string[] input) {
@@ -35,109 +60,38 @@ public sealed partial class Day08 {
 
 		for (int y = 1; y < gridHeight - 1; y++) {
 			for (int x = 1; x < gridWidth - 1; x++) {
-				long scenicScore = ScenicScore(x, y, grid);
-				if (scenicScore > scenicScoreMax) {
-					scenicScoreMax = scenicScore;
-				}
+				scenicScoreMax = Math.Max(ScenicScore(x, y), scenicScoreMax);
 			}
 		}
 
 		return scenicScoreMax;
+
+		long ScenicScore(int x, int y) {
+			if (x == 0 || y == 0 || x == gridWidth - 1 || y == gridHeight - 1) {
+				return 0;
+			}
+
+			int tree = grid[x, y];
+
+			return ViewingDistance(-1, 0)
+				* ViewingDistance(1, 0)
+				* ViewingDistance(0, -1)
+				* ViewingDistance(0, 1);
+
+			int ViewingDistance(int dX, int dY) {
+				int viewingDistance = 0;
+				int x1 = x + dX, y1 = y + dY;
+				while (x1 != -1 && y1 != -1 && x1 != gridWidth && y1 != gridHeight) {
+					viewingDistance++;
+					if (grid[x1, y1] >= tree) {
+						break;
+					}
+					x1 += dX;
+					y1 += dY;
+				}
+				return viewingDistance;
+			}
+		}
 	}
 
-	static bool IsVisible(int x, int y, int[,] grid) {
-		int gridWidth = grid.GetLength(0);
-		int gridHeight = grid.GetLength(1);
-
-		if (x == 0 || y == 0 || x == gridWidth - 1 || y == gridHeight - 1) {
-			return true;
-		}
-
-		int tree = grid[x, y];
-
-		for (int x1 = x - 1; x1 >= 0; x1--) {
-			int currTree = grid[x1, y];
-			if (currTree >= tree) {
-				break;
-			} else {
-				if (x1 == 0) {
-					return true;
-				}
-			}
-		}
-		for (int x1 = x + 1; x1 <= gridWidth - 1; x1++) {
-			int currTree = grid[x1, y];
-			if (currTree >= tree) {
-				break;
-			} else {
-				if (x1 == gridWidth - 1) {
-					return true;
-				}
-			}
-		}
-		for (int y1 = y - 1; y1 >= 0; y1--) {
-			int currTree = grid[x, y1];
-			if (currTree >= tree) {
-				break;
-			} else {
-				if (y1 == 0) {
-					return true;
-				}
-			}
-		}
-		for (int y1 = y + 1; y1 <= gridHeight - 1; y1++) {
-			int currTree = grid[x, y1];
-			if (currTree >= tree) {
-				break;
-			} else {
-				if (y1 == gridHeight - 1) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	static long ScenicScore (int x, int y, int[,] grid) {
-		int gridWidth = grid.GetLength(0);
-		int gridHeight = grid.GetLength(1);
-		int s1 = 0;
-		int s2 = 0;
-		int s3 = 0;
-		int s4 = 0;
-
-		if (x == 0 || y == 0 || x == gridWidth - 1 || y == gridHeight - 1) {
-			return 0;
-		}
-		int tree = grid[x, y];
-		for (int x1 = x - 1; x1 >= 0; x1--) {
-			s1++;
-			int currTree = grid[x1, y];
-			if (currTree >= tree) {
-				break;
-			}
-		}
-		for (int x1 = x + 1; x1 <= gridWidth - 1; x1++) {
-			s2++;
-			int currTree = grid[x1, y];
-			if (currTree >= tree) {
-				break;
-			}
-		}
-		for (int y1 = y - 1; y1 >= 0; y1--) {
-			s3++;
-			int currTree = grid[x, y1];
-			if (currTree >= tree) {
-				break;
-			}
-		}
-		for (int y1 = y + 1; y1 <= gridHeight - 1; y1++) {
-			s4++;
-			int currTree = grid[x, y1];
-			if (currTree >= tree) {
-				break;
-			}
-		}
-		return s1 * s2 * s3 * s4;
-	}
 }
