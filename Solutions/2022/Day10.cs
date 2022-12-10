6f85e4063ceac9f3c5a346232b9c179c54a188e1
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-10 06:58:02 +0000

    2022 Day10 Part1 Solved

---
+++ Solutions/2022/Day10.cs
@@ -0,0 +1,94 @@
+﻿namespace AdventOfCode.Solutions._2022;
+
+/// <summary>
+/// Day 10: Cathode-Ray Tube
+/// https://adventofcode.com/2022/day/10
+/// </summary>
+[Description("Cathode-Ray Tube")]
+public sealed partial class Day10 {
+
+	[Init] public static void Init(string[] input, params object[]? _) => LoadInstructions(input);
+
+	public static string Part1(string[] input, params object[]? args) {
+		int cycleCheckStart = GetArgument<int>(args, argumentNumber: 1, 20);
+		int cycleCheckInterval = GetArgument<int>(args, argumentNumber: 2, 40);
+		return Solution1(cycleCheckStart, cycleCheckInterval).ToString();
+	}
+	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+
+
+	private static IEnumerable<Instruction> _instructions = Array.Empty<Instruction>();
+
+	private static void LoadInstructions(string[] input) {
+		_instructions = input.Select(i => Instruction.Parse(i));
+	}
+
+	private static int Solution1(int cycleCheckStart, int cycleCheckInterval) {
+		List<int> signalStrengths = new Computer()
+			.ExecuteProgram(_instructions.ToList(), cycleCheckStart, cycleCheckInterval)
+			.ToList();
+
+		return signalStrengths.Sum();
+	}
+
+	private static string Solution2(string[] input) {
+		return "** Solution not written yet **";
+	}
+
+
+	private record Instruction(string Command, int Value) {
+		public static Instruction Parse(string input) {
+			string[] tokens = input.Split(' ');
+			int value = tokens.Length > 1 ? tokens[1].AsInt() : 0;
+			return new(tokens[0],value);
+		}
+	};
+
+
+	private record class Computer {
+		public Dictionary<string, int> registers = new();
+		public int CycleCount { get; set; } = 0;
+
+		public Computer() {
+			registers["x"] = 1;
+			CycleCount = 1;
+		}
+
+		public IEnumerable<int> ExecuteProgram(List<Instruction> Instructions, int cycleCheckStart, int cycleCheckInterval) {
+			int currentInstructionNo = 0;
+			Instruction currentInstruction = Instructions[currentInstructionNo];
+
+			int countdown = int.MinValue;
+			while (currentInstructionNo <= Instructions.Count) {
+
+				if ((CycleCount + cycleCheckStart) % cycleCheckInterval == 0 && CycleCount > 0) {
+					yield return registers["x"] * CycleCount;
+				}
+
+				if (currentInstructionNo < Instructions.Count) {
+					currentInstruction = Instructions[currentInstructionNo];
+				}
+
+				switch (currentInstruction.Command) {
+					case "noop":
+						currentInstructionNo++;
+						break;
+					case "addx": // Increment
+						if (countdown < 0) {
+							countdown = 1;
+						} else if (countdown == 0) {
+							registers["x"] += currentInstruction.Value;
+							currentInstructionNo++;
+						}
+						countdown--;
+						break;
+					default:
+						break;
+				}
+
+				CycleCount++;
+			}
+
+		}
+	}
+}
---
+++ Tests/2022/Tests_10.cs
@@ -0,0 +1,181 @@
+﻿namespace AdventOfCode.Tests._2022;
+
+public class Tests_10_Cathode_Ray_Tube {
+	[Theory]
+	[InlineData("""
+		addx 15
+		addx -11
+		addx 6
+		addx -3
+		addx 5
+		addx -1
+		addx -8
+		addx 13
+		addx 4
+		noop
+		addx -1
+		addx 5
+		addx -1
+		addx 5
+		addx -1
+		addx 5
+		addx -1
+		addx 5
+		addx -1
+		addx -35
+		addx 1
+		addx 24
+		addx -19
+		addx 1
+		addx 16
+		addx -11
+		noop
+		noop
+		addx 21
+		addx -15
+		noop
+		noop
+		addx -3
+		addx 9
+		addx 1
+		addx -3
+		addx 8
+		addx 1
+		addx 5
+		noop
+		noop
+		noop
+		noop
+		noop
+		addx -36
+		noop
+		addx 1
+		addx 7
+		noop
+		noop
+		noop
+		addx 2
+		addx 6
+		noop
+		noop
+		noop
+		noop
+		noop
+		addx 1
+		noop
+		noop
+		addx 7
+		addx 1
+		noop
+		addx -13
+		addx 13
+		addx 7
+		noop
+		addx 1
+		addx -33
+		noop
+		noop
+		noop
+		addx 2
+		noop
+		noop
+		noop
+		addx 8
+		noop
+		addx -1
+		addx 2
+		addx 1
+		noop
+		addx 17
+		addx -9
+		addx 1
+		addx 1
+		addx -3
+		addx 11
+		noop
+		noop
+		addx 1
+		noop
+		addx 1
+		noop
+		noop
+		addx -13
+		addx -19
+		addx 1
+		addx 3
+		addx 26
+		addx -30
+		addx 12
+		addx -1
+		addx 3
+		addx 1
+		noop
+		noop
+		noop
+		addx -9
+		addx 18
+		addx 1
+		addx 2
+		noop
+		noop
+		addx 9
+		noop
+		noop
+		noop
+		addx -1
+		addx 2
+		addx -37
+		addx 1
+		addx 3
+		noop
+		addx 15
+		addx -21
+		addx 22
+		addx -6
+		addx 1
+		noop
+		addx 2
+		addx 1
+		noop
+		addx -10
+		noop
+		noop
+		addx 20
+		addx 1
+		addx 2
+		addx 2
+		addx -6
+		addx -11
+		noop
+		noop
+		noop
+		"""
+		, 20, 40, 13140)]
+	[InlineData("""
+		noop
+		addx 3
+		addx -5
+		"""
+		, 1, 1, 29)]
+	public void Part1(string input, int cycleCheckStart, int cycleCheckInterval, int expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2022, 10, 1, input, cycleCheckStart, cycleCheckInterval), out int actual);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData("""
+		R 5
+		U 8
+		L 8
+		D 3
+		R 17
+		D 10
+		L 25
+		U 20
+		"""
+		, 36)]
+	public void Part2(string input, int expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2022, 10, 2, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+}
