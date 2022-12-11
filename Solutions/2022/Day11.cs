1fe0886311b745ea0e303b79bee4debc15a19010
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-11 06:44:04 +0000

    2022 Day11 Part1 Solved

---
+++ Solutions/2022/Day11.cs
@@ -0,0 +1,89 @@
+﻿namespace AdventOfCode.Solutions._2022;
+
+/// <summary>
+/// Day 11: Monkey in the Middle
+/// https://adventofcode.com/2022/day/11
+/// </summary>
+[Description("Monkey in the Middle")]
+public sealed partial class Day11 {
+
+	// [Init] public static void Init(string[] input, params object[]? _) => Load(input);
+	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+
+	private record Monkey(int Name, List<Item> Items, Operation Operation, int DivisibleBy, int TrueMonkeyName, int FalseMonkeyName) {
+		public int InspectionCount { get; set; } = 0;
+
+		public IEnumerable<(Item, int)>  Inspect() {
+			foreach (Item item in Items) {
+				InspectionCount++;
+				long worryLevel = Operation.Op switch {
+					Operation.Operand.Add => item.WorryLevel + Operation.Value,
+					Operation.Operand.Multiply when Operation.Value is Operation.SELF => item.WorryLevel * item.WorryLevel,
+					Operation.Operand.Multiply => item.WorryLevel * Operation.Value,
+					_ => throw new NotImplementedException(),
+				};
+				worryLevel = worryLevel / 3;
+				if (worryLevel % DivisibleBy == 0) {
+					yield return (item with { WorryLevel = worryLevel}, TrueMonkeyName);
+				} else {
+					yield return (item with { WorryLevel = worryLevel}, FalseMonkeyName);
+				}
+			}
+			Items.Clear();
+		}
+
+		public void ReceiveItem(Item item) => Items.Add(item);
+
+		static public IEnumerable<Monkey> Parse(string[] input) {
+			const int LinesPerMonkey = 7;
+			int noOfMonkeys = (input.Length + 1) / LinesPerMonkey;
+			for (int i = 0; i < noOfMonkeys; i++) {
+				int startLine = i * LinesPerMonkey;
+				int name = input[startLine][7..^1].AsInt();
+				List<Item> items = input[startLine + 1][17..]
+					.Split(", ")
+					.Select(x => new Item(long.Parse(x)))
+					.ToList();
+				Operation operation = 
+					new(input[startLine + 2][23] == '+' ? Operation.Operand.Add : Operation.Operand.Multiply,
+						input[startLine + 2][25..].AsInt());
+				int test = input[startLine + 3][21..].AsInt();
+				int trueName = input[startLine + 4][29..].AsInt();
+				int falseName = input[startLine + 5][29..].AsInt();
+				yield return new(name, items, operation, test, trueName, falseName);
+			}
+			yield break;
+		}
+	};
+	private record struct Item(long WorryLevel);
+	private record struct Operation(Operation.Operand Op, int Value) {
+		public const int SELF = 0;
+		public enum Operand { Add, Multiply } 
+	};
+
+	private static long Solution1(string[] input, int noOfRounds = 20) {
+		Dictionary<int, Monkey> monkeys = 
+			Monkey.Parse(input)
+			.ToDictionary(m => m.Name);
+
+		for (int i = 0; i < noOfRounds; i++) {
+			foreach (Monkey monkey in monkeys.Values) {
+				foreach ((Item item, int toMonkey) in monkey.Inspect()) {
+					monkeys[toMonkey].ReceiveItem(item);
+				}
+			}
+		}
+		long monkeyBusiness = 
+			monkeys.Values
+			.Select(m => m.InspectionCount)
+			.OrderByDescending(m => m)
+			.Take(2)
+			.Aggregate((total, next) => total * next);
+		return monkeyBusiness;
+	}
+
+	private static string Solution2(string[] input) {
+		return "** Solution not written yet **";
+	}
+}
---
+++ Tests/2022/Tests_11.cs
@@ -0,0 +1,49 @@
+﻿namespace AdventOfCode.Tests._2022;
+
+public class Tests_11_Monkey_in_the_Middle {
+	[Theory]
+	[InlineData("""
+		Monkey 0:
+		  Starting items: 79, 98
+		  Operation: new = old * 19
+		  Test: divisible by 23
+		    If true: throw to monkey 2
+		    If false: throw to monkey 3
+
+		Monkey 1:
+		  Starting items: 54, 65, 75, 74
+		  Operation: new = old + 6
+		  Test: divisible by 19
+		    If true: throw to monkey 2
+		    If false: throw to monkey 0
+
+		Monkey 2:
+		  Starting items: 79, 60, 97
+		  Operation: new = old * old
+		  Test: divisible by 13
+		    If true: throw to monkey 1
+		    If false: throw to monkey 3
+
+		Monkey 3:
+		  Starting items: 74
+		  Operation: new = old + 3
+		  Test: divisible by 17
+		    If true: throw to monkey 0
+		    If false: throw to monkey 1
+		"""
+		, 10605)]
+	public void Part1(string input, int expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2022, 11, 1, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData("""
+		a
+		"""
+		, 9999)]
+	public void Part2(string input, int expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2022, 11, 2, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+}
