5dfe15e4831ab3021b96923d7847734230a7c30f
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-12 08:07:00 +0000

    2022 Day12 Solved

--- Solutions/2022/Day12.cs
+++ Solutions/2022/Day12.cs
@@ -10,60 +10,67 @@ namespace AdventOfCode.Solutions._2022;
 public sealed partial class Day12 {
 
 	[Init]
-	public static void Init(string[] input, params object[]? _) => LoadInstructions(input);
+	public static void    Init(string[] input, params object[]? _) => LoadElevationMap(input);
 	public static string Part1(string[] input, params object[]? _) => Solution1().ToString();
-	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+	public static string Part2(string[] input, params object[]? _) => Solution2().ToString();
 
-	private static int[,] _elevationMap;
+	private static int[,] _elevationMap = default!;
+	private static Dictionary<Point, int> _costs = new();
 	private static Point _startPoint;
 	private static Point _endPoint;
-	private static Dictionary<Point, int> _costs = new();
 
-	private static void LoadInstructions(string[] input) {
-		_elevationMap = input.SelectMany(i => i).Select(x => x - 'a').To2dArray(input[0].Length);
-		foreach (var c in _elevationMap.Walk2dArrayWithValues()) {
-			if (c.Value == -14) {
-				_startPoint = new(c.X, c.Y);
-				_elevationMap[c.X, c.Y] = 'a' - 'a';
+	private static void LoadElevationMap(string[] input) {
+		_elevationMap = input
+			.SelectMany(i => i)
+			.Select(x => (int)x)
+			.To2dArray(input[0].Length);
+
+		foreach (Cell<int> c in _elevationMap.Walk2dArrayWithValues()) {
+			if (c.Value == 'S') {
+				_startPoint = c.Index;
+				_elevationMap[c.X, c.Y] = 'a';
 			}
-			if (c.Value == -28) {
-				_endPoint = new(c.X, c.Y);
-				_elevationMap[c.X, c.Y] = 'z' - 'a';
+			if (c.Value == 'E') {
+				_endPoint = c.Index;
+				_elevationMap[c.X, c.Y] = 'z';
 			}
 		}
-		_costs = new() { { _startPoint, 0 } };
-		ComputeCost(_startPoint);
 
-		void ComputeCost(Point p) {
-			int currentCost = _costs[p];
+		_costs = new() {
+			{ _endPoint, 0 }
+		};
 
-			void ComputeNeighbor(Point neighbour) {
-				if (neighbour.X < 0 || neighbour.X >= _elevationMap.NoOfColumns() || neighbour.Y < 0 || neighbour.Y >= _elevationMap.NoOfRows())
-					return;
+		CalculateCost(_endPoint);
+	}
 
-				if (_elevationMap[neighbour.X, neighbour.Y] <= _elevationMap[p.X, p.Y] + 1) {
-					if (!_costs.TryGetValue((neighbour), out int currentNeighborCost) || currentNeighborCost > currentCost + 1) {
-						_costs[neighbour] = currentCost + 1;
-						ComputeCost(neighbour);
-					}
+	private static int Solution1() => _costs[_startPoint];
+
+
+	private static int Solution2() =>
+		_elevationMap
+			.Walk2dArrayWithValues()
+			.Where(x => x.Value == 'a' && _costs.ContainsKey(x.Index))
+			.Select(x => _costs[x.Index] -_costs[_endPoint])
+			.Min();
+
+
+	private static void CalculateCost(Point p) {
+		int currentCost = _costs[p];
+
+		_elevationMap
+			.GetAdjacentCells(p)
+			.ToList()
+			.ForEach(adj => CalculateAdjacent(new(adj.x, adj.y)));
+
+		void CalculateAdjacent(Point adjacent) {
+			// Is this step not too steep - we're going backwards so we can work with parts 1 and 2
+			if (_elevationMap[p.X, p.Y] - 1 <= _elevationMap[adjacent.X, adjacent.Y]) {
+				if (!_costs.TryGetValue(adjacent, out int adjacentCost) || adjacentCost > currentCost + 1) {
+					_costs[adjacent] = currentCost + 1;
+					CalculateCost(adjacent);
 				}
 			}
-
-			ComputeNeighbor(p + (-1, 0));
-			ComputeNeighbor(p + (+1, 0));
-			ComputeNeighbor(p + (0, -1));
-			ComputeNeighbor(p + (0,+1));
 		}
-	}
-
-	private static int Solution1() {
-		int noOfSteps =  _costs[_endPoint];
-		return noOfSteps;
-	}
-
-
-	private static int Solution2(string[] input) {
-		return 0;
 	}
 
 }
