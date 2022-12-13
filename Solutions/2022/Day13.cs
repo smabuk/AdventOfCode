a42ae76ca0fdfadaba831941f55d07ded30c1d04
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-13 07:34:33 +0000

    2022 Day13 Tests work, but main input is too high

---
+++ Solutions/2022/Day13.cs
@@ -0,0 +1,109 @@
+﻿using System.Text.Json;
+
+namespace AdventOfCode.Solutions._2022;
+
+/// <summary>
+/// Day 13: Distress Signal
+/// https://adventofcode.com/2022/day/13
+/// </summary>
+[Description("Distress Signal")]
+public sealed partial class Day13 {
+
+	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+
+	private static int Solution1(string[] input) {
+		List<Pair> pairs = new();
+		List<int> correctPairs = new();
+
+		for (int i = 0; i < input.Length; i += 3) {
+			pairs.Add(new (Packet.Parse(input[i]), Packet.Parse(input[i + 1])));
+		}
+
+		for (int i = 0; i < pairs.Count; i++) {
+			if (pairs[i].Left.CompareItem(pairs[i].Right) ?? false) {
+				correctPairs.Add(i);
+			}
+		}
+
+		return correctPairs.Sum(x => x + 1);
+	}
+
+	private static string Solution2(string[] input) {
+		return "** Solution not written yet **";
+	}
+
+	private record Pair(Packet Left, Packet Right) {
+		public static Pair Parse(string left, string right)
+			=>  new (Packet.Parse(left), Packet.Parse(right));
+	};
+
+	private abstract record Packet {
+		public static Packet Parse(string value)
+			=> Parse(JsonSerializer.Deserialize<JsonElement>(value));
+
+		public static Packet Parse(JsonElement el) {
+			Packet value = el.ValueKind switch {
+				JsonValueKind.Number => new NumberPacket(el.GetInt32()),
+				JsonValueKind.Array => new ListPacket(el.EnumerateArray().Select(Parse).ToList()),
+				_ => throw new NotImplementedException(),
+			};
+			return value; 
+		}
+
+		// null represents continue checking
+		public bool? CompareItem(Packet right) {
+			Packet left = this;
+			
+			if (left is not null && right is null) {
+				return false;
+			}
+
+			if (left is null && right is not null) {
+				return true;
+			}
+
+			if (left is NumberPacket ln && right is NumberPacket rn) {
+				if (ln.Value < rn.Value) {
+					return true;
+				} else if (ln.Value > rn.Value) {
+					return false;
+				} else {
+					return null;
+				}
+			} 
+
+			if (left is ListPacket && right is NumberPacket) {
+				return left.CompareItem(new ListPacket(new() { right }));
+			}
+
+			if (left is NumberPacket && right is ListPacket) {
+				return new ListPacket(new() { left }).CompareItem(right);
+			}
+
+			if (left is ListPacket lp && right is ListPacket rp) {
+				for (int i = 0; i < lp.Packets.Count; i++) {
+					if (rp.Packets.Count <= i) {
+						return false;
+					}
+					bool? res = lp.Packets[i].CompareItem(rp.Packets[i]);
+					if (res is not null) {
+						return res;
+					};
+				} 
+				return true;
+			}
+			return null;
+		}
+	};
+
+	private record NumberPacket(int Value) : Packet {
+		public override string ToString() => $"{Value}";
+	}
+
+	private record ListPacket(List<Packet> Packets) : Packet {
+		public override string ToString() => $"[{String.Join(",", Packets)}]";
+	};
+
+
+}
---
+++ Tests/2022/Tests_13.cs
@@ -0,0 +1,77 @@
+﻿namespace AdventOfCode.Tests._2022;
+
+public class Tests_13_Distress_Signal {
+	[Theory]
+	[InlineData("""
+		[1,1,3,1,1]
+		[1,1,5,1,1]
+
+		[[1],[2,3,4]]
+		[[1],4]
+
+		[9]
+		[[8,7,6]]
+
+		[[4,4],4,4]
+		[[4,4],4,4,4]
+
+		[7,7,7,7]
+		[7,7,7]
+
+		[]
+		[3]
+
+		[[[]]]
+		[[]]
+
+		[1,[2,[3,[4,[5,6,7]]]],8,9]
+		[1,[2,[3,[4,[5,6,0]]]],8,9]
+		"""
+		, 13)]
+	[InlineData("""
+		[[[]]]
+		[[]]
+		"""
+		, 0)]
+	[InlineData("""
+		[[]]
+		[[[]]]
+		"""
+		, 1)]
+	public void Part1(string input, int expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2022, 13, 1, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData("""
+		[1,1,3,1,1]
+		[1,1,5,1,1]
+
+		[[1],[2,3,4]]
+		[[1],4]
+
+		[9]
+		[[8,7,6]]
+
+		[[4,4],4,4]
+		[[4,4],4,4,4]
+
+		[7,7,7,7]
+		[7,7,7]
+
+		[]
+		[3]
+
+		[[[]]]
+		[[]]
+
+		[1,[2,[3,[4,[5,6,7]]]],8,9]
+		[1,[2,[3,[4,[5,6,0]]]],8,9]
+		"""
+		, 1, 9999)]
+	public void Part2(string input, int noOfRounds, long expected) {
+		_ = long.TryParse(SolutionRouter.SolveProblem(2022, 13, 2, input, noOfRounds), out long actual);
+		Assert.Equal(expected, actual);
+	}
+}
