7c302afd7f63cbc4dffde198730b78732986195e
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-15 07:47:20 +0000

    2022 Day15 Part 2 Naive version
    Works with tests

--- Solutions/2022/Day15.cs
+++ Solutions/2022/Day15.cs
@@ -8,12 +8,15 @@
 public sealed partial class Day15 {
 
 	[Init]
-	public static    void Init(string[] input, params object[]? _) => LoadPairs(input);
+	public static void    Init(string[] input, params object[]? _) => LoadPairs(input);
 	public static string Part1(string[] input, params object[]? args) {
 		int rowToSearch = GetArgument<int>(args, argumentNumber: 1, 2_000_000);
 		return Solution1(input, rowToSearch).ToString();
 	}
-	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+	public static string Part2(string[] input, params object[]? args) {
+		int max = GetArgument<int>(args, argumentNumber: 1, 4_000_000);
+		return Solution2(input, max).ToString();
+	}
 
 	private static IEnumerable<SensorBeaconPair> _pairs = Array.Empty<SensorBeaconPair>();
 
@@ -23,7 +26,7 @@ public sealed partial class Day15 {
 
 	private static int Solution1(string[] input, int rowToSearch) {
 		List<Point> beacons = _pairs.Select(p => p.Beacon).ToList();
-		HashSet<Point> found= new();
+		HashSet<Point> found = new();
 
 		foreach (SensorBeaconPair sbp in _pairs) {
 			if (sbp.Sensor.Y - sbp.ManhattanDistance <= rowToSearch && sbp.Sensor.Y + sbp.ManhattanDistance >= rowToSearch) {
@@ -43,11 +46,33 @@ public sealed partial class Day15 {
 		return found.Count;
 	}
 
-	private static string Solution2(string[] input) {
-		//string inputLine = input[0];
-		//List<string> inputs = input.ToList();
-		//List<SensorBeaconPair> instructions = input.Select(ParseLine).ToList();
-		return "** Solution not written yet **";
+	private static long Solution2(string[] input, int max) {
+		List<Point> beacons = _pairs.Select(p => p.Beacon).ToList();
+		HashSet<Point> found = new();
+		for (int y = 0; y <= max; y++) {
+			char[] caveRow = new char[max + 1];
+			//char[,] cave = new char[max + 1, max + 1];
+			for (int x = 0; x < caveRow.Length; x++) {
+				caveRow[x] = ' ';
+			}
+			foreach (SensorBeaconPair sbp in _pairs) {
+				for (int x = int.Clamp(sbp.Sensor.X - sbp.ManhattanDistance, 0, max); x <= int.Clamp(sbp.Sensor.X + sbp.ManhattanDistance, 0, max); x++) {
+					if (ManhattanDistance(sbp.Sensor, new(x, y)) <= sbp.ManhattanDistance ) {
+						caveRow[x] = '#';
+					}
+				}
+			}
+			for (int x = 0; x < caveRow.Length; x++) {
+				if (caveRow[x] == ' ') {
+					return (x * 4_000_000) + y;
+				}
+			}
+			if (y % 10_000 == 0) {
+				Console.WriteLine($"Checked up to: {y}");
+			}
+		}
+
+		return 0;
 	}
 
 	private static int ManhattanDistance(Point from, Point to) => Math.Abs(from.X - to.X) + Math.Abs(from.Y - to.Y);
