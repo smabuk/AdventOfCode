81bb016a99861b4088f767056cede8d971baab54
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-15 05:21:31 +0000

    2022 Day15 WIP
    Tests work, but answer is too low

---
+++ Solutions/2022/Day15.cs
@@ -0,0 +1,75 @@
+﻿namespace AdventOfCode.Solutions._2022;
+
+/// <summary>
+/// Day 15: Beacon Exclusion Zone
+/// https://adventofcode.com/2022/day/15
+/// </summary>
+[Description("Beacon Exclusion Zone")]
+public sealed partial class Day15 {
+
+	[Init]
+	public static    void Init(string[] input, params object[]? _) => LoadPairs(input);
+	public static string Part1(string[] input, params object[]? args) {
+		int rowToSearch = GetArgument<int>(args, argumentNumber: 1, 2_000_000);
+		return Solution1(input, rowToSearch).ToString();
+	}
+	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+
+	private static IEnumerable<SensorBeaconPair> _pairs = Array.Empty<SensorBeaconPair>();
+
+	private static void LoadPairs(string[] input) {
+		_pairs = input.Select(SensorBeaconPair.Parse);
+	}
+
+	private static int Solution1(string[] input, int rowToSearch) {
+		int minX = _pairs.Select(p => p.Sensor.X).Union(_pairs.Select(p => p.Beacon.X)).Min();
+		int maxX = _pairs.Select(p => p.Sensor.X).Union(_pairs.Select(p => p.Beacon.X)).Max();
+		
+		List<Point> beacons = _pairs.Select(p => p.Beacon).ToList();
+		HashSet<Point> found= new();
+
+		foreach (SensorBeaconPair sbp in _pairs) {
+			for (int x = minX; x <= maxX; x++) {
+				Point check = new(x, rowToSearch);
+				if (ManhattanDistance(sbp.Sensor, check) <= sbp.ManhattanDistance) {
+					if (!beacons.Contains(check)) {
+						found.Add(check);
+					}
+				}
+			}
+		}
+
+		// 4403084 Too Low and too slow
+		// 4403085 Too Low about 5s
+		return found.Count;
+	}
+
+	private static string Solution2(string[] input) {
+		//string inputLine = input[0];
+		//List<string> inputs = input.ToList();
+		//List<SensorBeaconPair> instructions = input.Select(ParseLine).ToList();
+		return "** Solution not written yet **";
+	}
+
+	private static long ManhattanDistance(Point from, Point to) => Math.Abs(from.X - to.X) + Math.Abs(from.Y - to.Y);
+
+	private partial record SensorBeaconPair(Point Sensor, Point Beacon) : IParsable<SensorBeaconPair> {
+		public long ManhattanDistance => ManhattanDistance(Sensor, Beacon);
+
+		public static SensorBeaconPair Parse(string s) {
+			Match match = SensorBeaconPairRegEx().Match(s);
+			if (match.Success) {
+				return new(new(match.Groups["sx"].Value.AsInt(), match.Groups["sy"].Value.AsInt()),
+					new(match.Groups["bx"].Value.AsInt(), match.Groups["by"].Value.AsInt()));
+			}
+			return null!;
+		}
+
+		[GeneratedRegex("""Sensor at x=(?<sx>[\+\-]*\d+), y=(?<sy>[\+\-]*\d+): closest beacon is at x=(?<bx>[\+\-]*\d+), y=(?<by>[\+\-]*\d+)""")]
+		private static partial Regex SensorBeaconPairRegEx();
+
+		public static SensorBeaconPair Parse(string s, IFormatProvider? provider) => throw new NotImplementedException();
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out SensorBeaconPair result) => throw new NotImplementedException();
+	}
+
+}
---
+++ Tests/2022/Tests_15.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode.Tests._2022;
+
+public class Tests_15_Beacon_Exclusion_Zone {
+	[Theory]
+	[InlineData("""
+		Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+		Sensor at x=9, y=16: closest beacon is at x=10, y=16
+		Sensor at x=13, y=2: closest beacon is at x=15, y=3
+		Sensor at x=12, y=14: closest beacon is at x=10, y=16
+		Sensor at x=10, y=20: closest beacon is at x=10, y=16
+		Sensor at x=14, y=17: closest beacon is at x=10, y=16
+		Sensor at x=8, y=7: closest beacon is at x=2, y=10
+		Sensor at x=2, y=0: closest beacon is at x=2, y=10
+		Sensor at x=0, y=11: closest beacon is at x=2, y=10
+		Sensor at x=20, y=14: closest beacon is at x=25, y=17
+		Sensor at x=17, y=20: closest beacon is at x=21, y=22
+		Sensor at x=16, y=7: closest beacon is at x=15, y=3
+		Sensor at x=14, y=3: closest beacon is at x=15, y=3
+		Sensor at x=20, y=1: closest beacon is at x=15, y=3
+		"""
+		, 10, 26)]
+	public void Part1(string input, int rowToSearch, int expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2022, 15, 1, input, rowToSearch), out int actual);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData("""
+		a
+		"""
+		, 1, 9999)]
+	public void Part2(string input, int noOfRounds, long expected) {
+		_ = long.TryParse(SolutionRouter.SolveProblem(2022, 15, 2, input, noOfRounds), out long actual);
+		Assert.Equal(expected, actual);
+	}
+}
