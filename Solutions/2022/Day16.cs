df36e4d97dbeb4b7cf1fb597cb064d152d6a9656
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-17 15:01:55 +0000

    Skipped Day16

---
+++ Solutions/2022/Day16.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode.Solutions._2022;
+
+/// <summary>
+/// Day 16: Proboscidea Volcanium
+/// https://adventofcode.com/2022/day/16
+/// </summary>
+[Description("Proboscidea Volcanium")]
+public sealed partial class Day16 {
+
+	[Init]
+	public static    void Init(string[] input, params object[]? _) => LoadInstructions(input);
+	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+
+	private static IEnumerable<Instruction> _instructions = Array.Empty<Instruction>();
+
+	private static void LoadInstructions(string[] input) {
+		_instructions = input.Select(Instruction.Parse);
+	}
+
+	private static string Solution1(string[] input) {
+		//string inputLine = input[0];
+		//List<string> inputs = input.ToList();
+		List<Instruction> instructions = input.Select(ParseLine).ToList();
+		return "** Solution not written yet **";
+	}
+
+	private static string Solution2(string[] input) {
+		//string inputLine = input[0];
+		//List<string> inputs = input.ToList();
+		List<Instruction> instructions = input.Select(ParseLine).ToList();
+		return "** Solution not written yet **";
+	}
+
+	private record Instruction(string Name, int Value) : IParsable<Instruction> {
+		public static Instruction Parse(string s) => throw new NotImplementedException();
+		public static Instruction Parse(string s, IFormatProvider? provider) => throw new NotImplementedException();
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Instruction result) => throw new NotImplementedException();
+	}
+
+	private static Instruction ParseLine(string input) {
+		//MatchCollection match = InputRegEx().Matches(input);
+		Match match = InputRegEx().Match(input);
+		if (match.Success) {
+			return new(match.Groups["opts"].Value, int.Parse(match.Groups["number"].Value));
+		}
+		return null!;
+	}
+
+	[GeneratedRegex("""(?<opts>opt1|opt2|opt3) (?<number>[\+\-]\d+)""")]
+	private static partial Regex InputRegEx();
+}
---
+++ Tests/2022/Tests_16.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode.Tests._2022;
+
+public class Tests_16_Proboscidea_Volcanium {
+	[Theory]
+	[InlineData("""
+		Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+		Valve BB has flow rate=13; tunnels lead to valves CC, AA
+		Valve CC has flow rate=2; tunnels lead to valves DD, BB
+		Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+		Valve EE has flow rate=3; tunnels lead to valves FF, DD
+		Valve FF has flow rate=0; tunnels lead to valves EE, GG
+		Valve GG has flow rate=0; tunnels lead to valves FF, HH
+		Valve HH has flow rate=22; tunnel leads to valve GG
+		Valve II has flow rate=0; tunnels lead to valves AA, JJ
+		Valve JJ has flow rate=21; tunnel leads to valve II
+		"""
+		, 1651)]
+	public void Part1(string input, int expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2022, 16, 1, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData("""
+		Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+		Valve BB has flow rate=13; tunnels lead to valves CC, AA
+		Valve CC has flow rate=2; tunnels lead to valves DD, BB
+		Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+		Valve EE has flow rate=3; tunnels lead to valves FF, DD
+		Valve FF has flow rate=0; tunnels lead to valves EE, GG
+		Valve GG has flow rate=0; tunnels lead to valves FF, HH
+		Valve HH has flow rate=22; tunnel leads to valve GG
+		Valve II has flow rate=0; tunnels lead to valves AA, JJ
+		Valve JJ has flow rate=21; tunnel leads to valve II
+		"""
+		, 9999)]
+	public void Part2(string input, long expected) {
+		_ = long.TryParse(SolutionRouter.SolveProblem(2022, 16, 2, input), out long actual);
+		Assert.Equal(expected, actual);
+	}
+}
