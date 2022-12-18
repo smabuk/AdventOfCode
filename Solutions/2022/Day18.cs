bb4d7f7180ce730ab840a161586af612ee90bfe7
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-18 08:30:30 +0000

    2022 Day18 Part1 Solved

---
+++ Solutions/2022/Day18.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode.Solutions._2022;
+
+/// <summary>
+/// Day 18: Boiling Boulders
+/// https://adventofcode.com/2022/day/18
+/// </summary>
+[Description("Boiling Boulders")]
+public sealed partial class Day18 {
+
+	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+
+	private static int Solution1(string[] input) {
+		HashSet<Point3d> cubes = input.Select(ParseLine).ToHashSet();
+		int noOfExposedFaces = cubes.Count * 6;
+
+		foreach (Point3d cube in cubes) {
+			if (cubes.Contains(cube with { X = cube.X - 1 })) {
+				noOfExposedFaces--;
+			}
+			if (cubes.Contains(cube with { X = cube.X + 1 })) {
+				noOfExposedFaces--;
+			}
+			if (cubes.Contains(cube with { Y = cube.Y - 1 })) {
+				noOfExposedFaces--;
+			}
+			if (cubes.Contains(cube with { Y = cube.Y + 1 })) {
+				noOfExposedFaces--;
+			}
+			if (cubes.Contains(cube with { Z = cube.Z - 1 })) {
+				noOfExposedFaces--;
+			}
+			if (cubes.Contains(cube with { Z = cube.Z + 1 })) {
+				noOfExposedFaces--;
+			}
+		}
+
+		return noOfExposedFaces;
+	}
+
+	private static int Solution2(string[] input) {
+		HashSet<Point3d> cubes = input.Select(ParseLine).ToHashSet();
+		int noOfExposedFaces = cubes.Count * 6;
+
+		return noOfExposedFaces;
+	}
+
+	private static Point3d ParseLine(string input) {
+		List<int> numbers = input.Split(',').AsInts().ToList();
+		return new Point3d(numbers[0], numbers[1], numbers[2]);
+	}
+}
---
+++ Tests/2022/Tests_18.cs
@@ -0,0 +1,47 @@
+﻿namespace AdventOfCode.Tests._2022;
+
+public class Tests_18_Boiling_Boulders {
+	[Theory]
+	[InlineData("""
+		2,2,2
+		1,2,2
+		3,2,2
+		2,1,2
+		2,3,2
+		2,2,1
+		2,2,3
+		2,2,4
+		2,2,6
+		1,2,5
+		3,2,5
+		2,1,5
+		2,3,5
+		"""
+		, 64)]
+	public void Part1(string input, int expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2022, 18, 1, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData("""
+		2,2,2
+		1,2,2
+		3,2,2
+		2,1,2
+		2,3,2
+		2,2,1
+		2,2,3
+		2,2,4
+		2,2,6
+		1,2,5
+		3,2,5
+		2,1,5
+		2,3,5
+		"""
+		, 9999)]
+	public void Part2(string input, long expected) {
+		_ = long.TryParse(SolutionRouter.SolveProblem(2022, 18, 2, input), out long actual);
+		Assert.Equal(expected, actual);
+	}
+}
