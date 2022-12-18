368cd233917397826afe71504cf97d3a99636d49
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-18 13:36:14 +0000

    2022 Day18 simplified

--- Solutions/2022/Day18.cs
+++ Solutions/2022/Day18.cs
@@ -1,7 +1,4 @@
-﻿using System.Data;
-using System.Linq;
-
-namespace AdventOfCode.Solutions._2022;
+﻿namespace AdventOfCode.Solutions._2022;
 
 /// <summary>
 /// Day 18: Boiling Boulders
@@ -15,31 +12,16 @@ public sealed partial class Day18 {
 
 	private static int Solution1(string[] input) {
 		HashSet<Point3d> cubes = input.Select(ParseLine).ToHashSet();
-
-		return (cubes.Count * 6)
-				- cubes.SelectMany(Adjacent)
-					.Select(cubes.Contains)
-					.Count(exists => exists == true);
+		return NoOfExposedFaces(cubes);
 	}
 
 	private static int Solution2(string[] input) {
 		HashSet<Point3d> cubes = input.Select(ParseLine).ToHashSet();
-		HashSet<Point3d> processedCubes = new();
-		int noOfExposedFaces = cubes.Count * 6;
+		int noOfExposedFaces = NoOfExposedFaces(cubes);
 
-		foreach (Point3d cube in cubes) {
-			foreach (Point3d adjacentCube in Adjacent(cube)) {
-				if (cubes.Contains(adjacentCube)) {
-					noOfExposedFaces--;
-				} else if (IsSurrounded(adjacentCube, cubes)) {  // assumes 1x1x1 space
-					processedCubes.Add(adjacentCube);
-				}
-			}
-		}
-
-		// If we assume only 1x1x1 block of air enclosed then 4178 is too high
-		// Means we need to find the boundaries and attempt to flood fill any spaces
-		// Slicing the input data shows an irregular sphere with a hollow centre
+		// Find the boundaries and attempt to flood fill around the shape to leave 1 block with pockets in it
+		// Wo don't have to worry about exterior points after that
+		// Slicing the input data shows an irregular sphere with a hollowish centre
 
 		int minX = cubes.Select(cubes => cubes.X).Min() - 1;
 		int minY = cubes.Select(cubes => cubes.Y).Min() - 1;
@@ -48,74 +30,54 @@ public sealed partial class Day18 {
 		int maxY = cubes.Select(cubes => cubes.Y).Max() + 1;
 		int maxZ = cubes.Select(cubes => cubes.Z).Max() + 1;
 
-		// Pick a point on the edge and work inwards
-		Point3d start = new(minX, minY, minZ);
-		Queue<Point3d> exteriorQueue = new();
-		processedCubes.Clear();
-		exteriorQueue.Enqueue(start);
+
+		// Pick a point on the edge and work inwards filling up the space
+		Queue<Point3d> exteriorQueue = new(new Point3d[] { new(minX, minY, minZ) });
+
 		while ( exteriorQueue.Count > 0 ) {
 			Point3d cube = exteriorQueue.Dequeue();
-			processedCubes.Add(cube);
 			cubes.Add(cube);
 			foreach (Point3d adjacent in Adjacent(cube)) {
-				if    (adjacent.X < minX || adjacent.X > maxX
-					|| adjacent.Y < minY || adjacent.Y > maxY
-					|| adjacent.Z < minZ || adjacent.Z > maxZ
-						) {
-					continue;
-				}
-				if (cubes.Contains(adjacent) is false) {
-					if (processedCubes.Contains(adjacent) is false && exteriorQueue.Contains(adjacent) is false) {
-						exteriorQueue.Enqueue(adjacent);
-					}
+				if (   InBounds   (adjacent)
+					&& IsSpace    (adjacent)
+					&& IsNotQueued(adjacent)
+					) {
+					exteriorQueue.Enqueue(adjacent);
 				}
 			}
-
 		}
 
 		int countEnclosedFaces = 0;
 		for (int z = minZ; z <= maxZ; z++) {
-			for (int y = minY; y <= maxY; y++) {
-				for (int x = minX; x <= maxX; x++) {
-					Point3d cube = new(x, y, z);
-					if (cubes.Contains(cube) is false && !IsFloating(cube, cubes)) {
-						countEnclosedFaces +=
-							Adjacent(cube)
-							.Select(cubes.Contains)
-							.Count(b => b == true);
-					}
-				}
+		for (int y = minY; y <= maxY; y++) {
+		for (int x = minX; x <= maxX; x++) {
+			Point3d cube = new(x, y, z);
+			
+			if (   IsSpace      (cube)
+				&& IsNotFloating(cube)
+				) {
+				countEnclosedFaces += AdjacentSolidCubesCount(cube);
 			}
+		}
+		}
 		}
 
 		return noOfExposedFaces - countEnclosedFaces;
 
-		void PrintToConsole() {
-			// Start Debugging - print 3d slices
-			for (int z = minZ; z <= maxZ; z++) {
-				Console.WriteLine();
-				for (int y = minY; y <= maxY; y++) {
-					for (int x = minX; x <= maxX; x++) {
-						Console.Write($"{(cubes.Contains(new(x, y, z)) ? '#' : '.')}");
-					}
-					Console.WriteLine();
-				}
-				Console.WriteLine();
-			}
-			// End Debugging
-		}
-	}
+		// Local functions
 
-	private static bool IsSurrounded(Point3d cube, HashSet<Point3d> cubes) {
-		return Adjacent(cube)
-			.Select(cubes.Contains)
-			.Count(b => b == true) == 6;
-	}
+		int AdjacentSolidCubesCount(Point3d cube) => Adjacent(cube).Select(cubes.Contains).Count(b => b == true);
 
-	private static bool IsFloating(Point3d cube, HashSet<Point3d> cubes) {
-		return Adjacent(cube)
-			.Select(cubes.Contains)
-			.Count(b => b == true) == 0;
+		bool IsNotQueued(Point3d cube) => !exteriorQueue.Contains(cube);
+
+		bool IsSpace(Point3d cube) => !cubes.Contains(cube);
+
+		bool IsNotFloating(Point3d cube) => Adjacent(cube).Select(cubes.Contains).Any(b => b == true) ;
+
+		bool InBounds(Point3d cube) =>
+			   cube.X >= minX && cube.X <= maxX
+			&& cube.Y >= minY && cube.Y <= maxY
+			&& cube.Z >= minZ && cube.Z <= maxZ;
 	}
 
 	private static IEnumerable<Point3d> Adjacent(Point3d cube) {
@@ -129,6 +91,12 @@ public sealed partial class Day18 {
 		};
 	}
 
+	private static int NoOfExposedFaces(HashSet<Point3d> cubes) {
+		return (cubes.Count * 6) -
+					cubes.SelectMany(Adjacent)
+					.Select(cubes.Contains)
+					.Count(exists => exists == true);
+	}
 
 	private static Point3d ParseLine(string input) {
 		List<int> numbers = input.Split(',').AsInts().ToList();
