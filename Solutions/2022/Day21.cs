b2882192e1a6199e81ca318188c8c7e3ac65885f
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-21 06:01:18 +0000

    2022 Day21 Part Solved

---
+++ Solutions/2022/Day21.cs
@@ -0,0 +1,84 @@
+﻿namespace AdventOfCode.Solutions._2022;
+
+/// <summary>
+/// Day 21: Monkey Math
+/// https://adventofcode.com/2022/day/21
+/// </summary>
+[Description("Monkey Math")]
+public sealed partial class Day21 {
+
+	//[Init]
+	//public static    void Init(string[] input, params object[]? _) => LoadMonkeys(input);
+	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+
+
+	private static long Solution1(string[] input) {
+		List<Monkey> monkeys = input.Select(Monkey.Parse).ToList();
+
+		Monkey rootMonkey = monkeys.Where(m => m.Name == "root").Single();
+		return CalculateValue(rootMonkey, monkeys);
+		;
+	}
+	private static string Solution2(string[] input) {
+		List<Monkey> Monkeys = input.Select(Monkey.Parse).ToList();
+		return "** Solution not written yet **";
+	}
+
+
+	private static long CalculateValue(Monkey monkey, List<Monkey> monkeys) {
+
+		if (monkey is NUMBER_Monkey numberMonkey) {
+			return numberMonkey.Value;
+		}
+
+		if (monkey is MATH_Monkey mathMonkey) {
+			return mathMonkey.Operation switch {
+				Operation.add => CalculateValue(Find(mathMonkey.Monkey1), monkeys) + CalculateValue(Find(mathMonkey.Monkey2), monkeys),
+				Operation.subtract => CalculateValue(Find(mathMonkey.Monkey1), monkeys) - CalculateValue(Find(mathMonkey.Monkey2), monkeys),
+				Operation.multiply => CalculateValue(Find(mathMonkey.Monkey1), monkeys) * CalculateValue(Find(mathMonkey.Monkey2), monkeys),
+				Operation.divide => CalculateValue(Find(mathMonkey.Monkey1), monkeys) / CalculateValue(Find(mathMonkey.Monkey2), monkeys),
+				_ => throw new NotImplementedException(),
+			};
+		}
+
+		throw new NotImplementedException();
+
+		Monkey Find(Monkey monkey) => monkeys.Where(m => m.Name == monkey.Name).Single();
+	}
+
+
+	record Monkey(string Name) {
+		public static Monkey Parse(string s) {
+			string name = s[0..4];
+			if (Char.IsNumber(s[6])) {
+				return new NUMBER_Monkey(Name: name, Value: s[6..].AsInt());
+			} else {
+				return new MATH_Monkey(
+					Name: name,
+					Monkey1: new(s[6..10]),
+					Monkey2: new(s[13..17]),
+					Operation: s[11] switch {
+						'+' => Operation.add,
+						'-' => Operation.subtract,
+						'*' => Operation.multiply,
+						'/' => Operation.divide,
+						_ => throw new NotImplementedException(),
+					}
+				);
+			}
+		}
+
+		public static Monkey Parse(string s, IFormatProvider? provider) => throw new NotImplementedException();
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Monkey result) => throw new NotImplementedException();
+	};
+	record NUMBER_Monkey(string Name, long Value) : Monkey(Name);
+	record MATH_Monkey  (string Name, Operation Operation, Monkey Monkey1, Monkey Monkey2) : Monkey(Name);
+
+	public enum Operation {
+		add,
+		subtract,
+		multiply,
+		divide,
+	}
+}
---
+++ Tests/2022/Tests_21.cs
@@ -0,0 +1,51 @@
+﻿namespace AdventOfCode.Tests._2022;
+
+public class Tests_21_Monkey_Math {
+	[Theory]
+	[InlineData("""
+		root: pppw + sjmn
+		dbpl: 5
+		cczh: sllz + lgvd
+		zczc: 2
+		ptdq: humn - dvpt
+		dvpt: 3
+		lfqf: 4
+		humn: 5
+		ljgn: 2
+		sjmn: drzm * dbpl
+		sllz: 4
+		pppw: cczh / lfqf
+		lgvd: ljgn * ptdq
+		drzm: hmdt - zczc
+		hmdt: 32
+		"""
+		, 152)]
+	public void Part1(string input, int expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2022, 21, 1, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData("""
+		root: pppw + sjmn
+		dbpl: 5
+		cczh: sllz + lgvd
+		zczc: 2
+		ptdq: humn - dvpt
+		dvpt: 3
+		lfqf: 4
+		humn: 5
+		ljgn: 2
+		sjmn: drzm * dbpl
+		sllz: 4
+		pppw: cczh / lfqf
+		lgvd: ljgn * ptdq
+		drzm: hmdt - zczc
+		hmdt: 32
+		"""
+		, 9999)]
+	public void Part2(string input, long expected) {
+		_ = long.TryParse(SolutionRouter.SolveProblem(2022, 21, 2, input), out long actual);
+		Assert.Equal(expected, actual);
+	}
+}
