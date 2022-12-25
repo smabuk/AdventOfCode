e868d7de09c551a6fce17c17e49d1ffe426fb59c
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-25 06:06:43 +0000

    2022 Day25 Part1 Solved

---
+++ Solutions/2022/Day25.cs
@@ -0,0 +1,78 @@
+﻿namespace AdventOfCode.Solutions._2022;
+
+/// <summary>
+/// Day 25: Full of Hot Air
+/// https://adventofcode.com/2022/day/25
+/// </summary>
+[Description("Full of Hot Air")]
+public sealed partial class Day25 {
+
+	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+
+	private static string Solution1(string[] input) {
+		long sum = input.Select(i => new SnafuNumber(i.Trim()).Value()).Sum();
+		return SnafuNumber.ToSnafu(sum);
+	}
+
+	private static string Solution2(string[] input) {
+		return "** Solution not written yet **";
+	}
+
+	private record struct SnafuNumber(string Number) {
+		public long Value() {
+			string positive = Number.Trim().Replace("=", "0").Replace("-", "0");
+			string negative = Number.Trim().Replace("2", "0").Replace("1", "0").Replace("=", "2").Replace("-", "1");
+			
+			return FromBase5(positive) - FromBase5(negative);
+		}
+
+		public static long FromBase5(string number) {
+			long value = 0;
+			for (int i = 0; i < number.Length; i++) {
+				value += (long)Math.Pow(5, i) * number[number.Length - i - 1].ToString().AsInt(); 
+			}
+			return value;
+		} 
+
+		public static string ToBase5(long value) {
+			string result = "";
+			long index;
+			while (value != 0) {
+				index = value % 5;
+				value /= 5;
+				result = $"{index}{result}";
+			}
+			return result;
+		} 
+
+		public static string ToBase3(long value) {
+			string result = "";
+			long index;
+			while (value != 0) {
+				index = value % 3;
+				value /= 3;
+				result = $"{index}{result}";
+			}
+			return result;
+		}
+		public static string ToSnafu(long number) {
+
+			string result = "";
+			while (number > 0) {
+				int digit = (int)((number + 2) % 5) - 2;
+				result = digit switch { 
+					-1 => '-',
+					-2 => "=",
+					_ => digit.ToString(),
+				} + result;
+				number -= digit;
+				number /= 5;
+			}
+
+			return result;
+		}
+
+	}
+
+}
---
+++ Tests/2022/Tests_25.cs
@@ -0,0 +1,35 @@
+﻿namespace AdventOfCode.Tests._2022;
+
+public class Tests_25_Full_of_Hot_Air {
+	[Theory]
+	[InlineData("""
+		1=-0-2
+		12111 
+		 2=0= 
+		   21 
+		 2=01 
+		  111 
+		20012 
+		  112 
+		1=-1= 
+		 1-12 
+		   12 
+		   1= 
+		  122 
+		"""
+		, "2=-1=0")]
+	public void Part1(string input, string expected) {
+		string actual = SolutionRouter.SolveProblem(2022, 25, 1, input);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData("""
+		b
+		"""
+		, 9999)]
+	public void Part2(string input, long expected) {
+		_ = int.TryParse(SolutionRouter.SolveProblem(2022, 25, 2, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+}
