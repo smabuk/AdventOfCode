f1cb9367295b906dd1aa719afefff25fe3515bce
smabuk <2011834+smabuk@users.noreply.github.com>
2023-11-11 12:37:52 +0000

    2018 Day07 Part1 Solved

---
+++ Solutions/2018/Day07.cs
@@ -0,0 +1,56 @@
+﻿namespace AdventOfCode.Solutions._2018;
+
+/// <summary>
+/// Day 07: The Sum of Its Parts
+/// https://adventofcode.com/2018/day/07
+/// </summary>
+[Description("The Sum of Its Parts")]
+public sealed partial class Day07 {
+
+	[Init]
+	public static    void Init(string[] input, params object[]? _) => LoadInstructions(input);
+	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+
+	private static IEnumerable<Instruction> _instructions = [];
+
+	private static void LoadInstructions(string[] input) {
+		_instructions = input.Select(Instruction.Parse);
+	}
+
+	private static string Solution1(string[] input) {
+		List<Instruction> instructions = input.Select(ParseLine).ToList();
+		HashSet<char> steps = [
+			.. instructions.Select(instruction => instruction.BeforeCanBeginStep),
+			.. instructions.Select(instruction => instruction.MustBeFinishedStep),
+		];
+
+		string correctOrder = "";
+		do {
+			char nextAvailable = steps
+				.ExceptBy(instructions.Select(instruction => instruction.BeforeCanBeginStep), s => s)
+				.Order()
+				.First();
+			_ = instructions.RemoveAll(i => i.MustBeFinishedStep == nextAvailable);
+			_ = steps.Remove(nextAvailable);
+			correctOrder += nextAvailable;
+		} while (steps.Count != 0);
+
+		return correctOrder;
+	}
+
+	private static string Solution2(string[] input) {
+		//string inputLine = input[0];
+		//List<string> inputs = input.ToList();
+		List<Instruction> instructions = input.Select(ParseLine).ToList();
+		return "** Solution not written yet **";
+	}
+
+	private record Instruction(char MustBeFinishedStep, char BeforeCanBeginStep) : IParsable<Instruction> {
+		public static Instruction Parse(string s) => ParseLine(s);
+		public static Instruction Parse(string s, IFormatProvider? provider) => throw new NotImplementedException();
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Instruction result) => throw new NotImplementedException();
+	}
+
+	private static Instruction ParseLine(string input) => new(input[5], input[36]);
+}
---
+++ Tests/2018/Tests_07.cs
@@ -0,0 +1,40 @@
+﻿namespace AdventOfCode.Tests.Year2018;
+
+public class Tests_07_The_Sum_of_Its_Parts
+{
+	const int DAY = 7;
+
+	[Theory]
+	[InlineData("""
+		Step C must be finished before step A can begin.
+		Step C must be finished before step F can begin.
+		Step A must be finished before step B can begin.
+		Step A must be finished before step D can begin.
+		Step B must be finished before step E can begin.
+		Step D must be finished before step E can begin.
+		Step F must be finished before step E can begin.
+		"""
+		, "CABDFE")]
+	public void Part1(string input, string expected)
+	{
+		string actual = SolutionRouter.SolveProblem(YEAR, DAY, PART1, input);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData("""
+		Step C must be finished before step A can begin.
+		Step C must be finished before step F can begin.
+		Step A must be finished before step B can begin.
+		Step A must be finished before step D can begin.
+		Step B must be finished before step E can begin.
+		Step D must be finished before step E can begin.
+		Step F must be finished before step E can begin.
+		"""
+		, 9999)]
+	public void Part2(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART2, input, 32), out int actual);
+		Assert.Equal(expected, actual);
+	}
+}
