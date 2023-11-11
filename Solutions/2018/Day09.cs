50e7fc42fad56d716ed15d936968b68c2af4d4e8
smabuk <2011834+smabuk@users.noreply.github.com>
2023-11-11 17:07:39 +0000

    2018 Day09 Part1 Solved

---
+++ Solutions/2018/Day09.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode.Solutions._2018;
+
+/// <summary>
+/// Day 09: Marble Mania
+/// https://adventofcode.com/2018/day/09
+/// </summary>
+[Description("Marble Mania")]
+public sealed partial class Day09 {
+
+	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+
+	private static int Solution1(string[] input) {
+		string[] inputs = input[0].Split(" ");
+		int noOfPlayers = int.Parse(inputs[0]);
+		int lastMarble = int.Parse(inputs[6]);
+
+		int[] elfScores = new int[noOfPlayers];
+		List<int> circle = [0, 1];
+		int currentMarblePosition = 1;
+		int currentElf = 0;
+
+		for (int marble = 2; marble <= lastMarble; marble++) {
+			currentElf = (currentElf + 1) % noOfPlayers;
+			if (marble % 23 == 0) {
+				elfScores[currentElf] += marble;
+				int nextPosition = (currentMarblePosition >= 7) switch {
+					true  => currentMarblePosition - 7,
+					false => currentMarblePosition + circle.Count - 7,
+				};
+				elfScores[currentElf] += circle[nextPosition];
+				circle.RemoveAt(nextPosition);
+				currentMarblePosition = nextPosition % circle.Count;
+			} else {
+				int nextPosition = (currentMarblePosition + 2) % circle.Count;
+				if (nextPosition == 0) {
+					circle.Add(marble);
+					nextPosition = circle.Count - 1;
+				} else {
+					circle.Insert(nextPosition, marble);
+				}
+				currentMarblePosition = nextPosition;
+			}
+		}
+
+		return elfScores.Max();
+	}
+
+	private static string Solution2(string[] input) {
+		return "** Solution not written yet **";
+	}
+}
---
+++ Tests/2018/Tests_09.cs
@@ -0,0 +1,32 @@
+﻿namespace AdventOfCode.Tests.Year2018;
+
+public class Tests_09_Marble_Mania
+{
+	const int DAY = 9;
+
+	[Theory]
+	[InlineData("9 players; last marble is worth 25 points",        32)]
+	[InlineData("10 players; last marble is worth 1618 points",   8317)]
+	[InlineData("13 players; last marble is worth 7999 points", 146373)]
+	[InlineData("17 players; last marble is worth 1104 points",   2764)]
+	[InlineData("21 players; last marble is worth 6111 points",  54718)]
+	[InlineData("30 players; last marble is worth 5807 points",  37305)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData("9 players; last marble is worth 25 points",    9999)]
+	[InlineData("10 players; last marble is worth 1618 points", 9999)]
+	[InlineData("13 players; last marble is worth 7999 points", 9999)]
+	[InlineData("17 players; last marble is worth 1104 points", 9999)]
+	[InlineData("21 players; last marble is worth 6111 points", 9999)]
+	[InlineData("30 players; last marble is worth 5807 points", 9999)]
+	public void Part2(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART2, input, 2, 0), out int actual);
+		Assert.Equal(expected, actual);
+	}
+}
