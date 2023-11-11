e67155534b78e93f087a0ee1aa7e26cb0d623aa2
smabuk <2011834+smabuk@users.noreply.github.com>
2023-11-11 15:48:50 +0000

    2018 Day08 Part1 Solved

---
+++ Solutions/2018/Day08.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace AdventOfCode.Solutions._2018;
+
+/// <summary>
+/// Day 08: Memory Maneuver
+/// https://adventofcode.com/2018/day/08
+/// </summary>
+[Description("Memory Maneuver")]
+public sealed partial class Day08 {
+
+	[Init]
+	public static    void Init(string[] input, params object[]? _) => BuildTree(input);
+	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
+
+	private static Node _licenseTree = new();
+
+	private static void BuildTree(string[] input) {
+		List<int> inputs = input[0].Split(' ').AsInts().ToList();
+		(_licenseTree, int _) = GetNode(inputs);
+	}
+
+	private static int Solution1(string[] _) {
+		return _licenseTree.SumOfMetadataEntries;
+	}
+
+	private static string Solution2(string[] input) {
+		return "** Solution not written yet **";
+	}
+
+	private static (Node Node, int Skip) GetNode(List<int> inputs)
+	{
+		Node node = new();
+		int noOfNodes = inputs[0];
+		int noOfMetadataEntries = inputs[1];
+		int skip = 2;
+
+		for (int index = 0; index < noOfNodes; index++) {
+			(Node childNode, int skipMore) = GetNode(inputs[(skip)..]);
+			node.Nodes.Add(childNode);
+			skip += skipMore;
+		}
+
+		for (int index = 0; index < noOfMetadataEntries; index++) {
+			node.MetaData.Add(inputs[index + skip]);
+		}
+
+		skip += noOfMetadataEntries;
+
+		return (node, skip);
+	}
+
+	private record Node
+	{
+		public List<Node> Nodes = [];
+		public List<int> MetaData = [];
+
+		public int SumOfMetadataEntries => Nodes.Sum(n => n.SumOfMetadataEntries) + MetaData.Sum();
+	}
+
+}
---
+++ Tests/2018/Tests_08.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode.Tests.Year2018;
+
+public class Tests_08_Memory_Maneuver
+{
+	const int DAY = 8;
+
+	[Theory]
+	[InlineData("0 3 1 1 2", 4)]
+	[InlineData("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2", 138)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2", 9999)]
+	public void Part2(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART2, input, 2, 0), out int actual);
+		Assert.Equal(expected, actual);
+	}
+}
