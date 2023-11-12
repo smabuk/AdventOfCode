97939e85a7bc749fee90a55a8e592b4958d44ecb
smabuk <2011834+smabuk@users.noreply.github.com>
2023-11-12 13:33:05 +0000

    2018 Day12 Solved

--- Solutions/2018/Day12.cs
+++ Solutions/2018/Day12.cs
@@ -46,10 +46,44 @@ public sealed partial class Day12 {
 		return potPlantSum;
 	}
 
-	private static string Solution2(string[] input) {
-		//string inputLine = input[0];
-		//List<string> inputs = input.ToList();
-		//List<Instruction> instructions = input.Select(ParseLine).ToList();
-		return "** Solution not written yet **";
+	private static long Solution2(string[] input) {
+		const long noOfGenerations = 50_000_000_000;
+		string currentState = $"{NO_PLANT}{NO_PLANT}{NO_PLANT}{NO_PLANT}{input[0][15..]}{NO_PLANT}{NO_PLANT}{NO_PLANT}{NO_PLANT}";
+		long offset = 4;
+
+		Dictionary<string, long> state = [];
+		state.Add(currentState, offset);
+
+		HashSet<string> plantPatterns = input[2..]
+			.Where(i => i.EndsWith(PLANT))
+			.Select(i => i[..5])
+			.ToHashSet();
+
+		for (long gen = 1; gen <= noOfGenerations; gen++) {
+			char[] newStateArray = new char[currentState.Length - 4];
+			for (int pot = 0; pot < currentState.Length - 4; pot++) {
+				char plant = plantPatterns.Contains(currentState[pot..(pot + 5)]) ? PLANT : NO_PLANT;
+				newStateArray[pot] = plant;
+			}
+			string newState = new(newStateArray);
+			offset = offset + 2 - newState.IndexOf(PLANT);
+			currentState = $"{NO_PLANT}{NO_PLANT}{NO_PLANT}{NO_PLANT}{newState.Trim(NO_PLANT)}{NO_PLANT}{NO_PLANT}{NO_PLANT}{NO_PLANT}";
+			if (state.TryGetValue(currentState, out long stateOffset)) { 
+				// Turns out this is the previous state that repeats so we have a simple calculation for the remaining iterations
+				offset += (noOfGenerations - gen) * (offset - stateOffset);
+				break;
+			} else {
+				state.Add(currentState, offset);
+			}
+		}
+
+		long potPlantSum = 0;
+		for (int pot = 0; pot < currentState.Length - 4; pot++) {
+			if (currentState[pot] == PLANT) {
+				potPlantSum += pot - offset;
+			}
+		}
+
+		return potPlantSum;
 	}
 }
