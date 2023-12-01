eb54b523582ca5da29a56bd346e4089eb6333b6e
smabuk <2011834+smabuk@users.noreply.github.com>
2023-12-01 12:02:41 +0000

    2023 Day01 refactored

--- Solutions/2023/Day01.cs
+++ Solutions/2023/Day01.cs
@@ -1,83 +1,61 @@
 ﻿namespace AdventOfCode.Solutions._2023;
 
 /// <summary>
-/// Day 01: Trebuchet
+/// Day 01: Trebuchet?!
 /// https://adventofcode.com/2023/day/01
 /// </summary>
-[Description("Trebuchet")]
+[Description("Trebuchet?!")]
 public sealed partial class Day01 {
 
 	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
 	public static string Part2(string[] input, params object[]? _) => Solution2(input).ToString();
 
 
-	private static int Solution1(string[] input) => input.Select(ParseLine).Sum();
+	private static int Solution1(string[] input) => input.Sum(i => GetCalibrationValue(i, 1));
+	private static int Solution2(string[] input) => input.Sum(i => GetCalibrationValue(i, 2));
 
-	private static int Solution2(string[] input) => input.Select(ParseLinePart2).Sum();
 
-	private static int ParseLine(string input)
-	{
-		MatchCollection match = InputRegEx().Matches(input);
-		return (int.Parse(match[0].Value) * 10) + int.Parse(match[^1].Value);
-	}
-
-	private static int ParseLinePart2(string input)
-	{
-		string[] validMatches = [
-			"one",
-			"two",
-			"three",
-			"four",
-			"five",
-			"six",
-			"seven",
-			"eight",
-			"nine",
-			"1",
-			"2",
-			"3",
-			"4",
-			"5",
-			"6",
-			"7",
-			"8",
-			"9",
-			];
-	
-		int firstValue = GetValue(validMatches
-			.Select(s => (Match: s, Position: input.IndexOf(s)))
-			.Where(x => x.Position != -1)
-			.OrderBy(x => x.Position)
-			.First()
-			.Match);
-
-		int lastValue = GetValue(
-			validMatches
-			.Select(s => (Match: s, Position: input.LastIndexOf(s)))
-			.OrderByDescending(x => x.Position)
-			.First()
-			.Match);
-
-		return (firstValue * 10) + lastValue;
-	}
-
-	private static int GetValue(string value)
-	{
-		return value switch
+	private static readonly Dictionary<string, int> VALID_MATCHES = new()
 		{
-			"one"   => 1,
-			"two"   => 2,
-			"three" => 3,
-			"four"  => 4,
-			"five"  => 5,
-			"six"   => 6,
-			"seven" => 7,
-			"eight" => 8,
-			"nine"  => 9,
-			_ => int.Parse(value),
+			{ "1",     1 },
+			{ "2",     2 },
+			{ "3",     3 },
+			{ "4",     4 },
+			{ "5",     5 },
+			{ "6",     6 },
+			{ "7",     7 },
+			{ "8",     8 },
+			{ "9",     9 },
+			// Use these matches as well for Part2
+			{ "one",   1 },
+			{ "two",   2 },
+			{ "three", 3 },
+			{ "four",  4 },
+			{ "five",  5 },
+			{ "six",   6 },
+			{ "seven", 7 },
+			{ "eight", 8 },
+			{ "nine",  9 },
 		};
-	}
 
-	[GeneratedRegex("""(?<number>[\d])""")]
-	private static partial Regex InputRegEx();
+	private static int GetCalibrationValue(string input, int partNo)
+	{
+		const int NO_MATCH = -1;
+
+		IEnumerable<(int Value, int FirstPosition, int LastPosition)> matches = VALID_MATCHES
+			.Take(partNo * 9)
+			.Select(vm => (vm.Value, input.IndexOf(vm.Key), input.LastIndexOf(vm.Key)));
+
+		int firstDigit = matches
+			.Where(m => m.FirstPosition != NO_MATCH)
+			.MinBy(m => m.FirstPosition)
+			.Value;
+
+		int lastDigit = matches
+			.Where(m => m.LastPosition != NO_MATCH)
+			.MaxBy(m => m.LastPosition)
+			.Value;
+
+		return (firstDigit * 10) + lastDigit;
+	}
 }
