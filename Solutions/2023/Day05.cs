51c0a6f99d069d44aaf0a5215c6469608800b962
smabuk <2011834+smabuk@users.noreply.github.com>
2023-12-05 06:10:47 +0000

    2023 Day05 Part1 Solved

---
+++ Solutions/2023/Day05.cs
@@ -0,0 +1,106 @@
+﻿
+using System.Data;
+
+namespace AdventOfCode.Solutions._2023;
+
+/// <summary>
+/// Day 05: If You Give A Seed A Fertilizer
+/// https://adventofcode.com/2023/day/05
+/// </summary>
+[Description("If You Give A Seed A Fertilizer")]
+public sealed partial class Day05 {
+
+	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+
+	private static readonly string[] MAP_NAMES = [
+		"seed-to-soil",
+		"soil-to-fertilizer",
+		"fertilizer-to-water",
+		"water-to-light",
+		"light-to-temperature",
+		"temperature-to-humidity",
+		"humidity-to-location",
+		];
+
+
+	private static long Solution1(string[] input) {
+		Dictionary<string, Map> maps = [];
+		
+		int inputIndex = 0;
+		List<long> seeds = [.. input[inputIndex][7..].AsLongs()];
+
+		inputIndex += 3;
+		foreach (string mapName in MAP_NAMES) {
+			maps[mapName] = Map.Parse(input[inputIndex..]);
+			inputIndex += maps[mapName].Mappings.Count + 2;
+		}
+
+		long lowestLocationNumber = int.MaxValue;
+
+		foreach (long seed in seeds) {
+			long destination = seed;
+			foreach (string mapName in MAP_NAMES) {
+				destination =  maps[mapName].Destination(destination);
+			}
+			if (destination < lowestLocationNumber) {
+				lowestLocationNumber = destination;
+			}
+		}
+
+		return lowestLocationNumber;
+	}
+
+	private static string Solution2(string[] input) {
+		return "** Solution not written yet **";
+	}
+
+
+	private record Map(List<Mapping> Mappings) 
+	{
+		public long Destination(long source)
+		{
+			foreach (Mapping mapping in Mappings) {
+				if (mapping.TryMap(source, out long value)) {
+					return value;
+				}
+			}
+			return source;
+		}
+
+		public static Map Parse(string[] s)
+		{
+			List<Mapping> mappings = [];
+			foreach (string line in s) {
+				if (string.IsNullOrWhiteSpace(line)) {
+					return new(mappings);
+				}
+				mappings.Add(Mapping.Parse(line, null));
+			}
+			return new(mappings);
+		}
+	}
+
+	private record Mapping(long DestinationStart, long SourceStart, long Length) : ISimpleParsable<Mapping>
+	{
+		public bool TryMap(long value, out long match)
+		{
+			match = 0;
+			if (value >= SourceStart && value < SourceStart + Length) {
+				match = DestinationStart + (value - SourceStart);
+				return true;
+			}
+			return false;
+		}
+
+		public static Mapping Parse(string s, IFormatProvider? provider)
+		{
+			const char SEP = ' ';
+			long[] values = [.. s.Split(SEP).AsLongs()];
+			return new(values[0], values[1], values[2]);
+		}
+
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Mapping result)
+			=> ISimpleParsable<Mapping>.TryParse(s, provider, out result);
+	}
+}
---
+++ Tests/2023/Tests_05.cs
@@ -0,0 +1,48 @@
+﻿namespace AdventOfCode.Tests.Year2023;
+
+public class Tests_05_If_You_Give_A_Seed_A_Fertilizer
+{
+	const int DAY = 5;
+
+	[Theory]
+	[InlineData("""
+		seeds: 79 14 55 13
+
+		seed-to-soil map:
+		50 98 2
+		52 50 48
+
+		soil-to-fertilizer map:
+		0 15 37
+		37 52 2
+		39 0 15
+
+		fertilizer-to-water map:
+		49 53 8
+		0 11 42
+		42 0 7
+		57 7 4
+
+		water-to-light map:
+		88 18 7
+		18 25 70
+
+		light-to-temperature map:
+		45 77 23
+		81 45 19
+		68 64 13
+
+		temperature-to-humidity map:
+		0 69 1
+		1 0 69
+
+		humidity-to-location map:
+		60 56 37
+		56 93 4
+		""", 35)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+}
