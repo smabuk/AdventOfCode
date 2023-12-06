e4d4ef9cc6e95743a570239978c0b340ae211bb6
smabuk <2011834+smabuk@users.noreply.github.com>
2023-12-06 05:52:33 +0000

    2023 Day06 Part1 Solved

---
+++ Solutions/2023/Day06.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode.Solutions._2023;
+
+/// <summary>
+/// Day 06: Wait For It
+/// https://adventofcode.com/2023/day/06
+/// </summary>
+[Description("Wait For It")]
+public sealed partial class Day06
+{
+
+	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+
+	private static IEnumerable<Race> _races = [];
+
+	private static int Solution1(string[] input)
+	{
+		int[] raceTimes     = [.. input[0][10..].AsInts()];
+		int[] raceDistances = [.. input[1][10..].AsInts()];
+
+		List<Race> races = [.. raceTimes.Zip(raceDistances).Select(td => new Race(td.First, td.Second))];
+
+		int productOfWins = 1;
+
+		foreach (Race race in races) {
+			int wins = 0;
+			for (int i = 1; i < race.Time - 1; i++) {
+				wins += (Boat.Win(i, race.Time, race.Distance)) ? 1 : 0;
+			}
+			productOfWins *= wins;
+		}
+
+		return productOfWins;
+	}
+
+	private static string Solution2(string[] input)
+	{
+		//string inputLine = input[0];
+		//List<string> inputs = input.ToList();
+		//List<Race> races = [.. input.As<Race>()];
+		return "** Solution not written yet **";
+	}
+
+	private record Race(int Time, int Distance);
+	private record Boat()
+	{
+		public static int Distance(int timeButtonHeld, int timeAllowed) => (timeAllowed - timeButtonHeld) * timeButtonHeld;
+		public static bool Win(int timeButtonHeld, int timeAllowed, int distanceToBeat) => ((timeAllowed - timeButtonHeld) * timeButtonHeld) > distanceToBeat;
+
+	}
+
+}
---
+++ Tests/2023/Tests_06.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode.Tests.Year2023;
+
+public class Tests_06_Wait_For_It {
+	const int DAY = 6;
+
+	private const string TEST_DATA = """
+		Time:      7  15   30
+		Distance:  9  40  200
+		""";
+
+	[Theory]
+	[InlineData(TEST_DATA, 288)]
+	public void Part(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData(TEST_DATA, 9999)]
+	public void Part2(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART2, input), out int actual);
+		Assert.Equal(expected, actual);
+	}
+}
