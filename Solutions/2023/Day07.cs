bbf41382da0ef637c3fad4e0c1082c6a9e9ea82a
smabuk <2011834+smabuk@users.noreply.github.com>
2023-12-07 07:39:36 +0000

    2023 Day07 Solved

--- Solutions/2023/Day07.cs
+++ Solutions/2023/Day07.cs
@@ -7,103 +7,89 @@
 [Description("Camel Cards")]
 public sealed partial class Day07 {
 
-	[Init]
-	public static    void Init(string[] input, params object[]? args) => LoadInstructions(input);
 	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
 	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
 
-	private static IEnumerable<Hand> _instructions = [];
-
-	private static void LoadInstructions(string[] input) {
-		_instructions = input.As<Hand>();
-	}
-
 	private static int Solution1(string[] input) => new SetOfHands([.. input.As<Hand>()]).TotalWinnings;
+	private static int Solution2(string[] input) => new SetOfHands([.. input.Select(i => i.Replace('J', JOKER)).As<Hand>()]).TotalWinnings;
 
-	private static string Solution2(string[] input) {
-		return "** Solution not written yet **";
+	const char JOKER = 'j';
+
+	private record SetOfHands(List<Hand> Hands) {
+		public int TotalWinnings => Hands
+			.OrderBy(h => h.SortStrength)
+			.Select((hand, index) => (Hand: hand, Rank: index + 1))
+			.Sum(hand => hand.Hand.BidAmount * hand.Rank);
 	}
-
-
-	private record SetOfHands(List<Hand> Hands) { 
-
-		public int TotalWinnings {  get {
-				List<Hand> rankedHands = [.. Hands
-					.OrderBy(h => h.Type)
-					.ThenBy(h => h.Cards[0].Strength)
-					.ThenBy(h => h.Cards[1].Strength)
-					.ThenBy(h => h.Cards[2].Strength)
-					.ThenBy(h => h.Cards[3].Strength)
-					.ThenBy(h => h.Cards[4].Strength)
-					];
-
-				return rankedHands.Select((hand, index) => (Hand: hand, Rank: index + 1)).Sum(cards => cards.Hand.BidAmount * cards.Rank);
-			}
-		}
-
-	};
 
 	private record Hand(List<Card> Cards, int BidAmount) : IParsable<Hand> {
+		public HandType Type {
+			get
+			{
+				List<Card> cards = [.. Cards];
+				if (cards.Any(card => card.Label == JOKER)) {
+					char label = cards
+						.Where(card => card.Label is not JOKER)
+						.GroupBy(c => c)
+						.OrderByDescending(c => c.Count())
+						.Select(card => card.Key.Label)
+						.FirstOrDefault(JOKER);
+					cards = [.. cards.Select(card => card.Label == JOKER ? card with { Label = label} : card)];
+				}
 
-		public HandType Type => GetHandType();
+				IEnumerable<IGrouping<Card, Card>> counts = cards.GroupBy(card => card);
 
-		private HandType GetHandType()
-		{
-			var groupedCards = Cards.GroupBy(c => c);
-			if (groupedCards.Any(c => c.Count() == 5)) {
-				return HandType.FiveOfAKind;
-			} else if (groupedCards.Any(c => c.Count() == 4)) {
-				return HandType.FourOfAKind;
-			} else if (groupedCards.Any(c => c.Count() == 3) && groupedCards.Any(c => c.Count() == 2)) {
-				return HandType.FullHouse;
-			} else if (groupedCards.Any(c => c.Count() == 3)) {
-				return HandType.ThreeOfAKind;
-			} else if (groupedCards.Where(c => c.Count() == 2).Count() == 2) {
-				return HandType.TwoPair;
-			} else if (groupedCards.Any(c => c.Count() == 2)) {
-				return HandType.OnePair;
+				if (     counts.Any(c => c.Count() == 5))                { return HandType.FiveOfAKind; }
+				else if (counts.Any(c => c.Count() == 4))                { return HandType.FourOfAKind; }
+				else if (counts.Any(c => c.Count() == 3)
+					  && counts.Any(c => c.Count() == 2))                { return HandType.FullHouse; }
+				else if (counts.Any(c => c.Count() == 3))                { return HandType.ThreeOfAKind; }
+				else if (counts.Where(c => c.Count() == 2).Count() == 2) { return HandType.TwoPair; }
+				else if (counts.Any(c => c.Count() == 2))                { return HandType.OnePair; }
+				else                                                     { return HandType.HighCard; }
 			}
-			return HandType.HighCard;
 		}
+
+		public string SortStrength => $"{(int)Type:D2}{Cards[0].Strength:D2}{Cards[1].Strength:D2}{Cards[2].Strength:D2}{Cards[3].Strength:D2}{Cards[4].Strength:D2}";
 
 		public static Hand Parse(string s, IFormatProvider? provider)
 		{
-			List<Card> cards = [.. s[0..5].Select(c => c.ToString()).As<Card>()];
-			return new([.. cards], s[6..].AsInt());
+			 const int LABEL_INDEX = 0;
+			 const int BID_INDEX = 6;
+			 const int NO_OF_CARDS = 5;
+
+			List<Card> cards = [.. s[LABEL_INDEX..(LABEL_INDEX + NO_OF_CARDS)].Select(label => new Card(label))];
+			int bidAmount = s[BID_INDEX..].AsInt();
+
+			return new(cards, bidAmount);
 		}
 
 		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Hand result)
-			=> ISimpleParsable<Hand>.TryParse(s, provider, out result);
+		=> ISimpleParsable<Hand>.TryParse(s, provider, out result);
+
+		public enum HandType
+		{
+			HighCard,
+			OnePair,
+			TwoPair,
+			ThreeOfAKind,
+			FullHouse,
+			FourOfAKind,
+			FiveOfAKind,
+		}
 	}
 
-	private record Card(string Label) : IParsable<Card> {
-		public int Strength => Label[0] switch
+	private record struct Card(char Label) {
+		public readonly int Strength => Label switch
 		{
 			'A' => 14,
 			'K' => 13,
 			'Q' => 12,
 			'J' => 11,
 			'T' => 10,
-			>= '1' and <= '9' => Label[0] - '0',
+			>= '1' and <= '9' => Label - '0',
+			JOKER =>  1,
 			_ => throw new InvalidCastException(),
 		};
-
-		public static Card Parse(string s, IFormatProvider? provider) => new(s);
-
-		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Card result)
-			=> ISimpleParsable<Card>.TryParse(s, provider, out result);
-	};
-
-	public enum HandType
-	{
-		HighCard,
-		OnePair,
-		TwoPair,
-		ThreeOfAKind,
-		FullHouse,
-		FourOfAKind,
-		FiveOfAKind,
 	}
-
-
 }
--- Tests/2023/Tests_07.cs
+++ Tests/2023/Tests_07.cs
@@ -1,6 +1,6 @@
 ﻿namespace AdventOfCode.Tests.Year2023;
 
-public class Tests_07_CAmel_Cards {
+public class Tests_07_Camel_Cards {
 	const int DAY = 7;
 
 	private const string TEST_DATA = """
@@ -9,14 +9,21 @@ public class Tests_07_CAmel_Cards {
 		KK677 28
 		KTJJT 220
 		QQQJA 483
-		
 		""";
 
 	[Theory]
 	[InlineData(TEST_DATA, 6440)]
-	public void Part(string input, int expected)
+	public void Part1(string input, int expected)
 	{
 		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+
+	[Theory]
+	[InlineData(TEST_DATA, 5905)]
+	public void Part2(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART2, input), out int actual);
 		actual.ShouldBe(expected);
 	}
 }
