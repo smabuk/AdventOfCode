bf13a8d7a7524a3eb5967b4911b95ef2c2cdaf16
smabuk <2011834+smabuk@users.noreply.github.com>
2023-12-08 05:34:34 +0000

    2023 Day08 Part1 Solved

---
+++ Solutions/2023/Day08.cs
@@ -0,0 +1,44 @@
+﻿namespace AdventOfCode.Solutions._2023;
+
+/// <summary>
+/// Day 08: Haunted Wasteland
+/// https://adventofcode.com/2023/day/08
+/// </summary>
+[Description("Haunted Wasteland")]
+public sealed partial class Day08 {
+
+	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+
+	private static readonly char LEFT  = 'L';
+	//private static readonly char RIGHT = 'R';
+
+	private static int Solution1(string[] input) {
+		string instructions = input[0];
+
+		Dictionary<string, Node> nodeMap = input[2..]
+			.As<Node>()
+			.ToDictionary(node => node.Name, node => node);
+
+		int steps = 0;
+		Node currentNode = nodeMap["AAA"];
+		do {
+			char instruction = instructions[steps++ % instructions.Length];
+			string nextNode = instruction == LEFT ? nodeMap[currentNode.Name].LeftNode : nodeMap[currentNode.Name].RightNode;
+			currentNode = nodeMap[nextNode];
+		} while (currentNode.Name != "ZZZ");
+
+		return steps;
+	}
+
+	private static string Solution2(string[] input) {
+		return "** Solution not written yet **";
+	}
+
+	private record Node(string Name, string LeftNode, string RightNode) : IParsable<Node> {
+		public static Node Parse(string s, IFormatProvider? provider) => new(s[0..3], s[7..10], s[12..15]);
+		public static Node Parse(string s) => Parse(s, null);
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Node result)
+			=> ISimpleParsable<Node>.TryParse(s, provider, out result);
+	}
+}
---
+++ Tests/2023/Tests_08.cs
@@ -0,0 +1,38 @@
+﻿namespace AdventOfCode.Tests.Year2023;
+
+public class Tests_08_Haunted_Wasteland {
+	const int DAY = 8;
+
+	private const string TEST_DATA = """
+		LLR
+
+		AAA = (BBB, BBB)
+		BBB = (AAA, ZZZ)
+		ZZZ = (ZZZ, ZZZ)
+		""";
+
+	[Theory]
+	[InlineData("""
+		RL
+
+		AAA = (BBB, CCC)
+		BBB = (DDD, EEE)
+		CCC = (ZZZ, GGG)
+		DDD = (DDD, DDD)
+		EEE = (EEE, EEE)
+		GGG = (GGG, GGG)
+		ZZZ = (ZZZ, ZZZ)
+		""", 2)]
+	[InlineData("""
+		LLR
+
+		AAA = (BBB, BBB)
+		BBB = (AAA, ZZZ)
+		ZZZ = (ZZZ, ZZZ)
+		""", 6)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
