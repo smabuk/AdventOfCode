84002a087b4bbf5346b36f366affe431d91f2b37
smabuk <2011834+smabuk@users.noreply.github.com>
2023-12-09 06:26:18 +0000

    2023 Day09 refactored using lambdas

--- Solutions/2023/Day09.cs
+++ Solutions/2023/Day09.cs
@@ -1,75 +1,53 @@
-﻿namespace AdventOfCode.Solutions._2023;
+﻿using History    = System.Collections.Generic.IEnumerable<int>;
+using Sequencies = System.Collections.Generic.IEnumerable<System.Collections.Generic.IEnumerable<int>>;
+
+namespace AdventOfCode.Solutions._2023;
 
 /// <summary>
 /// Day 09: Mirage Maintenance
 /// https://adventofcode.com/2023/day/09
 /// </summary>
+/// <remarks>
+/// Using lambdas to practice.
+/// Don't like how long the lines of code get, but it's elegant.
+/// </remarks>
 [Description("Mirage Maintenance")]
 public sealed partial class Day09 {
 
-	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
-	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+	[Init]
+	public static   void  Init(string[] input, params object[]? args) => LoadHistories(input);
+	public static string Part1(string[] input, params object[]? args) => Solution1().ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2().ToString();
 
-	private static long Solution1(string[] input) {
-		List<List<long>> histories = [];
-		foreach (var line in input) {
-			List<long> history = [.. line.As<long>(' ')];
-			histories.Add(history);
-		}
+	private static Sequencies _histories = [];
 
-		return histories.Sum(h => GetNextInSequence(h));
-	}
+	private static void LoadHistories(string[] input)
+		=> _histories = [.. input.Select(i => i.As<int>(' '))];
 
-	private static long Solution2(string[] input) {
-		List<List<long>> histories = [];
-		foreach (var line in input) {
-			List<long> history = [.. line.As<long>(' ')];
-			histories.Add(history);
-		}
-		return histories.Sum(h => GetPreviousInSequence(h));
-	}
+	private static int Solution1()
+		=> _histories.Sum(history => Extrapolate(history,
+				(Sequencies x) => x.Select(x => x.Last()).Aggregate((a, b) => a + b)
+			));
 
-	private static long GetNextInSequence(IEnumerable<long> h)
+	private static int Solution2()
+		=> _histories.Sum(history=> Extrapolate(history,
+				(Sequencies x) => x.Select(x => x.First()).Reverse().Aggregate((a, b) => b - a)
+			));
+
+	private static int Extrapolate(History history, Func<Sequencies, int> extrapolate)
 	{
-		List<List<long>> sequences = [[.. h]];
+		List<List<int>> sequences = [[.. history]];
 
-		List<long> differences = [1];
+		List<int> differences = [];
 		do {
-			List<long> sequence = sequences[^1];
+			List<int> sequence = sequences.Last();
 			differences = [];
-
 			for (int i = 0; i < sequence.Count - 1; i++) {
 				differences.Add(sequence[i + 1] - sequence[i]);
 			}
 			sequences.Add(differences);
 		} while (differences.Any(x => x != 0));
 
-		for (int i = 0; i < sequences.Count - 1; i++) {
-			sequences[^(i + 2)].Add(sequences[^(i + 1)][^1] + sequences[^(i + 2)][^1]);
-		}
-		return sequences[0][^1];
+		return extrapolate(sequences);
 	}
-
-	private static long GetPreviousInSequence(IEnumerable<long> h)
-	{
-		List<List<long>> sequences = [[.. h]];
-
-		List<long> differences = [1];
-		do {
-			List<long> sequence = sequences[^1];
-			differences = [];
-
-			for (int i = 0; i < sequence.Count - 1; i++) {
-				differences.Add(sequence[i + 1] - sequence[i]);
-			}
-			sequences.Add(differences);
-		} while (differences.Any(x => x != 0));
-
-		for (int i = 0; i < sequences.Count - 1; i++) {
-			sequences[^(i + 2)].Insert(0, sequences[^(i + 2)][0] - sequences[^(i + 1)][0]);
-		}
-		return sequences[0][0];
-	}
-
-
 }
