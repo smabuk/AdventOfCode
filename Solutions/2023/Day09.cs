26b4a13e6dcc8c2edac85ddf543add8714fe5433
smabuk <2011834+smabuk@users.noreply.github.com>
2023-12-09 14:28:43 +0000

    2023 Day09 refactored again with .Zip()

--- Solutions/2023/Day09.cs
+++ Solutions/2023/Day09.cs
@@ -1,5 +1,4 @@
 ﻿using Sequence   = System.Collections.Generic.List<int>;
-using Sequencies = System.Collections.Generic.List<System.Collections.Generic.List<int>>;
 
 namespace AdventOfCode.Solutions._2023;
 
@@ -15,7 +14,7 @@ public sealed partial class Day09 {
 	public static string Part1(string[] input, params object[]? args) => Solution1().ToString();
 	public static string Part2(string[] input, params object[]? args) => Solution2().ToString();
 
-	private static Sequencies _histories = [];
+	private static IEnumerable<Sequence> _histories = [];
 
 	private static void LoadHistories(string[] input)
 		=> _histories = [.. input.Select(i => i.As<int>(' ').ToList())];
@@ -27,21 +26,17 @@ public sealed partial class Day09 {
 		=> _histories.Sum(history => history.Extrapolate().Select(x => x.First()).Reverse().Aggregate((a, b) => b - a));
 }
 
-file static class Day09Helpers
+public static class Day09Helpers
 {
-	public static Sequencies Extrapolate(this Sequence history)
+	public static IEnumerable<Sequence> Extrapolate(this Sequence history)
 	{
-		Sequencies sequences = [[.. history]];
-		Sequence differences = [];
+		Sequence differences = [.. history];
+		yield return history;
 		do {
-			Sequence sequence = sequences.Last();
-			differences = [];
-			for (int i = 0; i < sequence.Count - 1; i++) {
-				differences.Add(sequence[i + 1] - sequence[i]);
-			}
-			sequences.Add(differences);
-		} while (differences.Any(x => x != 0));
-
-		return sequences;
+			differences = [.. differences.Zip(differences.Skip(1)).Select(x => x.Second - x.First)];;
+			yield return differences;
+		} while (NotAllZeroes(differences));
 	}
+	
+	private static readonly Predicate<IEnumerable<int>> NotAllZeroes = (IEnumerable<int> sequence) => (!sequence.All(x => x == 0));
 }
