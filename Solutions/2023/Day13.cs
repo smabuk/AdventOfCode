9072a063726f501e2caf73ac2601151f1fa655c2
smabuk <2011834+smabuk@users.noreply.github.com>
2023-12-13 09:30:35 +0000

    2023 Day13 Solved

--- Solutions/2023/Day13.cs
+++ Solutions/2023/Day13.cs
@@ -9,25 +9,24 @@ public sealed partial class Day13 {
 
 	[Init]
 	public static   void  Init(string[] input, params object[]? args) => LoadPatterns(input);
-	public static string Part1(string[] input, params object[]? args) => Solution1().ToString();
+	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
 	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
 
-	private static readonly List<Pattern> _patterns = [];
+	public const char MIRROR = '#';
+	public const char ASH    = '.';
 
-	private static void LoadPatterns(string[] input) {
+	private static int Solution1(string[] input) => LoadPatterns(input).Sum(pattern => pattern.Value);
+
+	private static int Solution2(string[] input) => LoadPatterns(input).Sum(pattern => pattern.AlternateValue);
+
+	private static IEnumerable<Pattern> LoadPatterns(string[] input)
+	{
 		int inputIndex = 0;
 		while (inputIndex < input.Length) {
 			Pattern pattern = string.Join(Environment.NewLine, input[inputIndex..]).As<Pattern>();
-			_patterns.Add(pattern);
+			yield return pattern;
 			inputIndex += pattern.NoOfRows + 1;
 		}
-	}
-
-	private static int Solution1() => _patterns.Sum(pattern => pattern.Value);
-
-	private static string Solution2(string[] input) {
-		List<Pattern> instructions = [.. input.As<Pattern>()];
-		return "** Solution not written yet **";
 	}
 
 	private sealed record Pattern(List<Point> Mirrors) : IParsable<Pattern> {
@@ -35,73 +34,108 @@ public sealed partial class Day13 {
 		public int NoOfColumns = Mirrors.Max(mirror => mirror.X) + 1;
 		public int NoOfRows    = Mirrors.Max(mirror => mirror.Y) + 1;
 
-		private LineOfSymmetry lineOfSymmetry = LineOfSymmetry.None;
-		private int left  = 0;
-		private int above = 0;
+		public int Value => LineOfSymmetryValue();
+		public int AlternateValue => AlternateLineOfSymmetryValue();
 
-		public int Value
-		{
-			get
-				{
-					if (lineOfSymmetry is LineOfSymmetry.None) {
-						CalculateLineOfSymmetry();
-					}
-					return left + (100 * above);
-				}
-		}
-
-		private void CalculateLineOfSymmetry()
+		private int LineOfSymmetryValue()
 		{
 			string[] columnStrings = [..Enumerable.Range(0, NoOfColumns)
 				.Select(x => Mirrors.Where(m => m.X == x).Select(m => m.Y))
-				.Select(v => v.ValuesAsString())];
+				.Select(v => v.ValuesAsString(NoOfRows))];
 
 			for (int col = 0; col < NoOfColumns - 1; col++) {
-				if (columnStrings[col] != columnStrings[col+1]) {
-					continue;
-				}
-				for (int i = 1; true; i++) {
-					if (col + i + 1 >= NoOfColumns || col - i < 0) {
-						lineOfSymmetry = LineOfSymmetry.Vertical;
-						left = col + 1;
-						return;
-					}
-					if (columnStrings[col - i] != columnStrings[col + i + 1]) {
-						break;
-					}
+				if (IsSymmetrical(columnStrings, col, NoOfColumns, out int value)) {
+					return value;
 				}
 			}
 
 			string[] rowStrings = [..Enumerable.Range(0, NoOfRows)
 				.Select(y => Mirrors.Where(m => m.Y == y).Select(m => m.X))
-				.Select(v => v.ValuesAsString())];
+				.Select(v => v.ValuesAsString(NoOfColumns))];
 
 
 			for (int row = 0; row < NoOfRows - 1; row++) {
-				if (rowStrings[row] != rowStrings[row + 1]) {
-					continue;
+				if (IsSymmetrical(rowStrings, row, NoOfRows, out int value)) {
+					return value * 100;
 				}
-				for (int i = 1; true; i++) {
-					if (row + i + 1 >= NoOfRows || row - i < 0) {
-						lineOfSymmetry = LineOfSymmetry.Horizontal;
-						above = row + 1;
-						return;
-					}
-					if (rowStrings[row - i] != rowStrings[row + i + 1]) {
-						break;
-					}
+			}
+
+			throw new ApplicationException("Oh no!!");
+		}
+		private int AlternateLineOfSymmetryValue()
+		{
+			int oldValue = Value;
+
+			string[] columnStrings = [..Enumerable.Range(0, NoOfColumns)
+				.Select(x => Mirrors.Where(m => m.X == x).Select(m => m.Y))
+				.Select(v => v.ValuesAsString(NoOfRows))];
+
+			for (int col = 0; col < NoOfColumns - 1; col++) {
+				if (IsSymmetrical(columnStrings, col, NoOfColumns, out int value, true) && value != oldValue) {
+					return value;
+				}
+			}
+
+			string[] rowStrings = [..Enumerable.Range(0, NoOfRows)
+				.Select(y => Mirrors.Where(m => m.Y == y).Select(m => m.X))
+				.Select(v => v.ValuesAsString(NoOfColumns))];
+
+			for (int row = 0; row < NoOfRows - 1; row++) {
+				if (IsSymmetrical(rowStrings, row, NoOfRows, out int value, true) && (value * 100) != oldValue) {
+					return value * 100;
 				}
 			}
 
 			throw new ApplicationException("Oh no!!");
 		}
 
+		private static bool IsSymmetrical(string[] strings, int index, int max, out int value, bool fuzzyTest = false)
+		{
+			value = 0;
+			int firstPosition = -1;
+			if (!TryFuzzyTest(strings[index], strings[index + 1], out int position, fuzzyTest)) {
+				return false;
+			} else  if (fuzzyTest && position >= 0) {
+				firstPosition = position;
+			}
+			for (int i = 1; true; i++) {
+				if (index + i + 1 >= max || index - i < 0) {
+					value = index + 1;
+					return true;
+				}
+				if (!TryFuzzyTest(strings[index - i], strings[index + i + 1], out position, fuzzyTest)) {
+					break;
+				} else if (fuzzyTest && position >= 0) {
+					if (firstPosition >= 0) {
+						break;
+					}
+					firstPosition = position;
+				}
+			}
+			return false;
+		}
+
+		private static bool TryFuzzyTest(string string1, string string2, out int position, bool fuzzyTest = false)
+		{
+			position = -1;
+			if (string1 == string2) { return true; }
+			if (fuzzyTest is false) { return false; }
+
+			for (int i = 0; i < string1.Length; i++) {
+				if (string1[i] != string2[i]) {
+					if (position >= 0) {
+						return false;
+					}
+					position = i;
+				}
+			}
+
+			return true;
+		}
 		private static bool IsNotABlankLine(string s) => !string.IsNullOrWhiteSpace(s);
 
 		public static Pattern Parse(string s, IFormatProvider? provider)
 		{
-			const char MIRROR = '#';
-			
 			return new([.. s
 				.Split(Environment.NewLine)
 				.TakeWhile(IsNotABlankLine)
@@ -124,9 +158,11 @@ public sealed partial class Day13 {
 
 public static class Day13Helpers
 {
-	public static string ValuesAsString(this IEnumerable<int> values)
+	public static string ValuesAsString(this IEnumerable<int> values, int maxValue)
 	{
-		return string.Join(",", values.Select(value => value.ToString()));
+		HashSet<int> valuesSet = [.. values];
+		return string.Join("", Enumerable.Range(0, maxValue).Select(i => valuesSet.Contains(i) ? Day13.MIRROR : Day13.ASH));
 	}
+
 }
 
--- Tests/2023/Tests_13.cs
+++ Tests/2023/Tests_13.cs
@@ -29,4 +29,12 @@ public class Tests_13_Point_of_Incidence
 		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
 		actual.ShouldBe(expected);
 	}
+
+	[Theory]
+	[InlineData(TEST_DATA, 400)]
+	public void Part2(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART2, input), out int actual);
+		actual.ShouldBe(expected);
+	}
 }
