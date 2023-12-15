44006f379c8ffba18dbe99ec872ebb42d7fa7158
smabuk <2011834+smabuk@users.noreply.github.com>
2023-12-15 06:32:38 +0000

    2023 Day15 Part1 Solved

--- Solutions/2023/Day15.cs
+++ Solutions/2023/Day15.cs
@@ -1,49 +1,44 @@
 ﻿namespace AdventOfCode.Solutions._2023;
 
 /// <summary>
-/// Day 15: Title
+/// Day 15: Lens Library
 /// https://adventofcode.com/2023/day/15
 /// </summary>
-[Description("")]
+[Description("Lens Library")]
 public sealed partial class Day15 {
 
-	[Init]
-	public static   void  Init(string[] input, params object[]? args) => LoadInstructions(input);
 	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
 	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
 
-	private static IEnumerable<Instruction> _instructions = [];
-
-	private static void LoadInstructions(string[] input) {
-		_instructions = input.As<Instruction>();
-	}
-
-	private static string Solution1(string[] input) {
-		List<Instruction> instructions = [.. input.As<Instruction>()];
-		return "** Solution not written yet **";
+	private static int Solution1(string[] input) {
+		return input[0]
+			.TrimmedSplit(",")
+			.As<Step>()
+			.Sum(step => step.HashNumber);
 	}
 
 	private static string Solution2(string[] input) {
-		List<Instruction> instructions = [.. input.As<Instruction>()];
+		List<Step> initializationSequence = [.. input.As<Step>()];
 		return "** Solution not written yet **";
 	}
 
-	private sealed record Instruction(string Name, int Value) : IParsable<Instruction> {
-		public static Instruction Parse(string s, IFormatProvider? provider)
+	private sealed record Step(string Name) : IParsable<Step> {
+
+		public int HashNumber = GetHashNumber(Name);
+
+		private static int GetHashNumber(string s)
 		{
-			//MatchCollection match = InputRegEx().Matches(input);
-			Match match = InputRegEx().Match(s);
-			if (match.Success) {
-				return new(match.Groups["opts"].Value, match.As<int>("number"));
+			int result = 0;
+			foreach (char c in s) {
+				result += c;
+				result *= 17;
+				result %= 256;
 			}
-			return null!;
+			return result;
 		}
 
-		public static Instruction Parse(string s) => Parse(s, null);
-		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Instruction result)
-			=> ISimpleParsable<Instruction>.TryParse(s, provider, out result);
+		public static Step Parse(string s, IFormatProvider? provider) => new(s);
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Step result)
+			=> ISimpleParsable<Step>.TryParse(s, provider, out result);
 	}
-
-	[GeneratedRegex("""(?<opts>opt1|opt2|opt3) (?<number>[\+\-]?\d+)""")]
-	private static partial Regex InputRegEx();
 }
--- Tests/2023/Tests_15.cs
+++ Tests/2023/Tests_15.cs
@@ -1,15 +1,26 @@
 ﻿namespace AdventOfCode.Tests.Year2023;
 
-public class Tests_15
+public class Tests_15_Lens_Library
 {
 	const int DAY = 15;
 
 	private const string TEST_DATA = """
-		
+		rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
 		""";
 
 	[Theory]
-	[InlineData(TEST_DATA, 9999)]
+	[InlineData(TEST_DATA, 1320)]
+	[InlineData("rn=1",      30)]
+	[InlineData("cm-",      253)]
+	[InlineData("qp=3",      97)]
+	[InlineData("cm=2",      47)]
+	[InlineData("qp-",       14)]
+	[InlineData("pc=4",     180)]
+	[InlineData("ot=9",       9)]
+	[InlineData("ab=5",     197)]
+	[InlineData("pc-",       48)]
+	[InlineData("pc=6",     214)]
+	[InlineData("ot=7",     231)]
 	public void Part1(string input, int expected)
 	{
 		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
