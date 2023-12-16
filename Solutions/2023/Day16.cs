5ad866c6283ddd5cd871a929bd60558c46308481
smabuk <2011834+smabuk@users.noreply.github.com>
2023-12-16 11:32:14 +0000

    2023 Day16 Solved

--- Solutions/2023/Day16.cs
+++ Solutions/2023/Day16.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode.Solutions._2023;
+﻿using static AdventOfCode.Solutions._2023.Day16.Direction;
+
+namespace AdventOfCode.Solutions._2023;
 
 /// <summary>
 /// Day 16: The Floor Will Be Lava
@@ -7,7 +9,7 @@
 [Description("The Floor Will Be Lava")]
 public sealed partial class Day16 {
 
-	public static string Part1(string[] input, Action<string[], bool>? visualise = null, params object[]? args) => Solution1(input, visualise).ToString();
+	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
 	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
 
 	public const char SPLITTER_H = '-';
@@ -20,121 +22,99 @@ public sealed partial class Day16 {
 	public static readonly char[] MIRRORS   = [MIRROR_1, MIRROR_2]; 
 	public static readonly char[] SPLITTERS = [SPLITTER_H, SPLITTER_V]; 
 	public static readonly char[] OBSTACLES = [.. MIRRORS, .. SPLITTERS];
-	public static Action<string[], bool>? _visualise = null;
 
-	private static int Solution1(string[] input, Action<string[], bool>? visualise = null) {
-		_visualise = visualise;
+	private static int Solution1(string[] input) {
 		char[,] cave = input.To2dArray();
 
-		Beam beam = new(new(0, 0), Direction.Right);
-		HashSet<Beam> beams = [.. Beam.Energise(cave, beam, [])];
+		Point point = new(0, 0);
+		Beam  beam  = new(point, Right);
 
-		int noOfEnergisedTiles = beams.Select(b => b.Position).Distinct().Count();
-		return noOfEnergisedTiles;
+		return Energise(cave, beam).Count;
 	}
 
-	private static string Solution2(string[] input) {
-		return "** Solution not written yet **";
-	}
+	private static int Solution2(string[] input) {
+		char[,] cave = input.To2dArray();
 
-	private record struct Beam(Point Position, Direction Direction)
-	{
-		public static HashSet<Beam> Energise(char[,] cave, Beam thisBeam, HashSet<Beam> visited)
+		object? tileLock = 1;
+		int size = cave.ColsCount();
+		int maxTiles = int.MinValue;
+		_ = Parallel.For(0, size * 4, (i, state) =>
 		{
-			int count = 0;
+			Beam beam = GetStartBeam(i, size);
+			int tiles = Energise(cave, beam).Count;
+			lock (tileLock) {
+				maxTiles = int.Max(maxTiles, tiles);
+			}
+		});
 
-			while (true && count++ <1000) {
-				visited = [..visited, thisBeam];
+		return maxTiles;
+	}
 
-				if (cave[thisBeam.Position.X, thisBeam.Position.Y] is EMPTY) {
-					cave[thisBeam.Position.X, thisBeam.Position.Y] = thisBeam.Direction switch
-					{
-						Direction.Left  => '<',
-						Direction.Right => '>',
-						Direction.Up    => '^',
-						Direction.Down  => 'v',
-						_ => throw new NotImplementedException(),
-					};
-				} else if (cave[thisBeam.Position.X, thisBeam.Position.Y] is '<' or '>' or '^' or 'v') {
-					cave[thisBeam.Position.X, thisBeam.Position.Y] = '2';
-				} else if (cave[thisBeam.Position.X, thisBeam.Position.Y] is '2' or '3' or '4' or '5') {
-					cave[thisBeam.Position.X, thisBeam.Position.Y] = (char)(cave[thisBeam.Position.X, thisBeam.Position.Y] + 1);
-				}
-				DisplayGrid(cave, "", _visualise);
+	private static Beam GetStartBeam(int i, int size)
+	{
+		Direction direction = (Direction)(i / size);
+		int start = i % size;
+		Point point = direction switch
+		{
+			Left => new(size - 1, start),
+			Right => new(0, start),
+			Up => new(start, size - 1),
+			Down => new(start, 0),
+			_ => throw new NotImplementedException(),
+		};
+		Beam beam = new(point, direction);
+		return beam;
+	}
 
-				char obstacle = cave[thisBeam.Position.X, thisBeam.Position.Y];
-				Direction newDirection = obstacle switch
-				{
-					MIRROR_1   when thisBeam.Direction is Direction.Left                    => Direction.Down,
-					MIRROR_1   when thisBeam.Direction is Direction.Up                      => Direction.Right,
-					MIRROR_1   when thisBeam.Direction is Direction.Right                   => Direction.Up,
-					MIRROR_1   when thisBeam.Direction is Direction.Down                    => Direction.Left,
-					MIRROR_2   when thisBeam.Direction is Direction.Right                   => Direction.Down,
-					MIRROR_2   when thisBeam.Direction is Direction.Up                      => Direction.Left,
-					MIRROR_2   when thisBeam.Direction is Direction.Left                    => Direction.Up,
-					MIRROR_2   when thisBeam.Direction is Direction.Down                    => Direction.Right,
-					SPLITTER_H when thisBeam.Direction is Direction.Down or Direction.Up    => Direction.Left,
-					SPLITTER_V when thisBeam.Direction is Direction.Left or Direction.Right => Direction.Up,
-					_ => thisBeam.Direction,
-				};
+	private static HashSet<Point> Energise(char[,] cave, Beam beam)
+	{
+		HashSet<Beam> visited = [];
+		Queue<Beam> queue = new([beam]);
 
+		while (queue.TryDequeue(out beam)) {
+			_ = visited.Add(beam);
+
+			char obstacle = cave[beam.Position.X, beam.Position.Y];
+			Direction[] newDirections = obstacle switch
+			{
+//				   /
+				MIRROR_1 when beam.Direction is Left  => [Down],
+				MIRROR_1 when beam.Direction is Up    => [Right],
+				MIRROR_1 when beam.Direction is Right => [Up],
+				MIRROR_1 when beam.Direction is Down  => [Left],
+
+//				   \
+				MIRROR_2 when beam.Direction is Right => [Down],
+				MIRROR_2 when beam.Direction is Up    => [Left],
+				MIRROR_2 when beam.Direction is Left  => [Up],
+				MIRROR_2 when beam.Direction is Down  => [Right],
+
+				SPLITTER_H when beam.Direction is Down or Up    => [Left, Right],
+				SPLITTER_V when beam.Direction is Left or Right => [Up, Down],
+				_ => [beam.Direction],
+			};
+
+			foreach (Direction newDirection in newDirections) {
 				Point newPosition = newDirection switch
 				{
-					Direction.Left  => thisBeam.Position.Left(),
-					Direction.Right => thisBeam.Position.Right(),
-					Direction.Up    => thisBeam.Position.Up(),
-					Direction.Down  => thisBeam.Position.Down(),
+					Left  => beam.Position.Left(),
+					Right => beam.Position.Right(),
+					Up    => beam.Position.Up(),
+					Down  => beam.Position.Down(),
 					_ => throw new NotImplementedException(),
 				};
 
-				if (thisBeam.Direction != newDirection && SPLITTERS.Contains(obstacle)) {
-					if (cave.InBounds(newPosition.X, newPosition.Y)) {
-						Beam newBeam = new(newPosition, newDirection);
-						if (!visited.Contains(newBeam)) {
-							visited = [.. visited, .. Energise(cave, newBeam, visited)];
-						}
-					}
-					Direction splitDirection = obstacle == SPLITTER_H ? Direction.Right : Direction.Down;
-					Point splitPosition = splitDirection switch
-					{
-						Direction.Left  => thisBeam.Position.Left(),
-						Direction.Right => thisBeam.Position.Right(),
-						Direction.Up    => thisBeam.Position.Up(),
-						Direction.Down  => thisBeam.Position.Down(),
-						_ => throw new NotImplementedException(),
-					};
-					if (cave.InBounds(splitPosition.X, splitPosition.Y)) {
-						Beam splitBeam = new(splitPosition, splitDirection);
-						if (!visited.Contains(splitBeam)) {
-							visited = [.. visited, .. Energise(cave, splitBeam, visited)];
-						}
-					}
-					return visited;
+				Beam possibleBeam = new(newPosition, newDirection);
+				if (cave.InBounds(newPosition) && !visited.Contains(possibleBeam)) {
+					queue.Enqueue(possibleBeam);
 				}
-
-				if (cave.OutOfBounds(newPosition.X, newPosition.Y) || visited.Contains(new Beam(new(newPosition.X, newPosition.Y), newDirection))) {
-					return visited;
-				}
-
-				if (visited.Contains(new(newPosition, newDirection))) {
-					return visited;
-				}
-				thisBeam.Position  = newPosition;
-				thisBeam.Direction = newDirection;
-			}
-			return visited;
-		}
-		private static void DisplayGrid(char[,] grid, string title, Action<string[], bool>? visualise)
-		{
-			if (visualise is not null) {
-				string[] output = ["", title, "0123456789", .. grid.PrintAsStringArray(0)];
-				_ = Task.Run(() => visualise?.Invoke(output, false));
 			}
 		}
 
+		return [.. visited.Select(b => b.Position)];
 	}
 
+	private record struct Beam(Point Position, Direction Direction);
 
 	public enum Direction { Left, Right, Up, Down }
-
 }
--- Tests/2023/Tests_16.cs
+++ Tests/2023/Tests_16.cs
@@ -1,6 +1,6 @@
 ﻿namespace AdventOfCode.Tests.Year2023;
 
-public class Tests_16_The_Floor_Will_Be_Lava(ITestOutputHelper testOutputHelper)
+public class Tests_16_The_Floor_Will_Be_Lava
 {
 	const int DAY = 16;
 
@@ -21,27 +21,16 @@ public class Tests_16_The_Floor_Will_Be_Lava(ITestOutputHelper testOutputHelper)
 	[InlineData(TEST_DATA, 46)]
 	public void Part1(string input, int expected)
 	{
-		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input, new Action<string[], bool>(Callback)), out int actual);
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
 		actual.ShouldBe(expected);
 	}
 
-	//[Theory]
-	//[InlineData(TEST_DATA, 9999)]
-	//public void Part2(string input, int expected)
-	//{
-	//	_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART2, input), out int actual);
-	//	actual.ShouldBe(expected);
-	//}
-
-
-	private void Callback(string[] lines, bool _)
+	[Theory]
+	[InlineData(TEST_DATA, 51)]
+	public void Part2(string input, int expected)
 	{
-		if (lines is null or []) {
-			return;
-		}
-
-		testOutputHelper.WriteLine(string.Join(Environment.NewLine, lines));
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART2, input), out int actual);
+		actual.ShouldBe(expected);
 	}
-
 
 }
