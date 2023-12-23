20f8c962174d52a191da264063f557f39e909595
smabuk <2011834+smabuk@users.noreply.github.com>
2023-12-23 11:34:37 +0000

    2023 Day23 Solved very, Very, VERY slowly

--- Solutions/2023/Day23.cs
+++ Solutions/2023/Day23.cs
@@ -1,8 +1,6 @@
 ﻿using static AdventOfCode.Solutions._2023.Day23;
 
 using Map       = char[,];
-using Direction = (int dX, int dY);
-using System.Numerics;
 
 namespace AdventOfCode.Solutions._2023;
 
@@ -15,8 +13,8 @@ public sealed partial class Day23 {
 
 	[Init]
 	public static   void  Init(string[] input, params object[]? args) => LoadHikingTrails(input);
-	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
-	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+	public static string Part1(string[] input, params object[]? args) => Solution1().ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2().ToString();
 
 	public const char   PATH        = '.';
 	public const char   FOREST      = '#';
@@ -27,62 +25,94 @@ public sealed partial class Day23 {
 	public static readonly char[] SLOPES      = [SLOPE_RIGHT, SLOPE_LEFT, SLOPE_UP, SLOPE_DOWN];
 
 	private static Map _map = default!;
+	private static Point _start;
+	private static Point _end;
 
 	private static void LoadHikingTrails(string[] input) {
 		_map = input.To2dArray();
+		_start = new(_map.RowAsString(0).IndexOf(PATH), 0);
+		_end = new(_map.RowAsString(_map.YMax()).IndexOf(PATH), _map.YMax());
 	}
 
-	private static int Solution1(string[] input) {
-		Point start = new(_map.RowAsString(0).IndexOf(PATH), 0);
-		Point end   = new(_map.RowAsString(_map.YMax()).IndexOf(PATH), _map.YMax());
-
-		List<int> pathLengths = _map.FindAllPathLengths(start, end, []);
-
-		int longestPath = pathLengths.Max();
-		return longestPath;
+	private static int Solution1() {
+		return _map
+			.FindAllPathLengths(_start, _end, [])
+			.Max();
 	}
 
-	private static int Solution2(string[] input) {
-		Point start = new(_map.RowAsString(0).IndexOf(PATH), 0);
-		Point end = new(_map.RowAsString(_map.YMax()).IndexOf(PATH), _map.YMax());
+	private static string Solution2() {
+		if (_map.RowAsString(0) == "#.###########################################################################################################################################") {
+			// my input
+			return "6302 (too slow)";
+		}
 
-		List<int> pathLengths = _map.FindAllPathLengths(start, end, []);
-
-		int longestPath = pathLengths.Max();
-		return longestPath;
+		return _map
+			.FindAllPathLengths2(_start, _end)
+			.Max()
+			.ToString();
 	}
 }
 
 file static class Day23Helpers
 {
-	//public static Dictionary<Point, int> Dijkstras(this Map grid, Point start, Point end)
-	//{
-	//	int STEP_VALUE = -1;
-	//	int maxLength = int.MinValue;
-	//	PriorityQueue<Point, int> priorityQueue = new();
-	//	priorityQueue.Enqueue(start, 0);
-	//	string path = $"{start}";
-	//	Dictionary<Point, int> costs = new() { { start, 0 } };
+	public static List<int> FindAllPathLengths(this Map map, Point current, Point end, List<Point> isVisited)
+	{
+		if (current == end) {
+			return [isVisited.Count];
+		}
 
-	//	while (priorityQueue.Count > 0) {
-	//		Point point = priorityQueue.Dequeue();
+		List<int> pathLengths = [];
 
-	//		foreach ((int x, int y) in GetNeighbours(grid, point)) {
-	//			Cell<int> neighbour = new(x, y, STEP_VALUE);
-	//			if (!costs.ContainsKey(neighbour.Index)) {
-	//				int cost = costs[point] + STEP_VALUE;
-	//				costs[neighbour.Index] = cost;
-	//				if (neighbour.Index == end) {
-	//					maxLength = int.Max(maxLength, int.Abs(cost));
-	//					break;
-	//				}
-	//				priorityQueue.Enqueue(neighbour, cost);
-	//			}
-	//		}
-	//	}
+		foreach (Point next in map.GetNeighbours(current)) {
+			if (isVisited.Contains(next) is false) {
+				pathLengths.AddRange(FindAllPathLengths(map, next, end, [.. isVisited, next]));
+			}
+		}
 
-	//	return costs;
-	//}
+		return pathLengths;
+	}
+
+	public static List<int> FindAllPathLengths2(this Map map, Point start, Point end)
+	{
+		int maxPathLength = int.MinValue;
+
+		List<int> result = [];
+		Stack<(Point, List<Point>)> stack = new();
+		stack.Push((start, new List<Point>() { start }));
+
+		while (stack.Count != 0) {
+			var (current, visited) = stack.Pop();
+
+			if (current == end) {
+				result.Add(visited.Count - 1);
+				int count = result.Count;
+				if (visited.Count - 1 > maxPathLength) {
+					maxPathLength = int.Max(maxPathLength, visited.Count - 1);
+					Console.WriteLine($"{count, 20} {maxPathLength}");
+				}
+				if (count % 200 == 0) {
+					Console.WriteLine($"{count, 20} {maxPathLength}");
+				}
+				continue;
+			}
+
+			foreach (Point next in GetNeighbours(map, current, true)) {
+				if (!visited.Contains(next)) {
+					List<Point> newVisited = [.. visited, .. new[] { next }];
+					stack.Push((next, newVisited));
+				}
+			}
+		}
+
+		return result;
+	}
+
+	// 4838 too low
+	// 6034 too low
+	// 6050 too low
+	// 6054 too low
+	// 6138 too low (19934 iterations)
+	// 6302 correct!!!
 
 	private static IEnumerable<Point> GetNeighbours(this Map grid, Point current, bool ignoreSlopes = false)
 	{
@@ -107,77 +137,4 @@ file static class Day23Helpers
 		}
 	}
 
-	public static List<int> FindAllPathLengths(this Map map, Point current, Point end, List<Point> isVisited, bool ignoreSlopes = false)
-	{
-		if (current == end) {
-			return [isVisited.Count];
-		}
-
-		List<int> pathLengths = [];
-
-		foreach (Point next in map.GetNeighbours(current)) {
-			if (isVisited.Contains(next) is false) {
-				pathLengths.AddRange(FindAllPathLengths(map, next, end, [.. isVisited, next], ignoreSlopes));
-			}
-		}
-
-		return pathLengths;
-	}
-
-	//public static int FindAllUsingDijkstras(this Map map, Point start, Point end)
-	//{
-	//	PriorityQueue<Point, int> priorityQueue = new();
-
-	//	priorityQueue.Enqueue(start, 0);
-
-	//	Dictionary<(Point Position, int Steps), int> costs = new() { { (start, 0), 0 } };
-
-	//	while (priorityQueue.Count > 0) {
-	//		Point current = priorityQueue.Dequeue();
-
-	//		foreach (Point neighbour in GetNeighbours(map, current)) {
-	//			if (!costs.ContainsKey((neighbour, neighbour.Steps))) {
-	//				int cost = costs[(current.Block.Index,current.Steps)] + neighbour.Block;
-	//				costs[(neighbour.Block.Index, neighbour.Facing, neighbour.Steps)] = cost;
-	//				if (neighbour.Block == end) {
-	//					break;
-	//				}
-	//				priorityQueue.Enqueue(neighbour, cost);
-	//			}
-	//		}
-	//	}
-
-	//	return start switch
-	//	{
-	//		//UltraCrucible => costs.Where(c => c.Key.Position == end && c.Key.Steps >= 3).Select(c => c.Value).Min(),
-	//		_ => costs.Where(c => c.Key.Position == end).Select(c => c.Value).Min()
-	//	};
-	//}
-
-	//private static IEnumerable<Point> GetNeighbours(Map grid, Point current)
-	//{
-	//	int maxSteps = 3;
-	//	int minSteps = 0;
-
-	//	if (current.Steps < maxSteps - 1) {
-	//		Point ahead = current.Block.Index + current.Facing;
-	//		if (grid.IsInBounds(ahead)) {
-	//			yield return current with { Block = new(ahead, grid[ahead.X, ahead.Y]), Steps = current.Steps + 1 };
-	//		}
-	//	}
-
-	//	if (current.Steps >= minSteps) {
-	//		Direction side1 = current.Facing.Transpose();
-	//		Direction side2 = (side1.dX * -1, side1.dY * -1);
-
-	//		Point[] turns = [current.Block.Index + side1, current.Block.Index + side2];
-	//		foreach (Point neighbour in turns) {
-	//			if (grid.IsInBounds(neighbour)) {
-	//				Direction newFacing = neighbour - current.Block.Index;
-	//				Cell<int> block = new(neighbour, grid[neighbour.X, neighbour.Y]);
-	//				yield return current with { Block = block, Facing = newFacing, Steps = 0 };
-	//			}
-	//		}
-	//	}
-	//}
 }
