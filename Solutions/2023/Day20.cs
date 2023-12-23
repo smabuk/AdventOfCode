c5dd1465145b1bbdf44f8fb17e6d812007a3eeef
smabuk <2011834+smabuk@users.noreply.github.com>
2023-12-23 23:24:46 +0000

    2023 Day20 Part1 Solved

--- Solutions/2023/Day20.cs
+++ Solutions/2023/Day20.cs
@@ -7,27 +7,80 @@
 [Description("Pulse Propagation")]
 public sealed partial class Day20 {
 
-	[Init]
-	public static   void  Init(string[] input, params object[]? args) => LoadModules(input);
-	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
+	public static string Part1(string[] input, params object[]? args)
+	{
+		int noOfButtonPushes = GetArgument(args, argumentNumber: 1, defaultResult: 1000);
+		return Solution1(input, noOfButtonPushes).ToString();
+	}
+
 	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
 
-	private static IEnumerable<Module> _modules = [];
+	public const bool LOW_PULSE  = false;
+	public const bool HIGH_PULSE = true;
+	public const bool OFF = false;
+	public const bool ON  = true;
 
-	private static void LoadModules(string[] input) {
-		_modules = input.As<Module>();
+
+	private static long Solution1(string[] input, int noOfButtonPushes) {
+		Dictionary<string, Module> modules = input.As<Module>().ToDictionary(m => m.Name);
+		Queue<Pulse> queue = [];
+
+		List<string> existingModules = [.. modules.Keys];
+		foreach (string moduleName in existingModules) {
+			Module module = modules[moduleName];
+			foreach (string name in module.Outputs) {
+				if (existingModules.DoesNotContain(name)) {
+					modules.Add(name, new Output(name));
+				}
+				modules[name].Inputs.Add(module.Name, LOW_PULSE);
+			}
+		}
+
+		int noOfLowPulses  = 0;
+		int noOfHighPulses = 0;
+		for (int i = 0; i < noOfButtonPushes; i++) {
+			Button.Push(queue);
+			(int lowPulses, int highPulses) = ProcessQueue(queue, modules);
+			noOfLowPulses  += lowPulses;
+			noOfHighPulses += highPulses;
+		}
+
+		return noOfLowPulses * noOfHighPulses;
 	}
 
-	private static string Solution1(string[] input) {
-		List<Module> modules = [.. input.As<Module>()];
+	private static string Solution2(string[] input)
+	{
 		return "** Solution not written yet **";
 	}
 
-	private static string Solution2(string[] input) {
-		return "** Solution not written yet **";
+	private static (int lowPulses, int highPulses) ProcessQueue(Queue<Pulse> queue, Dictionary<string, Module> modules)
+	{
+		int lowPulses  = 0;
+		int highPulses = 0;
+
+		while (queue.Count > 0) {
+			Pulse pulse = queue.Dequeue();
+
+			if (pulse.Payload == LOW_PULSE) {
+				lowPulses++;
+			} else {
+				highPulses++;
+			}
+
+			foreach (Pulse newPulse in modules[pulse.Destination].Receive(pulse)) {
+				queue.Enqueue(newPulse);
+			}
+		}
+
+		return (lowPulses, highPulses);
 	}
 
-	private record Module(string Name, ModuleType ModuleType, List<string> Outputs) : IParsable<Module> {
+	private record Module(string Name, List<string> Outputs) : IParsable<Module> {
+
+		public Dictionary<string, bool> Inputs { get; set; } = [];
+		
+		public virtual IEnumerable<Pulse> Receive(Pulse pulse) { yield break; }
+
 		public static Module Parse(string s, IFormatProvider? provider)
 		{
 			ModuleType moduleType = (ModuleType)s[0];
@@ -35,7 +88,6 @@ public sealed partial class Day20 {
 			string[] tokens = s.TrimmedSplit(splitBy);
 			return moduleType switch
 			{
-
 				ModuleType.Broadcast   => new Broadcaster(tokens[0], [.. tokens[1..]]),
 				ModuleType.Conjunction => new ConjunctionModule(tokens[0][1..], [.. tokens[1..]]),
 				ModuleType.FlipFlop    => new FlipFlopModule(tokens[0][1..], [.. tokens[1..]]),
@@ -48,10 +100,54 @@ public sealed partial class Day20 {
 			=> ISimpleParsable<Module>.TryParse(s, provider, out result);
 	}
 
-	private sealed record Broadcaster(string Name, List<string> Outputs) : Module(Name, ModuleType.Broadcast, Outputs);
-	private sealed record ConjunctionModule(string Name, List<string> Outputs) : Module(Name, ModuleType.Conjunction, Outputs);
-	private sealed record FlipFlopModule(string Name, List<string> Outputs) : Module(Name, ModuleType.FlipFlop, Outputs);
-	private sealed record Output(string Name) : Module(Name, ModuleType.Output, []);
+	private sealed record Button(string Name, List<string> Outputs) : Module(Name, Outputs)
+	{
+		public static void Push(Queue<Pulse> queue) => queue.Enqueue(new Pulse(LOW_PULSE, "button", "broadcaster")); 
+	}
+
+	private sealed record Broadcaster(string Name, List<string> Outputs) : Module(Name, Outputs)
+	{
+		public override IEnumerable<Pulse> Receive(Pulse pulse)
+		{
+			foreach (string name in Outputs) {
+				yield return new Pulse(pulse.Payload, Name, name);
+			}
+		}
+	}
+
+	private sealed record ConjunctionModule(string Name, List<string> Outputs) : Module(Name, Outputs)
+	{
+		public override IEnumerable<Pulse> Receive(Pulse pulse)
+		{
+			Inputs[pulse.Source] = pulse.Payload;
+			bool pulseType = !Inputs.Values.All(v => v == HIGH_PULSE);
+
+			foreach (string name in Outputs) {
+				yield return new Pulse(pulseType, Name, name);
+			}
+		}
+	}
+
+	private sealed record FlipFlopModule(string Name, List<string> Outputs) : Module(Name, Outputs)
+	{
+		public bool Status { get; set; } = OFF;
+		public override IEnumerable<Pulse> Receive(Pulse pulse)
+		{
+			if (pulse.Payload == HIGH_PULSE) {
+				yield break;
+			}
+
+			Status = !Status;
+
+			foreach (string name in Outputs) {
+				yield return new Pulse(Status == ON, Name, name);
+			}
+		}
+	}
+
+	private sealed record Output(string Name) : Module(Name, []);
+
+	private sealed record Pulse(bool Payload, string Source, string Destination);
 
 	private enum ModuleType
 	{
--- Tests/2023/Tests_20.cs
+++ Tests/2023/Tests_20.cs
@@ -4,26 +4,31 @@ public class Tests_20_Pulse_Propagation
 {
 	const int DAY = 20;
 
-	private const string TEST_DATA = """
+	[Theory]
+	[InlineData("""
 		broadcaster -> a, b, c
 		%a -> b
 		%b -> c
 		%c -> inv
 		&inv -> a
-		""";
-
-	[Theory]
-	[InlineData(TEST_DATA, 32_000_000)]
+		""", 1, 32)]
+	[InlineData("""
+		broadcaster -> a, b, c
+		%a -> b
+		%b -> c
+		%c -> inv
+		&inv -> a
+		""", 1000, 32_000_000)]
 	[InlineData("""
 		broadcaster -> a
 		%a -> inv, con
 		&inv -> b
 		%b -> con
 		&con -> output
-		""", 11_687_500)]
-	public void Part1(string input, long expected)
+		""", 1000, 11_687_500)]
+	public void Part1(string input, int noOfButtonPushes, long expected)
 	{
-		_ = long.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out long actual);
+		_ = long.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input, noOfButtonPushes), out long actual);
 		actual.ShouldBe(expected);
 	}
 }
