97d30ba5b49fb2a29945cc24d5941bef7db12d8f
smabuk <2011834+smabuk@users.noreply.github.com>
2024-10-31 20:02:13 +0000

    2016 Day01 refactored

--- Solutions/2016/Day01.cs
+++ Solutions/2016/Day01.cs
@@ -1,7 +1,7 @@
 ﻿namespace AdventOfCode.Solutions._2016;
 
 /// <summary>
-/// Day XX: Title
+/// Day 01: No Time for a Taxicab
 /// https://adventofcode.com/2016/day/01
 /// </summary>
 [Description("No Time for a Taxicab")]
@@ -10,27 +10,20 @@ public sealed partial class Day01 {
 	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
 	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
 
+	private static readonly Point _start = new(0, 0);
 
-	private static int Solution1(string[] input) {
-		List<Instruction> instructions = [.. input[0].TrimmedSplit(",").As<Instruction>()];
-
-		Point start = new(0, 0);
-		Person me = new(Direction.Up,	start);
-
-		foreach (Instruction instruction in instructions) {
-			me = Move(me, instruction);
-		}
-
-		return me.Position.ManhattanDistance(start);
-	}
-
+	private static int Solution1(string[] input) =>
+		input[0]
+		.TrimmedSplit(",")
+		.As<Instruction>()
+		.Aggregate(new Person(Direction.North, _start), (person, instruction) => Move(person, instruction))
+		.Position
+		.ManhattanDistance(_start);
 
 	private static int Solution2(string[] input) {
+		HashSet<Point> locations = [_start];
 		List<Instruction> instructions = [.. input[0].TrimmedSplit(",").As<Instruction>()];
-
-		HashSet<Point> locations = [new(0, 0)];
-		Point start = new(0, 0);
-		Person me = new(Direction.Up, start);
+		Person me = new(Direction.North, _start);
 
 		foreach (Instruction instruction in instructions) {
 			Person newMe = Move(me, instruction);
@@ -40,7 +33,7 @@ public sealed partial class Day01 {
 			for (int i = 1; i <= instruction.Value; i++) {
 				Point newLocation = me.Position with { X = me.Position.X + (dX * i) , Y = me.Position.Y +  (dY * i)};
 				if (locations.Contains(newLocation)) {
-					return newLocation.ManhattanDistance(start);
+					return newLocation.ManhattanDistance(_start);
 				}
 				_ = locations.Add(newLocation);
 			}
@@ -49,34 +42,35 @@ public sealed partial class Day01 {
 
 		throw new ApplicationException("Should never reach here");
 	}
+
 	private static Person Move(Person me, Instruction instruction)
 	{
 		Direction newFacing = (me.Facing, instruction.TurnDirection) switch
 		{
-			(Direction.Left, Direction.R) => Direction.Up,
-			(Direction.Up, Direction.R) => Direction.Right,
-			(Direction.Right, Direction.R) => Direction.Down,
-			(Direction.Down, Direction.R) => Direction.Left,
-			(Direction.Left, Direction.L) => Direction.Down,
-			(Direction.Up, Direction.L) => Direction.Left,
-			(Direction.Right, Direction.L) => Direction.Up,
-			(Direction.Down, Direction.L) => Direction.Right,
+			(Direction.West,  Direction.R) => Direction.North,
+			(Direction.North, Direction.R) => Direction.East,
+			(Direction.East,  Direction.R) => Direction.South,
+			(Direction.South, Direction.R) => Direction.West,
+			(Direction.West,  Direction.L) => Direction.South,
+			(Direction.North, Direction.L) => Direction.West,
+			(Direction.East,  Direction.L) => Direction.North,
+			(Direction.South, Direction.L) => Direction.East,
 			_ => throw new NotImplementedException(),
 		};
-		me = me with
+
+		return me with
 		{
 			Facing = newFacing,
 			Position = newFacing switch
 			{
-				(Direction.Left) => me.Position with { X = me.Position.X - instruction.Value },
-				(Direction.Right) => me.Position with { X = me.Position.X + instruction.Value },
-				(Direction.Up) => me.Position with { Y = me.Position.Y - instruction.Value },
-				(Direction.Down) => me.Position with { Y = me.Position.Y + instruction.Value },
+				(Direction.West)  => me.Position with { X = me.Position.X - instruction.Value },
+				(Direction.East)  => me.Position with { X = me.Position.X + instruction.Value },
+				(Direction.North) => me.Position with { Y = me.Position.Y - instruction.Value },
+				(Direction.South) => me.Position with { Y = me.Position.Y + instruction.Value },
 
 				_ => throw new NotImplementedException(),
 			},
 		};
-		return me;
 	}
 
 	private sealed record Instruction(Direction TurnDirection, int Value) : IParsable<Instruction> {
@@ -95,13 +89,13 @@ public sealed partial class Day01 {
 
 	private enum Direction
 	{
-		Left,
-		Right,
-		Up,
-		Down,
+		West,
+		East,
+		North,
+		South,
 
-		L = Left,
-		R = Right,
+		L,
+		R,
 	}
 
 }
