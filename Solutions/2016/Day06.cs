0a2d5577ad31162b12df3723d86ce02467c26330
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-01 13:59:16 +0000

    2016 Day06 Part1 Solved

---
+++ Solutions/2016/Day06.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode.Solutions._2016;
+
+/// <summary>
+/// Day 06: Signals and Noise
+/// https://adventofcode.com/2016/day/06
+/// </summary>
+[Description("Signals and Noise")]
+public sealed partial class Day06 {
+
+	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+
+	private static string Solution1(string[] input) {
+		return new([..
+			Enumerable.Range(0, input[0].Length)
+			.Select(charIndex => 
+				input
+				.Select(msg => msg[charIndex])
+				.CountBy(c => c)
+				.MaxBy(kv => kv.Value)
+				.Key)]
+		);
+	}
+
+	private static string Solution2(string[] input) {
+		return NO_SOLUTION_WRITTEN_MESSAGE;
+	}
+}
---
+++ Tests/2016/Tests_06.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode.Tests.Year2016;
+
+public class Tests_06_Signals_and_Noise
+{
+	const int DAY = 06;
+
+	[Theory]
+	[InlineData("""
+		eedadn
+		drvtee
+		eandsr
+		raavrd
+		atevrs
+		tsrnev
+		sdttsa
+		rasrtv
+		nssdts
+		ntnada
+		svetve
+		tesnvt
+		vntsnd
+		vrdear
+		dvrsen
+		enarar
+		""", "easter")]
+	public void Part1(string input, string expected)
+	{
+		SolutionRouter.SolveProblem(YEAR, DAY, PART1, input)
+			.ShouldBe(expected);
+	}
+}
