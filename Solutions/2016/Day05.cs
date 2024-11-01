df4f87f1e378887cb21338ff1a387326e7c1c03a
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-01 13:26:32 +0000

    2016 Day05 Solved

--- Solutions/2016/Day05.cs
+++ Solutions/2016/Day05.cs
@@ -15,20 +15,51 @@ public sealed partial class Day05 {
 
 	private static string Solution1(string[] input)
 	{
+		const int PASSWORD_CHAR_INDEX = 5;
+		const int PASSWORD_LENGTH = 8;
+
 		string doorId = input[0];
 		string password = "";
-		long i = 0;
-		while (password.Length < 8) {
-			string hex = Convert.ToHexStringLower(MD5.HashData(Encoding.UTF8.GetBytes($"{doorId}{i}")));
-			if (hex.StartsWith("00000")) {
-				password = $"{password}{hex[5]}";
+		foreach (string hex in GetNextHash(doorId)) {
+			password = $"{password}{hex[PASSWORD_CHAR_INDEX]}";
+			if (password.Length >= PASSWORD_LENGTH) {
+				break;
 			}
-			i++;
 		}
+
 		return password;
 	}
 
 	private static string Solution2(string[] input) {
-		return NO_SOLUTION_WRITTEN_MESSAGE;
+		const char PLACE_HOLDER = '_';
+		const int PASSWORD_LENGTH = 8;
+		const int POSITION_INDEX = 5;
+		const int PASSWORD_CHAR_INDEX = 8;
+
+		string doorId = input[0];
+		Span<char> password = new string(PLACE_HOLDER, PASSWORD_LENGTH).ToCharArray();
+		
+		foreach (string hex in GetNextHash(doorId)) {
+			if (int.TryParse($"{hex[POSITION_INDEX]}", out int position) && position < PASSWORD_LENGTH && password[position] == PLACE_HOLDER) {
+				password[position] = hex[PASSWORD_CHAR_INDEX];
+				if (!password.Contains(PLACE_HOLDER)) {
+					break;
+				}
+			}
+		}
+
+		return $"{password}";
+	}
+
+	private static IEnumerable<string> GetNextHash(string doorId)
+	{
+		long i = 0;
+		while (true) {
+			string hex = Convert.ToHexStringLower(MD5.HashData(Encoding.UTF8.GetBytes($"{doorId}{i}")));
+			if (hex.StartsWith("00000")) {
+				yield return hex;
+			}
+			i++;
+		}
 	}
 }
--- Tests/2016/Tests_05.cs
+++ Tests/2016/Tests_05.cs
@@ -11,4 +11,12 @@ public class Tests_05_How_About_a_Nice_Game_of_Chess
 		SolutionRouter.SolveProblem(YEAR, DAY, PART1, input)
 			.ShouldBe(expected);
 	}
+
+	[Theory]
+	[InlineData("abc", "05ace8e3")]
+	public void Part2(string input, string expected)
+	{
+		SolutionRouter.SolveProblem(YEAR, DAY, PART2, input)
+			.ShouldBe(expected);
+	}
 }
