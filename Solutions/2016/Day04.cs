0884a0ad993dc8fa1f193f244e652dc295bec4f3
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-01 11:37:43 +0000

    2016 Day04 Part1 Solved

---
+++ Solutions/2016/Day04.cs
@@ -0,0 +1,54 @@
+﻿namespace AdventOfCode.Solutions._2016;
+
+/// <summary>
+/// Day 04: Security Through Obscurity
+/// https://adventofcode.com/2016/day/04
+/// </summary>
+[Description("Security Through Obscurity")]
+public sealed partial class Day04 {
+
+	public static string Part1(string[] input, params object[]? _) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+
+
+	private static int Solution1(string[] input) =>
+		input
+		.As<Room>()
+		.Where(room => room.IsValid)
+		.Sum(room => room.SectorId);
+
+	private static string Solution2(string[] input) {
+		return NO_SOLUTION_WRITTEN_MESSAGE;
+	}
+
+	private sealed record Room(string EncryptedName, int SectorId, string Checksum) : IParsable<Room> {
+
+		readonly string checksum = string.Join("", 
+			EncryptedName
+			.Replace("-", "")
+			.CountBy(c => c)
+			.OrderByDescending(kv => kv.Value)
+			.ThenBy(kv => kv.Key)
+			.Take(5)
+			.Select(kv => kv.Key));
+
+		public bool IsValid => Checksum == checksum;
+
+		public static Room Parse(string s, IFormatProvider? provider)
+		{
+			Match match = InputRegEx().Match(s);
+			if (match.Success) {
+				return new(match.Groups["encryptedName"].Value, match.As<int>("number"), match.Groups["checksum"].Value);
+			}
+
+			return null!;
+		}
+
+		public static Room Parse(string s) => Parse(s, null);
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Room result)
+			=> ISimpleParsable<Room>.TryParse(s, provider, out result);
+	}
+
+	[GeneratedRegex("""(?<encryptedName>([a-z\-]+)+)\-(?<number>\d+)\[(?<checksum>[a-z]+)\]""")]
+	private static partial Regex InputRegEx();
+}
---
+++ Tests/2016/Tests_04.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode.Tests.Year2016;
+
+public class Tests_04_Security_Through_Obscurity
+{
+	const int DAY = 04;
+
+	[Theory]
+	[InlineData("aaaaa-bbb-z-y-x-123[abxyz]", 123)]
+	[InlineData("a-b-c-d-e-f-g-h-987[abcde]", 987)]
+	[InlineData("not-a-real-room-404[oarel]", 404)]
+	[InlineData("totally-real-room-200[decoy]", 0)]
+	[InlineData("""
+		aaaaa-bbb-z-y-x-123[abxyz]
+		a-b-c-d-e-f-g-h-987[abcde]
+		not-a-real-room-404[oarel]
+		totally-real-room-200[decoy]
+		""", 1514)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
