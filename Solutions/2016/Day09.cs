c22935edc1ca91b13d0d67536ed25ca005201e2d
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-02 19:59:47 +0000

    2016 Day09 Solved

--- Solutions/2016/Day09.cs
+++ Solutions/2016/Day09.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text;
-using static AdventOfCode.Solutions._2016.Day09Constants;
 
+using static AdventOfCode.Solutions._2016.Day09Constants;
 namespace AdventOfCode.Solutions._2016;
 
 /// <summary>
@@ -13,8 +13,16 @@ public sealed partial class Day09 {
 	public static string Part1(string[] input) => Solution1(input).ToString();
 	public static string Part2(string[] input) => Solution2(input).ToString();
 
-	private static int Solution1(string[] input) {
-		string compressed = input[0];
+
+	private static long Solution1(string[] input) => input[0].DecompressV1();
+	private static long Solution2(string[] input) => input[0].DecompressV2();
+
+}
+
+file static class Day09Extensions
+{
+	public static long DecompressV1(this string compressed)
+	{
 		StringBuilder uncompressed = new();
 
 		for (int i = 0; i < compressed.Length; i++) {
@@ -28,6 +36,7 @@ public sealed partial class Day09 {
 				for (int j = 0; j < repeat; j++) {
 					_ = uncompressed.Append(compressed[(closeBracketIndex + 1)..(closeBracketIndex + 1 + chars)]);
 				}
+
 				i = closeBracketIndex + chars;
 			} else {
 				_ = uncompressed.Append(current);
@@ -37,8 +46,31 @@ public sealed partial class Day09 {
 		return uncompressed.Length;
 	}
 
-	private static string Solution2(string[] input) {
-		return NO_SOLUTION_WRITTEN_MESSAGE;
+	public static long DecompressV2(this string compressed)
+	{
+		long decompressedLength = 0;
+
+		for (int i = 0; i < compressed.Length; i++) {
+			char current = compressed[i];
+			if (current == OPEN_BRACKET) {
+				int closeBracketIndex = compressed[i..].IndexOf(CLOSE_BRACKET) + i;
+				string[] tokens = compressed[(i + 1)..closeBracketIndex].Split('x');
+				int chars = tokens[0].As<int>();
+				int repeat = tokens[1].As<int>();
+				string slice = compressed[(closeBracketIndex + 1)..(closeBracketIndex + 1 + chars)];
+				if (slice.Contains(OPEN_BRACKET)) {
+					decompressedLength += slice.DecompressV2() * repeat;
+				} else {
+					decompressedLength += chars * repeat;
+				}
+
+				i = closeBracketIndex + chars;
+			} else {
+				decompressedLength++;
+			}
+		}
+
+		return decompressedLength;
 	}
 }
 
--- Tests/2016/Tests_09.cs
+++ Tests/2016/Tests_09.cs
@@ -15,4 +15,15 @@ public class Tests_09_Explosives_in_Cyberspace
 		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
 		actual.ShouldBe(expected);
 	}
+
+	[Theory]
+	[InlineData("(3x3)XYZ", 9)]
+	[InlineData("X(8x2)(3x3)ABCY", 20)]
+	[InlineData("(27x12)(20x12)(13x14)(7x10)(1x12)A", 241920)]
+	[InlineData("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN", 445)]
+	public void Part2(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART2, input), out int actual);
+		actual.ShouldBe(expected);
+	}
 }
