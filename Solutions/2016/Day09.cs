b31159af4c54c7d4d97ca68f1625763302e3184f
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-02 19:27:37 +0000

    2016 Day09 Part1 Solved

---
+++ Solutions/2016/Day09.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using static AdventOfCode.Solutions._2016.Day09Constants;
+
+namespace AdventOfCode.Solutions._2016;
+
+/// <summary>
+/// Day 09: Explosives in Cyberspace
+/// https://adventofcode.com/2016/day/09
+/// </summary>
+[Description("Explosives in Cyberspace")]
+public sealed partial class Day09 {
+
+	public static string Part1(string[] input) => Solution1(input).ToString();
+	public static string Part2(string[] input) => Solution2(input).ToString();
+
+	private static int Solution1(string[] input) {
+		string compressed = input[0];
+		StringBuilder uncompressed = new();
+
+		for (int i = 0; i < compressed.Length; i++) {
+			char current = compressed[i];
+			if (current == OPEN_BRACKET) {
+				int closeBracketIndex = compressed[i..].IndexOf(CLOSE_BRACKET) + i;
+				string marker = compressed[(i + 1)..closeBracketIndex];
+				string[] tokens = marker.Split('x');
+				int chars = tokens[0].As<int>();
+				int repeat = tokens[1].As<int>();
+				for (int j = 0; j < repeat; j++) {
+					_ = uncompressed.Append(compressed[(closeBracketIndex + 1)..(closeBracketIndex + 1 + chars)]);
+				}
+				i = closeBracketIndex + chars;
+			} else {
+				_ = uncompressed.Append(current);
+			}
+		}
+
+		return uncompressed.Length;
+	}
+
+	private static string Solution2(string[] input) {
+		return NO_SOLUTION_WRITTEN_MESSAGE;
+	}
+}
+
+file static class Day09Constants
+{
+	public const char OPEN_BRACKET = '(';
+	public const char CLOSE_BRACKET = ')';
+}
---
+++ Tests/2016/Tests_09.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode.Tests.Year2016;
+
+public class Tests_09_Explosives_in_Cyberspace
+{
+	const int DAY = 09;
+
+	[Theory]
+	[InlineData("ADVENT", 6)]
+	[InlineData("A(1x5)BC", 7)]
+	[InlineData("(3x3)XYZ", 9)]
+	[InlineData("A(2x2)BCD(2x2)EFG", 11)]
+	[InlineData("X(8x2)(3x3)ABCY", 18)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
