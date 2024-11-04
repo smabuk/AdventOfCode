237d3f39339262f2aea4b073d93ddb37a267cbaa
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-04 14:19:08 +0000

    2016 Day15 Part1 Solved

---
+++ Solutions/2016/Day15.cs
@@ -0,0 +1,65 @@
+﻿using static AdventOfCode.Solutions._2016.Day15Constants;
+using static AdventOfCode.Solutions._2016.Day15Types;
+namespace AdventOfCode.Solutions._2016;
+
+/// <summary>
+/// Day 15: Timing is Everything
+/// https://adventofcode.com/2016/day/15
+/// </summary>
+[Description("Timing is Everything")]
+public sealed partial class Day15 {
+
+	public static string Part1(string[] input) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+
+	private static int Solution1(string[] input) => input.As<Disc>().FindAlignment();
+
+	private static string Solution2(string[] input) {
+		return NO_SOLUTION_WRITTEN_MESSAGE;
+	}
+}
+
+file static class Day15Extensions
+{
+	public static int FindAlignment(this IEnumerable<Disc> discs)
+	{
+		// Assume discs have a prime number of positions
+		for (int time = 0; ; time++) {
+			if (discs
+				.All(disc => (disc.PositionAtTimeZero + time + disc.DiscNo) % disc.NoOfPositions == 0)) {
+				return time;
+			}
+		}
+	}
+}
+
+internal sealed partial class Day15Types
+{
+
+	public sealed record Disc(int DiscNo, int NoOfPositions, int PositionAtTimeZero) : IParsable<Disc>
+	{
+		public readonly int OffsetToZero = (NoOfPositions - PositionAtTimeZero - DiscNo) % NoOfPositions;
+		public static Disc Parse(string s, IFormatProvider? provider)
+		{
+			Match match = InputRegEx().Match(s);
+			if (match.Success) {
+				return new(
+					match.As<int>("discNo"),
+					match.As<int>("positions"),
+					match.As<int>("position"));
+			}
+			return null!;
+		}
+
+		public static Disc Parse(string s) => Parse(s, null);
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Disc result)
+			=> ISimpleParsable<Disc>.TryParse(s, provider, out result);
+	}
+
+	[GeneratedRegex(""""Disc \#(?<discNo>[\+\-]?\d+) has (?<positions>[\+\-]?\d+) positions; at time=0, it is at position (?<position>[\+\-]?\d+)."""")]
+	public static partial Regex InputRegEx();
+}
+
+file static class Day15Constants
+{
+}
---
+++ Tests/2016/Tests_15.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode.Tests.Year2016;
+
+public class Tests_15_Timing_is_Everything
+{
+	const int DAY = 15;
+
+	[Theory]
+	[InlineData("""
+		Disc #1 has 5 positions; at time=0, it is at position 4.
+		Disc #2 has 2 positions; at time=0, it is at position 1.
+		""", 5)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
