14eac1c55e3dd850366253ccf7b6390a0b8196b6
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-05 13:00:01 +0000

    2016 Day22 Part1 Solved

---
+++ Solutions/2016/Day22.cs
@@ -0,0 +1,81 @@
+﻿using static AdventOfCode.Solutions._2016.Day22Constants;
+using static AdventOfCode.Solutions._2016.Day22Types;
+namespace AdventOfCode.Solutions._2016;
+
+/// <summary>
+/// Day 22: Grid Computing
+/// https://adventofcode.com/2016/day/22
+/// </summary>
+[Description("Grid Computing")]
+public sealed partial class Day22 {
+
+	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+	private static int Solution1(string[] input) {
+		return input
+			.Skip(2)
+			.As<Node>()
+			.Permute(2)
+			.Count(pair => pair.IsViablePair());
+	}
+
+	private static string Solution2(string[] input) {
+		return NO_SOLUTION_WRITTEN_MESSAGE;
+	}
+}
+
+file static class Day22Extensions
+{
+	public static bool IsViablePair(this IEnumerable<Node> pair)
+	{
+		Node[] nodes = [.. pair];
+		Node nodeA = nodes[0];
+		Node nodeB = nodes[1];
+
+		if (nodeA.Used == 0) {
+			return false;
+		}
+
+		if (nodeA.Name == nodeB.Name) {
+			return false;
+		}
+
+		if (nodeA.Used > nodeB.Available) {
+			return false;
+		}
+
+		return true;
+	}
+}
+
+internal sealed partial class Day22Types
+{
+
+	public sealed record Node(string Name, Point Position, int Size, int Used, int Available, int UsePercentage) : IParsable<Node>
+	{
+		public static Node Parse(string s, IFormatProvider? provider)
+		{
+			if (s[0] == '/') {
+				string[] tokens = s.TrimmedSplit([' ','T','%', '/']);
+				string name = tokens[2];
+				string[] nameTokens = name.TrimmedSplit(['-','x','y']);
+				Point position = new(nameTokens[1].As<int>(), nameTokens[2].As<int>());
+				return new(name,
+					position,
+					tokens[3].As<int>(),
+					tokens[4].As<int>(),
+					tokens[5].As<int>(),
+					tokens[6].As<int>());
+			}
+			return null!;
+		}
+
+		public static Node Parse(string s) => Parse(s, null);
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Node result)
+			=> ISimpleParsable<Node>.TryParse(s, provider, out result);
+	}
+}
+
+file static class Day22Constants
+{
+}
---
+++ Tests/2016/Tests_22.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode.Tests.Year2016;
+
+public class Tests_22_Grid_Computing
+{
+	const int DAY = 22;
+
+	[Theory]
+	[InlineData("""
+		root@ebhq-gridcenter# df -h
+		Filesystem              Size  Used  Avail  Use%
+		/dev/grid/node-x0-y0     94T   21T    22T   76%
+		/dev/grid/node-x0-y1     88T   73T    15T   82%
+		/dev/grid/node-x0-y2     88T   65T    23T   73%
+		/dev/grid/node-x1-y0     91T   67T    24T   73%
+		/dev/grid/node-x1-y1     92T   68T    24T   73%
+		/dev/grid/node-x1-y2     92T   69T    23T   75%
+		/dev/grid/node-x2-y0     86T   65T    21T   75%
+		/dev/grid/node-x2-y1     85T   73T    12T   85%
+		/dev/grid/node-x2-y2     94T   18T    30T   68%
+		/dev/grid/node-x3-y0     85T   68T    17T   80%
+		/dev/grid/node-x3-y1     88T   64T    24T   72%
+		/dev/grid/node-x3-y2     86T   65T    21T   75%
+		/dev/grid/node-x4-y0     86T   66T    20T   76%
+		/dev/grid/node-x4-y1     94T   67T    27T   71%
+		/dev/grid/node-x4-y2     89T   65T    24T   73%
+		/dev/grid/node-x5-y0     92T   72T    20T   78%
+		/dev/grid/node-x5-y1     92T   69T    23T   75%
+		/dev/grid/node-x5-y2     94T   73T    21T   77%
+		/dev/grid/node-x6-y0     87T   70T    17T   80%
+		/dev/grid/node-x6-y1     86T   65T    21T   75%
+		/dev/grid/node-x6-y2     86T   67T    19T   77%
+		/dev/grid/node-x7-y0     91T   69T    22T   75%
+		/dev/grid/node-x7-y1     87T   66T    21T   75%
+		/dev/grid/node-x7-y2     89T   69T    20T   77%
+		""", 34)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
