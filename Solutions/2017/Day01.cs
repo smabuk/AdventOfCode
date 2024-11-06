9d59acbb023808836218254f4731742a7bae0f99
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-06 06:46:44 +0000

    2017 Day01 refactored

--- Solutions/2017/Day01.cs
+++ Solutions/2017/Day01.cs
@@ -1,6 +1,4 @@
-﻿using static AdventOfCode.Solutions._2017.Day01Constants;
-using static AdventOfCode.Solutions._2017.Day01Types;
-namespace AdventOfCode.Solutions._2017;
+﻿namespace AdventOfCode.Solutions._2017;
 
 /// <summary>
 /// Day 01: Inverse Captcha
@@ -12,37 +10,21 @@ public sealed partial class Day01 {
 	public static string Part1(string[] input) => Solution1(input).ToString();
 	public static string Part2(string[] input) => Solution2(input).ToString();
 
-	private static int Solution1(string[] input) => 
-		input[0].AsDigits<int>().ToList()
-		.CheckSumPt1();
+	private static int Solution1(string[] input) =>
+		input.ToDigits().Captcha(1);
 
 	private static int Solution2(string[] input) =>
-		input[0].AsDigits<int>().ToList()
-		.CheckSumPt2();
+		input.ToDigits().Captcha(input[0].Length / 2);
 }
 
 file static class Day01Extensions
 {
-	public static int CheckSumPt1(this List<int> digits)
+	public static int Captcha(this List<int> digits, int slicePoint)
 	{
-		return digits.Zip([.. digits[1..], digits[0]])
+		return digits.Zip([.. digits[slicePoint..], ..digits[..slicePoint]])
 			.Where(pair => pair.First == pair.Second)
 			.Sum(pair => pair.First);
 	}
 
-	public static int CheckSumPt2(this List<int> digits)
-	{
-		return digits.Zip([.. digits[(digits.Count/2)..], ..digits[..(digits.Count/2)]])
-			.Where(pair => pair.First == pair.Second)
-			.Sum(pair => pair.First);
-	}
-
-}
-
-internal sealed partial class Day01Types
-{
-}
-
-file static class Day01Constants
-{
+	public static List<int> ToDigits(this string[] input) => [..input[0].AsDigits<int>()];
 }
