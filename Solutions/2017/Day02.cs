0c4e0220cd3ca1b53f20c278531cddea6418f292
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-06 08:54:39 +0000

    2017 Day02 Part1 Solved

---
+++ Solutions/2017/Day02.cs
@@ -0,0 +1,38 @@
+﻿using static AdventOfCode.Solutions._2017.Day02Constants;
+using static AdventOfCode.Solutions._2017.Day02Types;
+namespace AdventOfCode.Solutions._2017;
+
+/// <summary>
+/// Day 02: Corruption Checksum
+/// https://adventofcode.com/2016/day/02
+/// </summary>
+[Description("Corruption Checksum")]
+public sealed partial class Day02 {
+
+	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+
+	private static int Solution1(string[] input) {
+		return input
+			.Select(i => i.TrimmedSplit([TAB, SPACE]).As<int>())
+			.Sum(s => s.Max() - s.Min());
+	}
+
+	private static string Solution2(string[] input) {
+		return NO_SOLUTION_WRITTEN_MESSAGE;
+	}
+}
+
+file static class Day02Extensions
+{
+}
+
+internal sealed partial class Day02Types
+{
+}
+
+file static class Day02Constants
+{
+	public const char TAB = '\t';
+	public const char SPACE = ' ';
+}
---
+++ Tests/2017/Tests_02.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode.Tests.Year2017;
+
+public class Tests_02_Corruption_Checksum
+{
+	const int DAY = 02;
+
+	[Theory]
+	[InlineData("""
+		5	1	9	5
+		7	5	3
+		2	4	6	8
+		""", 18)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
