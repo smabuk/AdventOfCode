83ee471b4586d9f0924d8f63e0fdd80f4e92307c
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-07 10:01:18 +0000

    2017 Day03 Part1 Solved

---
+++ Solutions/2017/Day03.cs
@@ -0,0 +1,40 @@
+﻿using static AdventOfCode.Solutions._2017.Day03Constants;
+using static AdventOfCode.Solutions._2017.Day03Types;
+namespace AdventOfCode.Solutions._2017;
+
+/// <summary>
+/// Day 03: Spiral Memory
+/// https://adventofcode.com/2016/day/03
+/// </summary>
+[Description("Spiral Memory")]
+public sealed partial class Day03 {
+
+	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+
+	private static int Solution1(string[] input) {
+		int target = input[0].As<int>();
+		int rows = ((int)Math.Sqrt(target - 1)) + (int.IsEvenInteger((int)Math.Sqrt(target - 1)) ? 1 : 2);
+		int bottomRight = rows * rows;
+		int spiralLength = bottomRight - ((rows - 2) * (rows - 2));
+		int sideLengthMinus1 = (spiralLength / 4);
+		int offset = int.Abs((sideLengthMinus1 / 2) - ((bottomRight - target) % sideLengthMinus1)) ;
+		return (rows / 2) + offset;
+	}
+
+	private static string Solution2(string[] input) {
+		return NO_SOLUTION_WRITTEN_MESSAGE;
+	}
+}
+
+file static class Day03Extensions
+{
+}
+
+internal sealed partial class Day03Types
+{
+}
+
+file static class Day03Constants
+{
+}
---
+++ Tests/2017/Tests_03.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode.Tests.Year2017;
+
+public class Tests_03_Spiral_Memory
+{
+	const int DAY = 03;
+
+	[Theory]
+	[InlineData(1, 0)]
+	[InlineData(12, 3)]
+	[InlineData(23, 2)]
+	[InlineData(25, 4)]
+	[InlineData(1024, 31)]
+	public void Part1(int input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input.ToString()), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
