b866e1adf0bec0b863608bddc5118263d5ce7494
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-08 09:06:59 +0000

    2017 Day04 Part1 Solved

---
+++ Solutions/2017/Day04.cs
@@ -0,0 +1,44 @@
+﻿using static AdventOfCode.Solutions._2017.Day04Constants;
+using static AdventOfCode.Solutions._2017.Day04Types;
+namespace AdventOfCode.Solutions._2017;
+
+/// <summary>
+/// Day 04: Title
+/// https://adventofcode.com/2016/day/04
+/// </summary>
+[Description("")]
+public sealed partial class Day04 {
+
+	public static string Part1(string[] input) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+
+	private static int Solution1(string[] input) {
+		int validCount = 0;
+
+		foreach (string passphrase in input) {
+			string[] words = passphrase.TrimmedSplit(SPACE);
+			if (words.Length == words.Distinct().Count()) {
+				validCount += 1;
+			}
+		}
+
+		return validCount;
+	}
+
+	private static string Solution2(string[] input) {
+		return NO_SOLUTION_WRITTEN_MESSAGE;
+	}
+}
+
+file static class Day04Extensions
+{
+}
+
+internal sealed partial class Day04Types
+{
+}
+
+file static class Day04Constants
+{
+	public const char SPACE = ' ';
+}
---
+++ Tests/2017/Tests_04.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCode.Tests.Year2017;
+
+public class Tests_04_High_Entropy_Passphrases
+{
+	const int DAY = 04;
+
+	[Theory]
+	[InlineData("aa bb cc dd ee", 1)]
+	[InlineData("aa bb cc dd aa", 0)]
+	[InlineData("aa bb cc dd aaa", 1)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
