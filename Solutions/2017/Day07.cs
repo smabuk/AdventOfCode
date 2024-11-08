ef039556e6c0b60546fd1acecf846e5bd9604ee3
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-08 22:25:49 +0000

    2017 Day07 Part1 Solved

---
+++ Solutions/2017/Day07.cs
@@ -0,0 +1,51 @@
+﻿using static AdventOfCode.Solutions._2017.Day07Constants;
+using static AdventOfCode.Solutions._2017.Day07Types;
+namespace AdventOfCode.Solutions._2017;
+
+/// <summary>
+/// Day 07: Recursive Circus
+/// https://adventofcode.com/2016/day/07
+/// </summary>
+[Description("Recursive Circus")]
+public sealed partial class Day07 {
+
+	public static string Part1(string[] input) => Solution1(input).ToString();
+	public static string Part2(string[] input) => Solution2(input).ToString();
+
+	private static string Solution1(string[] input) {
+		List<Program> programs = [.. input.As<Program>()];
+		HashSet<string> subprograms =
+			[..programs.SelectMany(program => program.ProgramNames)];
+		return programs.Single(program => subprograms.DoesNotContain(program.Name)).Name;
+	}
+
+	private static string Solution2(string[] input) {
+		return NO_SOLUTION_WRITTEN_MESSAGE;
+	}
+}
+
+file static class Day07Extensions
+{
+}
+
+internal sealed partial class Day07Types
+{
+
+	public sealed record Program(string Name, int Weight, List<string> ProgramNames) : IParsable<Program>
+	{
+		public static Program Parse(string s, IFormatProvider? provider)
+		{
+			string[] tokens = s.TrimmedSplit([',', ' ', '-', '>', '(', ')']);
+			List<string> programNames = tokens.Length > 2 ? [.. tokens[2..]] : [];
+			return new(tokens[0], tokens[1].As<int>(), programNames);
+		}
+
+		public static Program Parse(string s) => Parse(s, null);
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Program result)
+			=> ISimpleParsable<Program>.TryParse(s, provider, out result);
+	}
+}
+
+file static class Day07Constants
+{
+}
---
+++ Tests/2017/Tests_07.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode.Tests.Year2017;
+
+public class Tests_07_Recursive_Circus
+{
+	const int DAY = 07;
+
+	[Theory]
+	[InlineData("""
+		pbga (66)
+		xhth (57)
+		ebii (61)
+		havc (66)
+		ktlj (57)
+		fwft (72) -> ktlj, cntj, xhth
+		qoyq (66)
+		padx (45) -> pbga, havc, qoyq
+		tknk (41) -> ugml, padx, fwft
+		jptl (61)
+		ugml (68) -> gyxo, ebii, jptl
+		gyxo (61)
+		cntj (57)
+		""", "tknk")]
+	public void Part1(string input, string expected)
+	{
+		string actual = SolutionRouter.SolveProblem(YEAR, DAY, PART1, input);
+		actual.ShouldBe(expected);
+	}
+}
