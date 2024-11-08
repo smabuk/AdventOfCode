b9e33df626e56665233bcefddd1088b7d89c7051
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-08 09:38:45 +0000

    2017 Day05 Part1 Solved

---
+++ Solutions/2017/Day05.cs
@@ -0,0 +1,43 @@
+﻿using static AdventOfCode.Solutions._2017.Day05Constants;
+using static AdventOfCode.Solutions._2017.Day05Types;
+namespace AdventOfCode.Solutions._2017;
+
+/// <summary>
+/// Day 05: A Maze of Twisty Trampolines, All Alike
+/// https://adventofcode.com/2016/day/05
+/// </summary>
+[Description("A Maze of Twisty Trampolines, All Alike")]
+public sealed partial class Day05 {
+
+	public static string Part1(string[] input) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+
+	private static int Solution1(string[] input) {
+		int[] jumps = [.. input.As<int>()];
+		int jumpPtr = 0;
+		int steps = 0;
+
+		while (jumpPtr >= 0 && jumpPtr < jumps.Length) {
+			jumpPtr += jumps[jumpPtr]++;
+			steps++;
+		}
+
+		return steps;
+	}
+
+	private static string Solution2(string[] input) {
+		return NO_SOLUTION_WRITTEN_MESSAGE;
+	}
+}
+
+file static class Day05Extensions
+{
+}
+
+internal sealed partial class Day05Types
+{
+}
+
+file static class Day05Constants
+{
+}
---
+++ Tests/2017/Tests_05.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode.Tests.Year2017;
+
+public class Tests_05_A_Maze_of_Twisty_Trampolines_All_Alike
+{
+	const int DAY = 05;
+
+	[Theory]
+	[InlineData("""
+		0
+		3
+		0
+		1
+		-3
+		""", 5)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
