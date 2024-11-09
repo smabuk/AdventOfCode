6a163788bd2f287847c3d57a10e3a5fef115e46c
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-09 14:30:31 +0000

    2017 Day09 Part1 Solved

---
+++ Solutions/2017/Day09.cs
@@ -0,0 +1,81 @@
+﻿using System.Text;
+
+using static AdventOfCode.Solutions._2017.Day09Constants;
+using static AdventOfCode.Solutions._2017.Day09Types;
+namespace AdventOfCode.Solutions._2017;
+
+/// <summary>
+/// Day 09: Stream Processing
+/// https://adventofcode.com/2016/day/09
+/// </summary>
+[Description("Stream Processing")]
+public sealed partial class Day09 {
+
+	public static string Part1(string[] input) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+
+	private static int Solution1(string[] input) =>
+		input[0]
+			.RemoveGarbage()
+			.ScoreStream();
+
+	private static string Solution2(string[] input) {
+		return NO_SOLUTION_WRITTEN_MESSAGE;
+	}
+}
+
+file static class Day09Extensions
+{
+	public static string RemoveGarbage(this string stream)
+	{
+		StringBuilder sb = new();
+
+		bool isInGarbage = false;
+
+		for (int i = 0; i < stream.Length; i++) {
+			char character = stream[i];
+			if (isInGarbage) {
+				if (character is CLOSE_GARBAGE) {
+					isInGarbage = false;
+				}
+				if (character is CANCEL) {
+					i++;
+				}
+			} else if (character is OPEN_GARBAGE) {
+				isInGarbage = true;
+			} else {
+				sb = sb.Append(character);
+			}
+		}
+
+		return sb.ToString();
+	}
+	public static int ScoreStream(this string stream)
+	{
+		int score = 0;
+		int level = 1;
+		for (int i = 0; i < stream.Length; i++) {
+			char character = stream[i];
+			if (character is OPEN_GROUP) {
+				score += level++;
+			} else  if (character is CLOSE_GROUP) {
+				level--;
+			}
+		}
+
+		return score;
+	}
+}
+
+internal sealed partial class Day09Types
+{
+}
+
+file static class Day09Constants
+{
+	public const char OPEN_GARBAGE  = '<';
+	public const char CLOSE_GARBAGE = '>';
+	public const char OPEN_GROUP    = '{';
+	public const char CLOSE_GROUP   = '}';
+	public const char CANCEL        = '!';
+}
---
+++ Tests/2017/Tests_09.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode.Tests.Year2017;
+
+public class Tests_09_Stream_Processing
+{
+	const int DAY = 09;
+
+	[Theory]
+	[InlineData("""{}""", 1)]
+	[InlineData("""{{{}}}""", 6)]
+	[InlineData("""{{},{}}""", 5)]
+	[InlineData("""{{{},{},{{}}}}""", 16)]
+	[InlineData("""{<a>,<a>,<a>,<a>}""", 1)]
+	[InlineData("""{{<ab>},{<ab>},{<ab>},{<ab>}}""", 9)]
+	[InlineData("""{{<!!>},{<!!>},{<!!>},{<!!>}}""", 9)]
+	[InlineData("""{{<a!>},{<a!>},{<a!>},{<ab>}}""", 3)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
