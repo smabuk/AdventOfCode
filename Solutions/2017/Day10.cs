e9ea547c846687b0b5b57402e874d7865cf6b3d0
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-09 16:58:33 +0000

    2017 Day10 Solved

--- Solutions/2017/Day10.cs
+++ Solutions/2017/Day10.cs
@@ -15,33 +15,88 @@ public sealed partial class Day10 {
 		return Solution1(input, listSize).ToString();
 	}
 
-	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+	public static string Part2(string[] input) => Solution2(input).ToString();
 
-	private static int Solution1(string[] input, int listSize) {
-		List<int> sequenceOfLengths = [..input[0].TrimmedSplit(',').As<int>()];
-		List<int> currentList = [.. Enumerable.Range(0, listSize)];
+	private static int Solution1(string[] input, int listSize)
+	{
+		List<int> sequenceOfLengths = [.. input[0].TrimmedSplit(',').As<int>()];
+		List<byte> currentList = [.. Enumerable.Range(0, listSize).Select(i => (byte)i)];
 
 		int skipSize = 0;
 		int currentPosition = 0;
-		foreach (int currentLength in sequenceOfLengths) {
-			currentList = currentList.ReverseElements(currentPosition, currentLength);
-			currentPosition += currentLength + skipSize++;
-		}
+		(currentList, _, _) = currentList.KnotHashRound(sequenceOfLengths, currentPosition, skipSize);
 
 		return currentList[0] * currentList[1];
 	}
 
+
 	private static string Solution2(string[] input) {
-		return NO_SOLUTION_WRITTEN_MESSAGE;
+		const int NO_OF_ROUNDS = 64;
+		const int LIST_SIZE = 256;
+		byte[] LIST_APPEND = [17, 31, 73, 47, 23];
+
+		List<int> sequenceOfLengths = input.Length == 0
+			? [.. LIST_APPEND]
+			: [.. input[0]?.Select(c => (byte)c), .. LIST_APPEND];
+		List<byte> currentList = [.. Enumerable.Range(0, LIST_SIZE).Select(i => (byte)i)];
+
+		int skipSize = 0;
+		int currentPosition = 0;
+		for (int i = 0; i < NO_OF_ROUNDS; i++) {
+			(currentList, currentPosition, skipSize) = currentList.KnotHashRound(sequenceOfLengths, currentPosition, skipSize);
+		}
+
+		List<byte> denseHash = [.. currentList.DenseHash()];
+
+		return denseHash.ToHexString();
 	}
 }
 
 file static class Day10Extensions
 {
-	public static List<int> ReverseElements(this List<int> list, int position, int noOfElementToReverse)
+	public static string ToHexString(this List<byte> hash) => Convert.ToHexStringLower([.. hash]);
+
+	public static (List<byte>, int, int) KnotHashRound(this List<byte> currentList, List<int> sequenceOfLengths, int position, int skip)
+	{
+		int skipSize = skip;
+		int currentPosition = position;
+		foreach (int currentLength in sequenceOfLengths) {
+			currentList = currentList.ReverseElements(currentPosition, currentLength);
+			currentPosition += currentLength + skipSize++;
+		}
+
+		return (currentList, currentPosition % currentList.Count, skipSize % currentList.Count);
+	}
+
+	public static IEnumerable<byte> DenseHash(this List<byte> sparseHash)
+	{
+		for (int i = 0; i < 16; i++) {
+			int offset = i * 16;
+
+			yield return (byte)(
+				sparseHash[offset]
+				^ sparseHash[offset + 1]
+				^ sparseHash[offset + 2]
+				^ sparseHash[offset + 3]
+				^ sparseHash[offset + 4]
+				^ sparseHash[offset + 5]
+				^ sparseHash[offset + 6]
+				^ sparseHash[offset + 7]
+				^ sparseHash[offset + 8]
+				^ sparseHash[offset + 9]
+				^ sparseHash[offset + 10]
+				^ sparseHash[offset + 11]
+				^ sparseHash[offset + 12]
+				^ sparseHash[offset + 13]
+				^ sparseHash[offset + 14]
+				^ sparseHash[offset + 15]);
+		}
+	}
+
+	public static List<byte> ReverseElements(this List<byte> list, int position, int noOfElementToReverse)
 	{
 		int listLength = list.Count;
-		int[] newList = new int[listLength];
+		byte[] newList = new byte[listLength];
 		for (int i = 0; i < listLength; i++)
 		{
 			int currentPosition = position + i;
--- Tests/2017/Tests_10.cs
+++ Tests/2017/Tests_10.cs
@@ -11,4 +11,15 @@ public class Tests_10_Knot_Hash
 		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input, listSize), out int actual);
 		actual.ShouldBe(expected);
 	}
+
+	[Theory]
+	[InlineData("", "a2582a3a0e66e6e86e3812dcb672a272")]
+	[InlineData("AoC 2017", "33efeb34ea91902bb2f59c9920caa6cd")]
+	[InlineData("1,2,3", "3efbe78a8d82f29979031a4aa0b16a9d")]
+	[InlineData("1,2,4", "63960835bcdc130f0b66d7ff4f6a5a8e")]
+	public void Part2(string input, string expected)
+	{
+		string actual = SolutionRouter.SolveProblem(YEAR, DAY, PART2, input);
+		actual.ShouldBe(expected);
+	}
 }
