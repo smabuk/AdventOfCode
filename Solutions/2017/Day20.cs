19e9aec9b34e59433a1da0c0c1132a73463d735d
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-24 12:21:12 +0000

    2017 Day20 Part1 Solved

---
+++ Solutions/2017/Day20.cs
@@ -0,0 +1,77 @@
+﻿using static AdventOfCode.Solutions._2017.Day20Constants;
+using static AdventOfCode.Solutions._2017.Day20Types;
+namespace AdventOfCode.Solutions._2017;
+
+/// <summary>
+/// Day 20: Particle Swarm
+/// https://adventofcode.com/2016/day/20
+/// </summary>
+[Description("Particle Swarm")]
+public sealed partial class Day20 {
+
+	[Init]
+	public static   void  Init(string[] input, params object[]? args) => LoadParticles(input);
+	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+
+	private static List<Particle> _particles = [];
+
+	private static void LoadParticles(string[] input) => 
+		_particles = [
+			.. input
+			.Index()
+			.Select(item => $"{item.Index}, {item.Item}")
+			.As<Particle>()];
+
+	private static int Solution1(string[] input) {
+		return _particles
+			.OrderBy(p => p.Acceleration.ManhattanDistance(Point3d.Zero))
+			.ThenBy(p => p.Velocity.ManhattanDistance(Point3d.Zero))
+			.ThenBy(p => p.Position.ManhattanDistance(Point3d.Zero))
+			.First()
+			.Id;
+	}
+
+	private static string Solution2(string[] input) {
+		return NO_SOLUTION_WRITTEN_MESSAGE;
+	}
+}
+
+file static class Day20Extensions
+{
+	public static Particle Update(this Particle particle)
+	{
+		Point3d newVelocity = particle.Velocity + particle.Acceleration;
+		Point3d newPosition = particle.Position + particle.Velocity;
+		return particle with {
+			Position = newPosition,
+			Velocity = newVelocity,
+		};
+	}
+}
+
+internal sealed partial class Day20Types
+{
+
+	public sealed record Particle(int Id ,Point3d Position, Point3d Velocity, Point3d Acceleration) : IParsable<Particle>
+	{
+		public static Particle Parse(string s, IFormatProvider? provider)
+		{
+			string[] tokens = s.TrimmedSplit([',', 'p', 'v', 'a', '=', '<', '>']);
+			return new(
+				tokens[0].As<int>(),
+				new(tokens[1].As<int>(), tokens[2].As<int>(), tokens[3].As<int>()),
+				new(tokens[4].As<int>(), tokens[5].As<int>(), tokens[6].As<int>()),
+				new(tokens[7].As<int>(), tokens[8].As<int>(), tokens[9].As<int>())
+				);
+		}
+
+		public static Particle Parse(string s) => Parse(s, null);
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Particle result)
+			=> ISimpleParsable<Particle>.TryParse(s, provider, out result);
+	}
+}
+
+file static class Day20Constants
+{
+}
---
+++ Tests/2017/Tests_20.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode.Tests.Year2017;
+
+public class Tests_20_Particle_Swarm
+{
+	const int DAY = 20;
+
+	[Theory]
+	[InlineData("""
+		p=< 4,0,0>, v=< 0,0,0>, a=<-2,0,0>
+		p=< 3,0,0>, v=< 2,0,0>, a=<-1,0,0>
+		""", 1)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
