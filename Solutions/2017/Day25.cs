a1bb769d391e444155bb18df328e95c68091b8da
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-28 15:26:05 +0000

    2017 Day25 Solved

---
+++ Solutions/2017/Day25.cs
@@ -0,0 +1,91 @@
+﻿using static AdventOfCode.Solutions._2017.Day25Constants;
+using static AdventOfCode.Solutions._2017.Day25Types;
+namespace AdventOfCode.Solutions._2017;
+
+/// <summary>
+/// Day 25: The Halting Problem
+/// https://adventofcode.com/2016/day/25
+/// </summary>
+[Description("The Halting Problem")]
+public sealed partial class Day25 {
+
+	public static string Part1(string[] input) => Solution1(input).ToString();
+	public static string Part2(string[] _) => "⭐CONGRATULATIONS⭐";
+
+	private static int Solution1(string[] input) {
+		string currentStateName = $"{input[0][^2]}";
+		int steps = input[1].TrimmedSplit(' ')[^2].As<int>();
+
+		Dictionary<string, StateRule> stateRules =
+			input[3..]
+			.Chunk(10)
+			.Select(StateRule.Parse)
+			.ToDictionary(sr => sr.Name);
+
+		LinkedList<bool> tape = [];
+		LinkedListNode<bool> current = tape.AddFirst(false);
+
+		for (int step = 0; step < steps; step++) {
+			Rule rule = current.Value
+				? stateRules[currentStateName].Rule1
+				: stateRules[currentStateName].Rule0;
+			current.Value = rule.WriteValue;
+			current = rule.Right
+				? current.Next     ?? tape.AddLast(false)
+				: current.Previous ?? tape.AddFirst(false);
+			currentStateName = rule.NextStateName;
+		}
+
+		return tape.Count(v => v is true);
+	}
+}
+
+file static class Day25Extensions
+{
+}
+
+internal sealed partial class Day25Types
+{
+	public sealed record StateRule(string Name, Rule Rule0, Rule Rule1) : IParsable<StateRule>
+	{
+		public static StateRule Parse(string[] s)
+		{
+			return new StateRule(
+				$"{s[0][^2]}",
+				Rule.Parse(s[1..5]),
+				Rule.Parse(s[5..9])
+				);
+		}
+
+		public static StateRule Parse(string s, IFormatProvider? provider) => null!;
+
+		public static StateRule Parse(string s) => Parse(s, null);
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out StateRule result)
+			=> ISimpleParsable<StateRule>.TryParse(s, provider, out result);
+	}
+
+	public sealed record Rule(bool CurrentValue, bool WriteValue, bool Right, string NextStateName) : IParsable<Rule>
+	{
+		public static Rule Parse(string[] s)
+		{
+			return new Rule(
+				s[0][^2] == ONE,
+				s[1][^2] == ONE,
+				s[2][^3] == 'h',  // rig 'h' t
+				$"{s[3][^2]}"
+				);
+		}
+
+		public static Rule Parse(string s, IFormatProvider? provider) => null!;
+
+		public static Rule Parse(string s) => Parse(s, null);
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Rule result)
+			=> ISimpleParsable<Rule>.TryParse(s, provider, out result);
+	}
+}
+
+file static class Day25Constants
+{
+	public const char ZERO = '0';
+	public const char ONE  = '1';
+}
---
+++ Tests/2017/Tests_25.cs
@@ -0,0 +1,38 @@
+﻿namespace AdventOfCode.Tests.Year2017;
+
+public class Tests_25_The_Halting_Problem
+{
+	const int DAY = 25;
+
+	[Theory]
+	[InlineData("""
+		Begin in state A.
+		Perform a diagnostic checksum after 6 steps.
+
+		In state A:
+		  If the current value is 0:
+		    - Write the value 1.
+		    - Move one slot to the right.
+		    - Continue with state B.
+		  If the current value is 1:
+		    - Write the value 0.
+		    - Move one slot to the left.
+		    - Continue with state B.
+
+		In state B:
+		  If the current value is 0:
+		    - Write the value 1.
+		    - Move one slot to the left.
+		    - Continue with state A.
+		  If the current value is 1:
+		    - Write the value 1.
+		    - Move one slot to the right.
+		    - Continue with state A.
+		""", 3)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+
+}
