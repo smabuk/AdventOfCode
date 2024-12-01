fd7f90bb0eef0765fca5ef9bd21c0b6846bd83a7
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-01 05:16:48 +0000

    2024 Day01 Solved

---
+++ Solutions/2024/Day01.cs
@@ -0,0 +1,51 @@
+﻿using static AdventOfCode.Solutions._2024.Day01Constants;
+using static AdventOfCode.Solutions._2024.Day01Types;
+namespace AdventOfCode.Solutions._2024;
+
+/// <summary>
+/// Day 01: Historian Hysteria
+/// https://adventofcode.com/2024/day/01
+/// </summary>
+[Description("Historian Hysteria")]
+public sealed partial class Day01 {
+
+	public static string Part1(string[] input) => Solution1(input).ToString();
+	public static string Part2(string[] input) => Solution2(input).ToString();
+
+	private static int Solution1(string[] input)
+	{
+		IEnumerable<int> list1 = [..input.Select(i => i.TrimmedSplit(SPACE)[0].As<int>()).Order()];
+		IEnumerable<int> list2 = [..input.Select(i => i.TrimmedSplit(SPACE)[1].As<int>()).Order()];
+		return list1
+			.Zip(list2)
+			.Sum(numbers => numbers.Distance());
+	}
+
+	private static int Solution2(string[] input)
+	{
+		List<int> list1 = [.. input.Select(i => i.TrimmedSplit(SPACE)[0].As<int>()).Order()];
+		Dictionary<int, int> countsOfList2 = 
+			input
+				.Select(i => i.TrimmedSplit(SPACE)[1].As<int>())
+				.CountBy(n => n)
+				.ToDictionary();
+
+		return list1
+			.Sum(number => number.SimilarityScore(countsOfList2.GetValueOrDefault(number, 0)));
+	}
+}
+
+file static class Day01Extensions
+{
+	public static int Distance(this (int First, int Second) numbers) => int.Abs(numbers.First - numbers.Second);
+	public static int SimilarityScore(this int number, int count) => number * count;
+}
+
+internal sealed partial class Day01Types
+{
+}
+
+file static class Day01Constants
+{
+	public const char SPACE = ' ';
+}
--- Tests/2024/Tests_01.cs
+++ Tests/2024/Tests_01.cs
@@ -1,26 +1,31 @@
 ﻿namespace AdventOfCode.Tests.Year2024;
 
-public class Tests_01_
+public class Tests_01_Historian_Hysteria
 {
 	const int DAY = 01;
 
 	private const string TEST_DATA = """
-		test data
+		3   4
+		4   3
+		2   5
+		1   3
+		3   9
+		3   3
 		""";
 
-	//[Theory]
-	//[InlineData(TEST_DATA, 9999)]
-	//public void Part1(string input, int expected)
-	//{
-	//	_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
-	//	actual.ShouldBe(expected);
-	//}
+	[Theory]
+	[InlineData(TEST_DATA, 11)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
 
-	//[Theory]
-	//[InlineData(TEST_DATA, 9999)]
-	//public void Part2(string input, int expected)
-	//{
-	//	_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART2, input), out int actual);
-	//	actual.ShouldBe(expected);
-	//}
+	[Theory]
+	[InlineData(TEST_DATA, 31)]
+	public void Part2(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART2, input), out int actual);
+		actual.ShouldBe(expected);
+	}
 }
