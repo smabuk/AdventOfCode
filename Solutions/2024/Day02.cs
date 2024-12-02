34ac6465655e325787ef51a88a0fde7de2000bb0
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-02 11:24:55 +0000

    2024 Day02 refactored

--- Solutions/2024/Day02.cs
+++ Solutions/2024/Day02.cs
@@ -1,5 +1,5 @@
 ﻿using static AdventOfCode.Solutions._2024.Day02Constants;
-using static AdventOfCode.Solutions._2024.Day02Types;
+
 namespace AdventOfCode.Solutions._2024;
 
 /// <summary>
@@ -24,18 +24,18 @@ file static class Day02Extensions
 	public static IEnumerable<IEnumerable<int>> GetReports(this IEnumerable<string> input)
 		=> input.Select(i => i.As<int>(SPACE));
 
-	public static bool HasMinMaxGap(this IEnumerable<int> report)
-	{
-		return report
-			.Zip(report.Skip(1))
-			.All(p => int.Abs(p.First - p.Second) is >= MIN_GAP and <= MAX_GAP);
-	}
+	public static bool HasMinMaxGap(this (int First, int Second) pair)
+		=> int.Abs(pair.First - pair.Second) is >= MIN_GAP and <= MAX_GAP;
 
-	public static bool IsInOrder(this IEnumerable<int> report)
-		=> report.Order().SequenceEqual(report) || report.OrderDescending().SequenceEqual(report);
+	public static bool HasSign(this (int First, int Second) pair, int sign)
+		=> int.Sign(pair.First - pair.Second) == sign;
 
 	public static bool IsSafe(this IEnumerable<int> report)
-		=> report.IsInOrder() && report.HasMinMaxGap();
+	{
+		int sign = int.Sign(report.First() - report.Skip(1).First());
+		return report.Zip(report.Skip(1))
+				.All(pair => pair.HasMinMaxGap() && pair.HasSign(sign));
+	}
 
 	public static bool IsTolerablySafe(this IEnumerable<int> report)
 	{
@@ -43,10 +43,6 @@ file static class Day02Extensions
 			.Range(0, report.Count())
 			.Any(i => report.Where((_, index) => index != i).IsSafe());
 	}
-}
-
-internal sealed partial class Day02Types
-{
 }
 
 file static class Day02Constants
