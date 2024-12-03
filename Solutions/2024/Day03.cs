2047afd4010fd772764557f0ead18509a603de22
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-03 05:16:14 +0000

    2024 Day03 Part1 Solved

---
+++ Solutions/2024/Day03.cs
@@ -0,0 +1,61 @@
+﻿using Smab.Helpers;
+
+using static AdventOfCode.Solutions._2024.Day03Constants;
+using static AdventOfCode.Solutions._2024.Day03Types;
+namespace AdventOfCode.Solutions._2024;
+
+/// <summary>
+/// Day 03: Mull It Over
+/// https://adventofcode.com/2024/day/03
+/// </summary>
+[Description("Mull It Over")]
+public sealed partial class Day03 {
+
+	[Init]
+	public static   void  Init(string[] input, params object[]? args) => LoadInstructions(input);
+	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+
+	private static IEnumerable<Instruction> _instructions = [];
+
+	private static void LoadInstructions(string[] input)
+		=> _instructions = [.. Instruction.ParseAll(input)];
+
+	private static int Solution1(string[] input) => _instructions.Sum(i => i.Value1 * i.Value2);
+
+	private static string Solution2(string[] input) => NO_SOLUTION_WRITTEN_MESSAGE;
+}
+
+file static class Day03Extensions
+{
+}
+
+internal sealed partial class Day03Types
+{
+
+	public sealed record Instruction(int Value1, int Value2) : IParsable<Instruction>
+	{
+		public static IEnumerable<Instruction> ParseAll(string[] s)
+		{
+			foreach (var line in s) {
+				MatchCollection matches = InputRegEx().Matches(line);
+				foreach (Match match in matches) {
+					yield return new(match.As<int>("number1"), match.As<int>("number2"));
+				}
+			}
+		}
+
+		public static Instruction Parse(string s, IFormatProvider? provider) => null!;
+
+		public static Instruction Parse(string s) => Parse(s, null);
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Instruction result)
+			=> ISimpleParsable<Instruction>.TryParse(s, provider, out result);
+	}
+
+	[GeneratedRegex("""mul\((?<number1>\d+),(?<number2>\d+)\)""")]
+	public static partial Regex InputRegEx();
+}
+
+file static class Day03Constants
+{
+}
---
+++ Tests/2024/Tests_03.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode.Tests.Year2024;
+
+public class Tests_03_Mull_It_Over
+{
+	const int DAY = 03;
+
+	[Theory]
+	[InlineData("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))", 161)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
