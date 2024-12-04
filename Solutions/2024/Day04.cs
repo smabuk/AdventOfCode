1b93050c1f066708bc717cc5709213064c4b4f5c
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-04 07:46:10 +0000

    2017 Day04 Solved

--- Solutions/2024/Day04.cs
+++ Solutions/2024/Day04.cs
@@ -12,7 +12,7 @@ namespace AdventOfCode.Solutions._2024;
 public sealed partial class Day04 {
 
 	public static string Part1(string[] _) => Solution1().ToString();
-	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+	public static string Part2(string[] _) => Solution2().ToString();
 
 	private static char[,] _wordSearch = default!;
 
@@ -21,20 +21,34 @@ public sealed partial class Day04 {
 
 	private static int Solution1()
 	{
-		int count = 0;
-		count += XmasRegEx().Count(string.Join(" ", _wordSearch.RowsAsStrings()));
-		count += XmasRegEx().Count(string.Join(" ", _wordSearch.ColsAsStrings()));
-		count += XmasRegEx().Count(string.Join(" ", _wordSearch.DiagonalsLRAsStrings()));
-		count += XmasRegEx().Count(string.Join(" ", _wordSearch.DiagonalsRLAsStrings()));
-
-		return count;
+		return XmasRegEx().Count(_wordSearch.RowsAsStrings().AsString())
+			 + XmasRegEx().Count(_wordSearch.ColsAsStrings().AsString())
+			 + XmasRegEx().Count(_wordSearch.DiagonalsAsStrings().AsString());
 	}
 
-	private static string Solution2(string[] input) => NO_SOLUTION_WRITTEN_MESSAGE;
+	private static int Solution2() => _wordSearch.Walk().Count(_wordSearch.IsXmasCross);
 }
 
 file static class Day04Extensions
 {
+	public static bool IsXmasCross(this char[,] array, (int Col, int Row) point)
+	{
+		if (array[point.Col, point.Row] == A) {
+			char[,] subArray = array.SubArray(point.Col - 1, point.Row - 1, 3, 3);
+
+			if (MasRegEx().Count(subArray.DiagonalsLRAsStrings().AsString()) == 1 &&
+				MasRegEx().Count(subArray.DiagonalsRLAsStrings().AsString()) == 1) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static string AsString(this IEnumerable<string> strings) => string.Join(SPACE, strings);
+	public static IEnumerable<string> DiagonalsAsStrings(this char[,] array) =>
+		[.. array.DiagonalsLRAsStrings(), .. array.DiagonalsRLAsStrings()];
+
 	public static IEnumerable<string> DiagonalsLRAsStrings(this char[,] array)
 	{
 		StringBuilder stringBuilder = new();
@@ -100,10 +114,15 @@ file static class Day04Extensions
 
 internal sealed partial class Day04Types
 {
-	[GeneratedRegex("""(?={XMAS}|SAMX)""")]
+	[GeneratedRegex("""(?=XMAS|SAMX)""")]
 	public static partial Regex XmasRegEx();
+
+	[GeneratedRegex("""(MAS|SAM)""")]
+	public static partial Regex MasRegEx();
 }
 
 file static class Day04Constants
 {
+	public const char A = 'A';
+	public const char SPACE = ' ';
 }
--- Tests/2024/Tests_04.cs
+++ Tests/2024/Tests_04.cs
@@ -17,12 +17,39 @@ public class Tests_04_Ceres_Search
 		MAMMMXMMMM
 		MXMXAXMASX
 		""", 18)]
-	[InlineData("""
-		XMASAMX.MM
-		""", 2)]
+	[InlineData("""XMASAMX.MM""", 2)] // overlapping result
 	public void Part1(string input, int expected)
 	{
 		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+
+	[Theory]
+	[InlineData("""
+		.M.S......
+		..A..MSMS.
+		.M.S.MAA..
+		..A.ASMSM.
+		.M.S.M....
+		..........
+		S.S.S.S.S.
+		.A.A.A.A..
+		M.M.M.M.M.
+		..........
+		""", 9)]
+	[InlineData("""
+		M.S
+		.A.
+		M.S
+		""", 1)]
+	[InlineData("""
+		.M.
+		MAS
+		.S.
+		""", 0)]
+	public void Part2(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART2, input), out int actual);
 		actual.ShouldBe(expected);
 	}
 }
