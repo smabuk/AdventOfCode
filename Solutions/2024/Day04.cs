8950bce7d0df44922a2528f8d4ff68838a6fb33e
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-04 06:47:10 +0000

    2024 Day04 Part1 Solved

---
+++ Solutions/2024/Day04.cs
@@ -0,0 +1,109 @@
+﻿using System.Text;
+
+using static AdventOfCode.Solutions._2024.Day04Constants;
+using static AdventOfCode.Solutions._2024.Day04Types;
+namespace AdventOfCode.Solutions._2024;
+
+/// <summary>
+/// Day 04: Ceres Search
+/// https://adventofcode.com/2024/day/04
+/// </summary>
+[Description("Ceres Search")]
+public sealed partial class Day04 {
+
+	public static string Part1(string[] _) => Solution1().ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+
+	private static char[,] _wordSearch = default!;
+
+	[Init]
+	public static void LoadWordSearch(string[] input) => _wordSearch = input.To2dArray();
+
+	private static int Solution1()
+	{
+		int count = 0;
+		count += XmasRegEx().Count(string.Join(" ", _wordSearch.RowsAsStrings()));
+		count += XmasRegEx().Count(string.Join(" ", _wordSearch.ColsAsStrings()));
+		count += XmasRegEx().Count(string.Join(" ", _wordSearch.DiagonalsLRAsStrings()));
+		count += XmasRegEx().Count(string.Join(" ", _wordSearch.DiagonalsRLAsStrings()));
+
+		return count;
+	}
+
+	private static string Solution2(string[] input) => NO_SOLUTION_WRITTEN_MESSAGE;
+}
+
+file static class Day04Extensions
+{
+	public static IEnumerable<string> DiagonalsLRAsStrings(this char[,] array)
+	{
+		StringBuilder stringBuilder = new();
+
+		for (int col = array.ColsMax(); col >= 0; col--) {
+			for (int row = 0; row < array.RowsCount(); row++) {
+				if (array.TryGetValue(col + row, row, out char c)) {
+					_ = stringBuilder.Append(c);
+				} else {
+					break;
+				}
+			}
+
+			yield return stringBuilder.ToString();
+			_ = stringBuilder.Clear();
+		}
+
+		for (int row = 1; row < array.RowsCount(); row++) {
+			for (int col = 0; col < array.ColsCount(); col++) {
+				if (array.TryGetValue(col, row + col, out char c)) {
+					_ = stringBuilder.Append(c);
+				} else {
+					break;
+				}
+			}
+
+			yield return stringBuilder.ToString();
+			_ = stringBuilder.Clear();
+		}
+	}
+
+	public static IEnumerable<string> DiagonalsRLAsStrings(this char[,] array)
+	{
+		StringBuilder stringBuilder = new();
+
+		for (int col = 0; col < array.ColsCount(); col++) {
+			for (int row = 0; row < array.RowsCount(); row++) {
+				if (array.TryGetValue(col - row, row, out char c)) {
+					_ = stringBuilder.Append(c);
+				} else {
+					break;
+				}
+			}
+
+			yield return stringBuilder.ToString();
+			_ = stringBuilder.Clear();
+		}
+
+		for (int row = 1; row < array.RowsCount(); row++) {
+			for (int col = 0; col < array.ColsCount(); col++) {
+				if (array.TryGetValue(array.ColsMax() - col, row + col, out char c)) {
+					_ = stringBuilder.Append(c);
+				} else {
+					break;
+				}
+			}
+
+			yield return stringBuilder.ToString();
+			_ = stringBuilder.Clear();
+		}
+	}
+}
+
+internal sealed partial class Day04Types
+{
+	[GeneratedRegex("""(?={XMAS}|SAMX)""")]
+	public static partial Regex XmasRegEx();
+}
+
+file static class Day04Constants
+{
+}
---
+++ Tests/2024/Tests_04.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode.Tests.Year2024;
+
+public class Tests_04_Ceres_Search
+{
+	const int DAY = 04;
+
+	[Theory]
+	[InlineData("""
+		MMMSXXMASM
+		MSAMXMSMSA
+		AMXSXMAAMM
+		MSAMASMSMX
+		XMASAMXAMM
+		XXAMMXXAMA
+		SMSMSASXSS
+		SAXAMASAAA
+		MAMMMXMMMM
+		MXMXAXMASX
+		""", 18)]
+	[InlineData("""
+		XMASAMX.MM
+		""", 2)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
