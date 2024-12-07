5e4816197a3fde69953305199ff72726f413548e
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-07 05:43:39 +0000

    2024 Day07 Part1 Solved

---
+++ Solutions/2024/Day07.cs
@@ -0,0 +1,86 @@
+﻿using static AdventOfCode.Solutions._2024.Day07Constants;
+using static AdventOfCode.Solutions._2024.Day07Types;
+namespace AdventOfCode.Solutions._2024;
+
+/// <summary>
+/// Day 07: Bridge Repair
+/// https://adventofcode.com/2024/day/07
+/// </summary>
+[Description("Bridge Repair")]
+public static partial class Day07 {
+
+	public static string Part1(string[] input, params object[]? args) => Solution1(input).ToString();
+	public static string Part2(string[] input, params object[]? args) => Solution2(input).ToString();
+
+	private static List<Equation> _equations = [];
+
+	[Init]
+	public static void LoadEquations(string[] input) => _equations = [.. input.As<Equation>()];
+
+	private static long Solution1(string[] _)
+	{
+		var result = _equations
+			.Where(CouldPossibleBeTrue)
+			.Sum(e => e.Result);
+		return result;
+	}
+
+	private static string Solution2(string[] _) => NO_SOLUTION_WRITTEN_MESSAGE;
+
+	private static bool CouldPossibleBeTrue(this Equation equation)
+	{
+		int iterations = (int)Math.Pow(2, equation.Values.Count - 1);
+		for (int i = 0; i < iterations ; i++) {
+			string operators = Convert.ToString(i, 2).PadLeft(equation.Values.Count - 1, ZERO).Replace(ZERO, ADD).Replace(ONE, MUL);
+			long result = equation.Values[0];
+
+			for (int ix = 0; ix < operators.Length; ix++) {
+				result = operators[ix] switch
+				{
+					ADD => result + equation.Values[ix + 1],
+					MUL => result * equation.Values[ix + 1],
+					_ => throw new NotImplementedException(),
+				};
+			}
+
+			if (result == equation.Result) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private const char ADD = '+';
+	private const char MUL = '*';
+	
+	private const char ZERO = '0';
+	private const char ONE = '1';
+
+
+
+}
+
+internal sealed partial class Day07Types
+{
+
+	public sealed record Equation(long Result, List<int> Values) : IParsable<Equation>
+	{
+		public static Equation Parse(string s, IFormatProvider? provider)
+		{
+			string[] tokens = s.TrimmedSplit(':');
+			return new(tokens[0].As<long>(), [.. tokens[1].As<int>(' ')]);
+		}
+
+		public static Equation Parse(string s) => Parse(s, null);
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Equation result)
+			=> ISimpleParsable<Equation>.TryParse(s, provider, out result);
+	}
+
+	[GeneratedRegex("""(?<opts>opt1|opt2|opt3) (?<number>[\+\-]?\d+)""")]
+	public static partial Regex InputRegEx();
+}
+
+file static class Day07Constants
+{
+}
---
+++ Tests/2024/Tests_07.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode.Tests.Year2024;
+
+public class Tests_07_Bridge_Repair
+{
+	const int DAY = 07;
+
+	private const string TEST_DATA = """
+		190: 10 19
+		3267: 81 40 27
+		83: 17 5
+		156: 15 6
+		7290: 6 8 6 15
+		161011: 16 10 13
+		192: 17 8 14
+		21037: 9 7 18 13
+		292: 11 6 16 20
+		""";
+
+	[Theory]
+	[InlineData(TEST_DATA, 3749)]
+	[InlineData("190: 10 19", 190)]
+	[InlineData("3267: 81 40 27", 3267)]
+	[InlineData("292: 11 6 16 20", 292)]
+	[InlineData("161011: 16 10 13", 0)]
+	[InlineData("21037: 9 7 18 13", 0)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
