a2db72f4e4133504ce84eb778c697d383d0e6f85
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-08 06:03:57 +0000

    2024 Day08 Part1 Solved

---
+++ Solutions/2024/Day08.cs
@@ -0,0 +1,67 @@
+﻿using System.Reflection.Emit;
+
+namespace AdventOfCode.Solutions._2024;
+
+/// <summary>
+/// Day 08: Resonant Collinearity
+/// https://adventofcode.com/2024/day/08
+/// </summary>
+[Description("Resonant Collinearity")]
+public static partial class Day08 {
+
+	private static char[,] _map = default!;
+	private static ILookup<char, Point> _antennae = default!;
+
+	[Init]
+	public static void LoadMap(string[] input)
+	{
+		_map = input.To2dArray();
+		_antennae = _map
+			.ForEachCell()
+			.Where(c => char.IsAsciiLetterOrDigit(c))
+			.ToLookup(a => a.Value, a => a.Index);
+	}
+
+	public static int Part1(string[] _, Action<string[], bool>? visualise = null)
+	{
+		List<Point> antinodes = [.._antennae
+			.Select(a => _antennae[a.Key])
+			.Select(locations => locations.Antinodes())
+			.SelectMany(antinodes => antinodes)
+			.Distinct()
+			.Where(antinode => _map.IsInBounds(antinode))];
+
+		_map.VisualiseMap(antinodes, "Final", visualise);
+		return antinodes.Count;
+	}
+
+	public static string Part2(string[] _) => NO_SOLUTION_WRITTEN_MESSAGE;
+
+	private static IEnumerable<Point> Antinodes(this IEnumerable<Point> locations)
+	{
+		foreach (IEnumerable<Point> item in locations.Combinations(2)) {
+			Point a1 = item.First();
+			Point a2 = item.Last();
+			yield return a1 - a2 + a1;
+			yield return a2 - a1 + a2;
+		}
+	}
+
+
+	public static void VisualiseMap(this char[, ] map, IEnumerable<Point> antinodes, string title, Action<string[], bool>? visualise)
+	{
+		char[,] copy = (char[,])map.Clone();
+
+		foreach (Point antinode in antinodes) {
+			copy[antinode.X, antinode.Y] = ANTINODE;
+		}
+
+		if (visualise is not null) {
+			string[] output = ["", title, .. copy.PrintAsStringArray(0)];
+			_ = Task.Run(() => visualise?.Invoke(output, false));
+		}
+	}
+
+	private const char EMPTY    = '.';
+	private const char ANTINODE = '#';
+}
---
+++ Tests/2024/Tests_08.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode.Tests.Year2024;
+
+public class Tests_08_Resonant_Collinearity(ITestOutputHelper testOutputHelper)
+{
+	const int DAY = 08;
+
+	private const string TEST_DATA = """
+		............
+		........0...
+		.....0......
+		.......0....
+		....0.......
+		......A.....
+		............
+		............
+		........A...
+		.........A..
+		............
+		............
+		""";
+
+	[Theory]
+	[InlineData(TEST_DATA, 14)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input, new Action<string[], bool>(Callback)), out int actual);
+		actual.ShouldBe(expected);
+	}
+
+
+	private void Callback(string[] lines, bool _)
+	{
+		if (lines is null or []) {
+			return;
+		}
+
+		testOutputHelper.WriteLine(string.Join(Environment.NewLine, lines));
+	}
+}
