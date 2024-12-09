85a206bc0ae2a12ff459ae2d125cdcf19952ea3e
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-09 05:59:19 +0000

    2024 Day09 Part1 Solved (ugly - will fix later)

---
+++ Solutions/2024/Day09.cs
@@ -0,0 +1,71 @@
+﻿namespace AdventOfCode.Solutions._2024;
+
+/// <summary>
+/// Day 09: Disk Fragmenter
+/// https://adventofcode.com/2024/day/09
+/// </summary>
+[Description("Disk Fragmenter")]
+public partial class Day09 {
+
+	public static long Part1(string[] input)
+	{
+		string densediskMap = input[0];
+		List<int> diskMapAsInts = [.. densediskMap.AsDigits<int>()];
+		int blockCount = diskMapAsInts.Sum();
+
+		List<int> diskMap = new (blockCount);
+		for (int i = 0; i < blockCount; i++) {
+			diskMap.Add(EMPTY);
+		}
+
+		bool fileOrFreeSpace = FILE;
+		int diskMapPtr = 0;
+		int blockNo = 0;
+		foreach (int item in diskMapAsInts) {
+			if (fileOrFreeSpace is FILE) {
+				for (int i = 0; i < item; i++) {
+					diskMap[diskMapPtr++] = blockNo;
+				}
+				blockNo++;
+				fileOrFreeSpace = FREE_SPACE;
+			} else {
+				for (int i = 0; i < item; i++) {
+					diskMap[diskMapPtr++] = EMPTY;
+				}
+				fileOrFreeSpace = FILE;
+			}
+		}
+
+		// Defragment
+		for (int i = 0; i < blockCount; i++) {
+			if (diskMap[^(i+1)] is not EMPTY) {
+				int ptr = diskMap.IndexOf(EMPTY);
+				if (ptr >= blockCount - i) {
+					break;
+				}
+				diskMap[ptr] = diskMap[^(i+1)];
+				diskMap[^(i+1)] = EMPTY;
+			}
+		}
+
+		long checkSum = 0;
+		for (int i = 0; i < blockCount; i++) {
+			if (diskMap[i] is EMPTY) {
+				break;
+			}
+			checkSum += diskMap[i] * i;
+		}
+
+
+		return checkSum;
+	}
+
+	public static string Part2(string[] input) => NO_SOLUTION_WRITTEN_MESSAGE;
+
+
+	private sealed record Block(int id, int BlockSize);
+
+	private const bool FILE = true;
+	private const bool FREE_SPACE = false;
+	private const int EMPTY = int.MinValue;
+}
---
+++ Tests/2024/Tests_09.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode.Tests.Year2024;
+
+public class Tests_09_Disk_Fragmenter(ITestOutputHelper testOutputHelper)
+{
+	const int DAY = 09;
+
+	private const string TEST_DATA = """
+		2333133121414131402
+		""";
+
+	[Theory]
+	[InlineData(TEST_DATA, 1928)]
+	public async Task Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input, new Action<string[], bool>(Callback)), out int actual);
+		actual.ShouldBe(expected);
+		await Task.Delay(500);
+	}
+
+
+	private void Callback(string[] lines, bool _)
+	{
+		if (lines is null or []) {
+			return;
+		}
+
+		testOutputHelper.WriteLine(string.Join(Environment.NewLine, lines));
+	}
+}
