9eef6d146a8c04fa4d9f48c01933ef8663dd7fbb
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-10 13:25:18 +0000

    Update Smab.Helpers v1.7.19

--- Solutions/2024/Day10.cs
+++ Solutions/2024/Day10.cs
@@ -13,51 +13,54 @@ public static partial class Day10
 	[Init]
 	public static void FindAllTheTrails(string[] input)
 	{
-		int[,] lavaMap = input.Select(i => i.AsHeights()).ToList().To2dArray();
+		int[,] lavaMap = input.Select(i => i.AsHeights()).To2dArray();
 
 		_allRoutes = [..
 			lavaMap
 				.ForEachCell()
 				.Where(location => location.Value is TRAIL_HEAD)
-				.SelectMany(th => lavaMap
-					.HikeToSummit(new(TRAIL_HEAD, TRAIL_HEAD, th.Index, th.Index, [th.Index])))
-				];
+				.SelectMany(th => lavaMap.HikeToSummit(th.CreateTrailHead()))
+			];
 	}
 
-	public static int Part1(string[] _)
-	{
-		return _allRoutes
-				.Select(trail => (trail.StartPoint, trail.EndPoint))
-				.Distinct()
-				.Count();
-	}
+	public static int Part1(string[] _) => _allRoutes.SumOfRatings();
+	public static int Part2(string[] _) => _allRoutes.SumOfScores();
 
-	public static int Part2(string[] _) => _allRoutes.Count;
 
 	private static IEnumerable<Trail> HikeToSummit(this int[,] map, Trail trail)
 	{
 		foreach (Trail nextTrail in map.NextPositions(trail)) {
-			if (nextTrail.End is SUMMIT) {
+			if (nextTrail.ReachedTheSummit()) {
 				yield return nextTrail;
 				continue;
 			}
 
-			List<Trail> newTrails = [.. map.HikeToSummit(nextTrail)];
-			foreach (Trail newTrail in newTrails) {
-				if (newTrail.End is SUMMIT) {
-					yield return newTrail;
-				}
+			foreach (Trail completedTrail in map.HikeToSummit(nextTrail).ReachedTheSummit()) {
+				yield return completedTrail;
 			}
 		}
 	}
+
+
+
+	private static IEnumerable<int> AsHeights(this string s)
+		=> [.. s.Select(x => char.IsAsciiDigit(x) ? int.Parse($"{x}") : int.MinValue)];
+
+	private static Trail CreateTrailHead(this Cell<int> th)
+		=> new (TRAIL_HEAD, TRAIL_HEAD, th.Index, th.Index, [th.Index]);
 
 	private static IEnumerable<Trail> NextPositions(this int[,] map, Trail trail)
 		=> map
 			.GetAdjacentCells(trail.EndPoint)
 			.Where(next => next.Value == trail.End + 1)
 			.Select(next => trail with { EndPoint = next.Index, End = next.Value, Route = [.. trail.Route, next] });
+	
+	private static bool ReachedTheSummit(this Trail trail) => trail.End is SUMMIT;
+	private static IEnumerable<Trail> ReachedTheSummit(this IEnumerable<Trail> trails) => trails.Where(ReachedTheSummit);
 
-	private static List<int> AsHeights(this string s) => [.. s.Select(x => char.IsAsciiDigit(x) ? int.Parse($"{x}", null) : int.MinValue)];
+	private static int SumOfRatings(this List<Trail> trails) => trails.DistinctBy(t => (t.StartPoint, t.EndPoint)).Count();
+	private static int SumOfScores(this List<Trail> trails) => trails.Count;
+
 
 	public record Trail(int Start, int End, Point StartPoint, Point EndPoint, List<Point> Route);
 
