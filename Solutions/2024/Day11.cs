3ef4ee3f468525ed37773b7a966414d32a4aff74
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-11 07:43:42 +0000

    2024 Day11 refactored

--- Solutions/2024/Day11.cs
+++ Solutions/2024/Day11.cs
@@ -7,40 +7,23 @@
 [Description("Plutonian Pebbles")]
 public static partial class Day11 {
 
-	public static long Part1(string[] input, params object[]? args)
+	private static List<long> _initialStones = [];
+
+	
+	[Init]
+	public static void InitialStones(string[] input)
+		=> _initialStones = [ .. input[0].AsNumbers<long>(separator: ' ')];
+
+
+	public static long Part1(string[] _, params object[]? args)
 	{
-		int noOfBlinks = args.NoOfBlinksPart1();
-		List<long> initialStones = [ .. input[0].AsNumbers<long>(' ')];
+		LinkedList<long> stones = new(_initialStones);
 
-		LinkedList<long> stones = new(initialStones);
-
-		for (int i = 0; i < noOfBlinks; i++) {
+		for (int i = 0; i < args.NoOfBlinksPart1(); i++) {
 			stones = stones.Blink();
 		}
 
 		return stones.Count;
-	}
-
-	public static long Part2(string[] input, params object[]? args)
-	{
-		int noOfBlinks = args.NoOfBlinksPart2();
-		List<long> initialStones = [.. input[0].AsNumbers<long>(' ')];
-
-		//LinkedList<long> stones =new(initialStones);
-
-		//for (int i = 0; i < noOfBlinks; i++) {
-		//	//(stones, otherCount) = stones.Blink(otherCount);
-		//	//Console.WriteLine($"No of stones: {i, 2} => {stones.Count}");
-		//}
-		Dictionary<(long, long), long> cache = [];
-
-		long count = 0;
-
-		foreach (long stone in initialStones) {
-			count += stone.CountStones(cache, noOfBlinks);
-		}
-
-		return count;
 	}
 
 	private static LinkedList<long> Blink(this LinkedList<long> stones)
@@ -51,10 +34,7 @@ public static partial class Day11 {
 		while (current is not null) {
 			if (current.Value == 0) {
 				current.Value = 1;
-				newStones.Remove(current);
-
 			} else if (current.Value.TrySplit(out (long Left, long Right) values)) {
-				//(long left, long right) = current.Value.Split();
 				_ = newStones.AddBefore(current, values.Left);
 				current.Value = values.Right;
 			} else {
@@ -66,45 +46,53 @@ public static partial class Day11 {
 		return newStones;
 	}
 
-	private static long CountStones(this long stone, Dictionary<(long, long), long> cache, int blinks)
-	{
-		if (blinks == 0) {
-			return 1;
-		}
-
-		if (cache.TryGetValue((stone, blinks), out long cacheValue) ) {
-			return cacheValue;
-		}
-
-		long count = 0;
-
-		if (stone == 0) {
-			count = CountStones(1, cache, blinks - 1);
-		} else if (stone.TrySplit(out (long Left, long Right) values)) {
-			count += CountStones(values.Left, cache,  blinks - 1);
-			count += CountStones(values.Right, cache, blinks - 1);
-		} else {
-			count += CountStones(stone * 2024, cache, blinks - 1);
-		}
-
-		cache.Add((stone, blinks), count);
-
-		return count;
-	}
-
-	private static bool TrySplit(this long value, [NotNull] out (long Left, long Right) leftAndRight)
+	private static bool TrySplit(this long value, out (long Left, long Right) leftAndRight)
 	{
 		string valueString = value.ToString();
 		if (valueString.Length.IsOdd()) {
 			leftAndRight = (0, 0);
 			return false;
 		}
+
 		int length = valueString.Length / 2;
 		leftAndRight = (long.Parse(valueString[..length]), long.Parse(valueString[length..]));
+
 		return true;
 	}
 
 
+	public static long Part2(string[] _, params object[]? args)
+	{
+		Dictionary<(long, long), long> cache = [];
+
+		return _initialStones.Sum(stone => stone.CountStones(cache, args.NoOfBlinksPart2()));
+	}
+
+	private static long CountStones(this long stone, Dictionary<(long, long), long> cache, int blinksRemaining)
+	{
+		if (blinksRemaining == 0) {
+			return 1;
+		}
+
+		if (cache.TryGetValue((stone, blinksRemaining), out long cacheValue) ) {
+			return cacheValue;
+		}
+
+		long count = 0;
+
+		if (stone == 0) {
+			count = CountStones(1, cache, blinksRemaining - 1);
+		} else if (stone.TrySplit(out (long Left, long Right) values)) {
+			count += CountStones(values.Left, cache,  blinksRemaining - 1);
+			count += CountStones(values.Right, cache, blinksRemaining - 1);
+		} else {
+			count += CountStones(stone * 2024, cache, blinksRemaining - 1);
+		}
+
+		cache.Add((stone, blinksRemaining), count);
+
+		return count;
+	}
 
 	private static int NoOfBlinksPart1(this object[]? args) => GetArgument(args, 1, 25);
 	private static int NoOfBlinksPart2(this object[]? args) => GetArgument(args, 1, 75);
