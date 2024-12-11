3b2e7e089b340f2158a002943c78f49ab51e0664
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-11 14:49:34 +0000

    2024 Day11 building the pyramid of 10s and cleaning up some code

--- Solutions/2024/Day11.cs
+++ Solutions/2024/Day11.cs
@@ -36,8 +36,8 @@ public static partial class Day11 {
 	private static LinkedList<long> Blink(this LinkedList<long> stones)
 	{
 		LinkedList<long> newStones = new(stones);
-		LinkedListNode<long>? current = newStones.First;
 
+		LinkedListNode<long>? current = newStones.First;
 		while (current is not null) {
 			if (current.Value == 0) {
 				current.Value = 1;
@@ -61,19 +61,36 @@ public static partial class Day11 {
 			return false;
 		}
 
-		long divisor = (int)Math.Pow(10, length / 2);
+		//long divisor2 = (int)Math.Pow(10, length / 2);
+
+		// Maximum 19 digits in a long
+		long divisor = length switch
+		{
+			 2 =>         10L,
+			 4 =>        100L,
+			 6 =>       1000L,
+			 8 =>      10000L,
+			10 =>     100000L,
+			12 =>    1000000L,
+			14 =>   10000000L,
+			16 =>  100000000L,
+			18 => 1000000000L,
+			_ => throw new NotImplementedException(),
+		};
+
 		leftAndRight = (value / divisor, value % divisor);
 
 		return true;
 	}
 
-	private static long CountStones(this long stone, Dictionary<(long, long), long> cache, int blinksRemaining)
+	private static long CountStones(this long stone, Dictionary<CacheState, long> cache, int blinksRemaining)
 	{
 		if (blinksRemaining == 0) {
 			return 1;
 		}
 
-		if (cache.TryGetValue((stone, blinksRemaining), out long cacheValue) ) {
+		CacheState cacheState = new(stone, blinksRemaining);
+		if (cache.TryGetValue(cacheState, out long cacheValue) ) {
 			return cacheValue;
 		}
 
@@ -88,17 +105,19 @@ public static partial class Day11 {
 			count += CountStones(stone * 2024, cache, blinksRemaining - 1);
 		}
 
-		cache.Add((stone, blinksRemaining), count);
+		cache.Add(cacheState, count);
 
 		return count;
+
 	}
 
-
+	private record struct CacheState(long Stone, int BlinksRemaining);
 
 	private static string Method(this object[]? args) => GetArgument(args, 1, "count");
 
 	private static int NoOfBlinksPart1(this object[]? args)
 	{
+		// if method is specified it will be in the first argument so retrieve blinks from the 2nd
 		return args?.Length switch
 		{
 			>= 1 when args[0] is string => GetArgument(args, 2, 25),
