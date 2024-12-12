ec4b94aca63de2c542983c5b7b2d462c36bc8497
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-12 08:04:06 +0000

    2024 Day12 refactored

--- Solutions/2024/Day12.cs
+++ Solutions/2024/Day12.cs
@@ -8,25 +8,18 @@
 public static partial class Day12 {
 
 	private static char[,] _farm = default!;
+	private static List<Region> _regions = [];
 
 	[Init]
-	public static void LoadFarm(string[] input) => _farm = input.To2dArray();
-
-	public static int Part1(string[] _)
+	public static void LoadFarm(string[] input)
 	{
-		return
-			_farm
-			.FindRegions()
-			.Sum(region => region.RegionPrice(_farm));
+		_farm = input.To2dArray();
+		_regions = [.. _farm.FindRegions()];
 	}
 
-	public static int Part2(string[] input, params object[]? args)
-	{
-		return
-			_farm
-			.FindRegions()
-			.Sum(region => region.RegionBulkDiscountPrice(_farm));
-	}
+	public static int Part1(string[] _) => _regions.Sum(region => region.RegionPrice(_farm));
+	public static int Part2(string[] _) => _regions.Sum(region => region.RegionBulkDiscountPrice(_farm));
+
 
 	private static IEnumerable<Region> FindRegions(this char[,] farm)
 	{
@@ -67,79 +60,88 @@ public static partial class Day12 {
 
 	private static int RegionPerimeter(this Region region, char[,] farm)
 	{
-		int perimeter = 0;
+		int perimeterCount = 0;
 		foreach (Point plot in region.Plots) {
 			List<Cell<char>> adjacents = [.. farm.GetAdjacentCells(plot)];
-			perimeter += 4 - adjacents.Count;
+			perimeterCount += 4 - adjacents.Count;
 			foreach (Cell<char> adjacentPlot in adjacents) {
 				if (adjacentPlot.Value != region.Type) {
-					perimeter++;
+					perimeterCount++;
 				}
 			}
 		}
 
-		return perimeter;
+		return perimeterCount;
 	}
 
 	private static int RegionSidesCount(this Region region, char[,] farm)
 	{
-		HashSet<(Point, Direction)> edges = [];
+		HashSet<Edge> edges = [.. region.RegionEdges(farm)];
+
+		int sidesCount = 0;
+		while (edges.Count > 0) {
+			sidesCount++;
+
+			Edge edge = edges.First();
+			_ = edges.Remove(edge);
+
+			Direction checkDirection = edge.Direction switch {
+				Direction.North or Direction.South => Direction.East,
+				Direction.East  or Direction.West  => Direction.South,
+				_ => throw new NotImplementedException(),
+			};
+
+			bool keepGoing      = true;
+			bool keepGoingPlus  = true;
+			bool keepGoingMinus = true;
+
+			for (int n = 1; keepGoing; n++) {
+				keepGoing = false;
+				
+				Point delta = new(checkDirection.Delta());
+
+				Edge nextEdge = edge with { Plot = edge.Plot + (delta * n) };
+				if (keepGoingPlus && edges.Contains(nextEdge)) {
+					_ = edges.Remove(nextEdge);
+					keepGoing = true;
+				} else {
+					keepGoingPlus = false;
+				}
+
+				nextEdge = edge with { Plot = edge.Plot - (delta * n) };
+				if (keepGoingMinus && edges.Contains(nextEdge)) {
+					_ = edges.Remove(nextEdge);
+					keepGoing = true;
+				} else {
+					keepGoingMinus = false;
+				}
+			}
+		}
+
+		return sidesCount;
+	}
+
+	/// <summary>
+	/// Returns the outside edges of each individual plot
+	/// </summary>
+	/// <param name="region"></param>
+	/// <param name="farm"></param>
+	/// <returns></returns>
+	private static IEnumerable<Edge> RegionEdges(this Region region, char[,] farm)
+	{
 		foreach (Point plot in region.Plots) {
 			List<Cell<char>> adjacents = [.. farm.GetAdjacentCells(plot)];
 			foreach (Direction direction in Directions.AllDirections) {
 				Point next = plot + direction.Delta();
 				if (farm.TryGetValue(next.X, next.Y, out char value)) {
 					if (value != region.Type) {
-						_ = edges.Add((next, direction));
+						yield return new(next, direction);
 					}
 				} else {
-					_ = edges.Add((next, direction));
+					yield return new(next, direction);
 				}
 			}
 		}
-
-		int sides = 0;
-
-		while (edges.Count > 0) {
-			sides++;
-			(Point edge, Direction direction) = edges.First();
-			_ = edges.Remove((edge, direction));
-
-			Direction check = direction switch {
-				Direction.North => Direction.East,
-				Direction.South => Direction.West,
-				Direction.East => Direction.South,
-				Direction.West => Direction.North,
-				_ => throw new NotImplementedException(),
-			};
-
-			bool keepGoing = true;
-			bool keepGoingPlus = true;
-			bool keepGoingMinus = true;
-			int step = 1;
-			while (keepGoing) {
-				(int dX, int dY) delta = (check.Delta().dX * step, check.Delta().dY * step);
-				keepGoing = false;
-				if (keepGoingPlus && edges.Contains((edge + delta, direction))) {
-					_ = edges.Remove((edge + delta, direction));
-					keepGoing = true;
-				} else {
-					keepGoingPlus = false;
-				}
-
-				if (keepGoingMinus && edges.Contains((edge - delta, direction))) {
-					_ = edges.Remove((edge - delta, direction));
-					keepGoing = true;
-				} else {
-					keepGoingMinus = false;
-				}
-
-				step++;
-			}
-
-		}
-
-		return sides;
 	}
 
 	private static int RegionBulkDiscountPrice(this Region r, char[,] farm) => r.RegionArea() * r.RegionSidesCount(farm);
@@ -148,4 +150,5 @@ public static partial class Day12 {
 
 
 	private record Region(char Type, List<Point> Plots);
+	private record Edge(Point Plot, Direction Direction);
 }
