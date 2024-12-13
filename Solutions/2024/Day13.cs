9626a71ef255d406875b2d7470bdf6a1418c4d64
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-13 07:11:12 +0000

    2024 Day13 refactored

--- Solutions/2024/Day13.cs
+++ Solutions/2024/Day13.cs
@@ -1,4 +1,4 @@
-﻿using BigLocation = (long X, long Y);
+﻿using HugeLocation = (long X, long Y);
 
 namespace AdventOfCode.Solutions._2024;
 
@@ -9,73 +9,25 @@ namespace AdventOfCode.Solutions._2024;
 [Description("Claw Contraption")]
 public static partial class Day13 {
 
-	private static IEnumerable<ClawMachine> _ClawMachines = [];
+	private static IEnumerable<ClawMachine> _clawMachines = [];
 
 	[Init]
 	public static void LoadClawMachines(string[] input)
-		=> _ClawMachines = [.. input.Chunk(4).Select(i => ClawMachine.Parse(i))];
+		=> _clawMachines = [.. input.Chunk(4).Select(ClawMachine.Parse)];
 
-	public static int Part1(string[] _)
-	{
-		const int maxNoOfButtonPushes = 100;
+	public static long Part1(string[] _)
+		=> _clawMachines.Sum(claw => claw.TrySolveCost(out long cost) ? cost : 0);
 
-		int lowestCost = 0;
+	public static long Part2(string[] _)
+		=> _clawMachines.Sum(claw => claw.MakeHuge().TrySolveCost(out long cost) ? cost : 0);
 
-		foreach (ClawMachine clawMachine in _ClawMachines) {
-			if (clawMachine.TryCheapestCost(maxNoOfButtonPushes, out int cost)) {
-				lowestCost += cost;
-			}
-		}
 
-		return lowestCost;
-	}
 
-	internal static bool TryCheapestCost(this ClawMachine clawmachine, int noOfPresses, out int cost)
+	private static bool TrySolveCost(this ClawMachine clawmachine, out long cost)
 	{
 		const int buttonACost = 3;
-		const int buttonBCost = 1;
-
-		int lowestCost = int.MaxValue;
-		for (int b = 0; b < noOfPresses; b++) {
-			for (int a = 0; a < noOfPresses; a++) {
-				if ((a * clawmachine.A.DX) + (b * clawmachine.B.DX) == clawmachine.PrizeLocation.X
-					&& (a * clawmachine.A.DY) + (b * clawmachine.B.DY) == clawmachine.PrizeLocation.Y) {
-					lowestCost = int.Min(lowestCost, (a * buttonACost) + (b * buttonBCost));
-				}
-			}
-		}
-
-		if (lowestCost != int.MaxValue) {
-			cost = lowestCost;
-			return true;
-		}
-
+		
 		cost = 0;
-		return false;
-	}
-
-	public static long Part2(string[] _, params object[] args)
-	{
-		const long BIG_OFFSET = 10_000_000_000_000;
-
-		long lowestCost = 0;
-
-		foreach (ClawMachine clawMachine in _ClawMachines) {
-			ClawMachine bigClawMachine = clawMachine with
-				{ PrizeLocation = new(clawMachine.PrizeLocation.X + BIG_OFFSET, clawMachine.PrizeLocation.Y + BIG_OFFSET)};
-			if (args.Testing()) { bigClawMachine = clawMachine with { }; }
-
-			if (bigClawMachine.TryCheapestCostBig(out long cost)) {
-				lowestCost += cost;
-			}
-		}
-
-		return lowestCost;
-	}
-
-	internal static bool TryCheapestCostBig(this ClawMachine clawmachine, out long cost)
-	{
-		const int buttonACost = 3;
 
 		long ax = clawmachine.A.DX;
 		long bx = clawmachine.B.DX;
@@ -94,41 +46,43 @@ public static partial class Day13 {
 		long X = axby - aybx;
 		long P = pxby - pybx;
 
-
-		if (P % X == 0) {
-			long a = P / X;
-			if ((px - (ax * a)) % bx == 0) {
-				long b = (px - (ax * a)) / bx;
-				cost = (a * buttonACost) + b;
-				return true;
-			}
+		(long a, long remainder) = long.DivRem(P, X);
+		if (remainder is not 0) {
+			return false;
 		}
 
-		cost = 0;
-		return false;
+		(long b, remainder) = long.DivRem(px - (ax * a), bx);
+		if (remainder is not 0) {
+			return false;
+		}
+
+		cost = (a * buttonACost) + b;
+		return true;
 	}
 
-	public sealed record Button(string Name, int DX, int DY);
+	private static ClawMachine MakeHuge(this ClawMachine clawMachine)
+		=> clawMachine with
+		{ PrizeLocation = new(clawMachine.PrizeLocation.X + 10_000_000_000_000, clawMachine.PrizeLocation.Y + 10_000_000_000_000) };
 
-	public sealed record ClawMachine(Button A, Button B, BigLocation PrizeLocation)
+
+	private sealed record Button(int DX, int DY);
+
+	private sealed record ClawMachine(Button A, Button B, HugeLocation PrizeLocation)
 	{
 		public static ClawMachine Parse(IEnumerable<string> s)
 		{
 			string[] lines = [.. s];
 
-			int[] numbers = [..lines[0].Replace("Button A: X+", "").Replace("Y+", "").AsInts()];
-			Button A = new("A", numbers[0], numbers[1]);
+			int[] numbers = [.. lines[0][12..].Replace("Y+", "").AsInts()];
+			Button buttonA = new(numbers[0], numbers[1]);
 
-			numbers = [..lines[1].Replace("Button B: X+", "").Replace("Y+", "").AsInts()];
-			Button B = new("B", numbers[0], numbers[1]);
+			numbers = [..lines[1][12..].Replace("Y+", "").AsInts()];
+			Button buttonB = new(numbers[0], numbers[1]);
 
-			numbers = [..lines[2].Replace("Prize: X=", "").Replace("Y=", "").AsInts()];
+			numbers = [.. lines[2][9..].Replace("Y=", "").AsInts()];
 			Point prizeLocation = new(numbers[0], numbers[1]);
 
-			return new(A, B, prizeLocation);
+			return new(buttonA, buttonB, prizeLocation);
 		}
 	}
-
-	private static bool Testing(this object[]? args) => GetArgument(args, 1, false);
-
 }
--- Tests/2024/Tests_13.cs
+++ Tests/2024/Tests_13.cs
@@ -37,38 +37,4 @@ public class Tests_13_Claw_Contraption
 		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
 		actual.ShouldBe(expected);
 	}
-
-	[Theory]
-	[InlineData("""
-		Button A: X+94, Y+34
-		Button B: X+22, Y+67
-		Prize: X=8400, Y=5400
-		""", 280)]
-	[InlineData("""
-		Button A: X+17, Y+86
-		Button B: X+84, Y+37
-		Prize: X=7870, Y=6450
-		""", 200)]
-	[InlineData("""
-		Button A: X+94, Y+34
-		Button B: X+22, Y+67
-		Prize: X=8400, Y=5400
-
-		Button A: X+26, Y+66
-		Button B: X+67, Y+21
-		Prize: X=12748, Y=12176
-
-		Button A: X+17, Y+86
-		Button B: X+84, Y+37
-		Prize: X=7870, Y=6450
-
-		Button A: X+69, Y+23
-		Button B: X+27, Y+71
-		Prize: X=18641, Y=10279
-		""", 480)]
-	public void Part2(string input, int expected)
-	{
-		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART2, input, true), out int actual);
-		actual.ShouldBe(expected);
-	}
 }
