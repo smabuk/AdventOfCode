4bf52444f777a7bcf52998e1e1d069431117165a
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-19 05:22:34 +0000

    2024 Day19 Part1 Solved

---
+++ Solutions/2024/Day19.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode.Solutions._2024;
+
+/// <summary>
+/// Day 19: Linen Layout
+/// https://adventofcode.com/2024/day/19
+/// </summary>
+[Description("Linen Layout")]
+public static partial class Day19 {
+
+	private static List<string> _towelPatterns  = [];
+	private static List<string> _desiredDesigns = [];
+
+	[Init]
+	public static void LoadTowels(string[] input)
+	{
+		_towelPatterns  = [.. input[0].TrimmedSplit(',')];
+		_desiredDesigns = [.. input[2..]];
+	}
+
+	public static int Part1(string[] _)
+		=> _desiredDesigns
+		.Where(design => design.IsPossible(_towelPatterns))
+		.Count();
+
+	public static bool IsPossible(this string design, List<string> patterns)
+	{
+		if (design is "") {
+			return true;
+		}
+
+		foreach (string pattern in patterns.Where(design.StartsWith)) {
+			if (design[pattern.Length..].IsPossible(patterns)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static string Part2(string[] input, params object[]? args) => NO_SOLUTION_WRITTEN_MESSAGE;
+}
---
+++ Tests/2024/Tests_19.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode.Tests.Year2024;
+
+public class Tests_19_Linen_Layout
+{
+	const int DAY = 19;
+
+	[Theory]
+	[InlineData("""
+		r, wr, b, g, bwu, rb, gb, br
+
+		brwrr
+		bggr
+		gbbr
+		rrbgbr
+		ubwu
+		bwurrg
+		brgr
+		bbrgwb
+		""", 6)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
