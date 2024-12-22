b97ab7d653c76c0bde285b6308ac38bd488ec5e7
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-22 05:54:36 +0000

    2024 Day22 Part1 Solved

---
+++ Solutions/2024/Day22.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode.Solutions._2024;
+
+/// <summary>
+/// Day 22: Monkey Market
+/// https://adventofcode.com/2024/day/22
+/// </summary>
+[Description("Monkey Market")]
+public static partial class Day22 {
+
+	private static IEnumerable<MonkeyBuyer> _buyers = [];
+
+	[Init]
+	public static void LoadBuyers(string[] input) => _buyers = [.. input.As<MonkeyBuyer>()];
+
+	public static long Part1(string[] _, params object[]? args)
+	{
+		int iterations = args.Iterations();
+		return _buyers
+			.Select(b => b.SecretNumber)
+			.Select(sn => sn.GetSecretNumbers(iterations).Last())
+			.Sum();
+	}
+
+	public static string Part2(string[] input, params object[]? args) => NO_SOLUTION_WRITTEN_MESSAGE;
+
+
+	private static IEnumerable<long> GetSecretNumbers(this long secretNumber, int iterations)
+	{
+		long current = secretNumber;
+		for (int i = 0; i < iterations; i++) {
+			current = current.Mix(current * 64).Prune();
+			current = (current / 32).Mix(current).Prune();
+			current = (current * 2048).Mix(current).Prune();
+			yield return current;
+		}
+
+	}
+
+	private static long Mix(this long secretNumber, long value) => secretNumber ^ value;
+	private static long Prune(this long secretNumber) => secretNumber % 16777216;
+
+
+	public sealed record MonkeyBuyer(long SecretNumber) : IParsable<MonkeyBuyer>
+	{
+		public static MonkeyBuyer Parse(string s, IFormatProvider? provider) => new(long.Parse(s));
+		public static MonkeyBuyer Parse(string s) => Parse(s, null);
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out MonkeyBuyer result)
+			=> ISimpleParsable<MonkeyBuyer>.TryParse(s, provider, out result);
+	}
+
+	private static int Iterations(this object[]? args) => GetArgument(args, 1, 2000);
+}
---
+++ Tests/2024/Tests_22.cs
@@ -0,0 +1,38 @@
+﻿namespace AdventOfCode.Tests.Year2024;
+
+public class Tests_22_Monkey_Market(ITestOutputHelper testOutputHelper)
+{
+	const int DAY = 22;
+
+	[Theory]
+	[InlineData("""
+		1
+		10
+		100
+		2024
+		""", 2000, 37327623)]
+	[InlineData(   "1", 2000, 8685429)]
+	[InlineData(  "10", 2000, 4700978)]
+	[InlineData( "100", 2000, 15273692)]
+	[InlineData("2024", 2000, 8667524)]
+	[InlineData("123", 1, 15887950)]
+	[InlineData("123", 2, 16495136)]
+	[InlineData("123", 10, 5908254)]
+	public void Part1(string input, int iterations, long expected)
+	{
+		_ = long.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input, new Action<string[], bool>(Callback), iterations), out long actual);
+		actual.ShouldBe(expected);
+	}
+
+
+
+	private void Callback(string[] lines, bool _)
+	{
+		if (lines is null or []) {
+			return;
+		}
+
+		testOutputHelper.WriteLine(string.Join(Environment.NewLine, lines));
+	}
+
+}
