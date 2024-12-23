080112292f888f399d46cf9079a8a0729e05ba1d
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-23 07:06:05 +0000

    2024 Day23 Part1 Solved VERY SLOW

---
+++ Solutions/2024/Day23.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode.Solutions._2024;
+
+/// <summary>
+/// Day 23: LAN Party
+/// https://adventofcode.com/2024/day/23
+/// </summary>
+[Description("LAN Party")]
+public static partial class Day23 {
+
+	private static ILookup<Computer, Computer> _Lan = default!;
+
+	[Init]
+	public static void LoadComputers(string[] input)
+	{
+		List<(Computer, Computer)> pairs = [..input.Select(i => (new Computer(i[..2]), new Computer(i[3..])))];
+
+		_Lan = pairs.Concat(pairs.Select(p => (p.Item2, p.Item1))).ToLookup(p => p.Item1, p => p.Item2);
+	}
+
+	public static int Part1(string[] _, params object[]? args)
+	{
+		return _Lan
+			.Combinations(3)
+			.Count(combination => combination.Any(c => c.Key.Name.StartsWith("t")) && IsTriumvarate(combination));
+	}
+
+	private static bool IsTriumvarate(IEnumerable<IGrouping<Computer, Computer>> combination)
+	{
+		List<IGrouping<Computer, Computer>> computers = [.. combination];
+
+		return
+			   computers[0].Contains(computers[1].Key) && computers[0].Contains(computers[2].Key)
+			&& computers[1].Contains(computers[0].Key) && computers[1].Contains(computers[2].Key)
+			&& computers[2].Contains(computers[0].Key) && computers[2].Contains(computers[1].Key)
+			;
+	
+
+	}
+
+	public static string Part2(string[] input, params object[]? args) => NO_SOLUTION_WRITTEN_MESSAGE;
+
+
+
+
+	public sealed record Computer(string Name) : IParsable<Computer>
+	{
+		public static Computer Parse(string s, IFormatProvider? provider) => new(s);
+
+		public static Computer Parse(string s) => Parse(s, null);
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Computer result)
+			=> ISimpleParsable<Computer>.TryParse(s, provider, out result);
+	}
+}
---
+++ Tests/2024/Tests_23.cs
@@ -0,0 +1,47 @@
+﻿namespace AdventOfCode.Tests.Year2024;
+
+public class Tests_23_LAN_Party
+{
+	const int DAY = 23;
+
+	[Theory]
+	[InlineData("""
+		kh-tc
+		qp-kh
+		de-cg
+		ka-co
+		yn-aq
+		qp-ub
+		cg-tb
+		vc-aq
+		tb-ka
+		wh-tc
+		yn-cg
+		kh-ub
+		ta-co
+		de-co
+		tc-td
+		tb-wq
+		wh-td
+		ta-ka
+		td-qp
+		aq-cg
+		wq-ub
+		ub-vc
+		de-ta
+		wq-aq
+		wq-vc
+		wh-yn
+		ka-de
+		kh-ta
+		co-tc
+		wh-qp
+		tb-vc
+		td-yn
+		""", 7)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
