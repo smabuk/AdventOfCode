cf1fb53a3c5c6994c6eb3f8fa39cbe2be5b0be1e
smabuk <2011834+smabuk@users.noreply.github.com>
2025-11-21 12:46:39 +0000

    2024 Day25 Solved

---
+++ Solutions/2024/Day25.cs
@@ -0,0 +1,57 @@
+﻿namespace AdventOfCode.Solutions._2024;
+
+/// <summary>
+/// Day 25: Code Chronicle
+/// https://adventofcode.com/2024/day/25
+/// </summary>
+[Description("Code Chronicle")]
+public partial class Day25 {
+
+	public const char EMPTY = '.';
+	public const char FILLED = '#';
+
+	private static List<Schematic> _schematics = [];
+
+	[Init]
+	public static void LoadSchematics(string[] input) =>
+		_schematics = [
+			.. input
+				.Chunk(8)
+				.Select(chunk => string.Join(Environment.NewLine, chunk))
+				.As<Schematic>()
+			];
+
+	public static int Part1()
+	{
+		return _schematics
+			.OfType<LockSchematic>()
+			.SelectMany(aLock => _schematics
+				.OfType<KeySchematic>()
+				.Where(aKey => aLock.Heights.Zip(aKey.Heights).All(height => height.First + height.Second <= 5)))
+			.Count();
+	}
+
+	public static string Part2() => "⭐CONGRATULATIONS⭐";
+
+
+	private abstract record Schematic(int[] Heights) : IParsable<Schematic>
+	{
+		public static Schematic Parse(string s, IFormatProvider? provider)
+		{
+			char[,] schematic = s.TrimmedSplit().To2dArray();
+
+			bool isKey = schematic.Row(0).All(c => c == FILLED) || schematic.Row(6).All(c => c == EMPTY);
+
+			return isKey
+				? new  KeySchematic([.. Enumerable.Range(0, schematic.ColsCount()).Select(colIndex => schematic.Col(colIndex).Count(c => c == FILLED) - 1)])
+				: new LockSchematic([.. Enumerable.Range(0, schematic.ColsCount()).Select(colIndex => schematic.Col(colIndex).Count(c => c == FILLED) - 1)]);
+		}
+
+		public static Schematic Parse(string s) => Parse(s, null);
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Schematic result)
+			=> ISimpleParsable<Schematic>.TryParse(s, provider, out result);
+	}
+
+	private sealed record KeySchematic(int[] Heights) : Schematic(Heights);
+	private sealed record LockSchematic(int[] Heights) : Schematic(Heights);
+}
---
+++ Tests/2024/Tests_25.cs
@@ -0,0 +1,54 @@
+﻿namespace AdventOfCode.Tests.Year2024;
+
+public class Tests_25_Code_Chronicle
+{
+	const int DAY = 25;
+
+	[Theory]
+	[InlineData("""
+		#####
+		.####
+		.####
+		.####
+		.#.#.
+		.#...
+		.....
+
+		#####
+		##.##
+		.#.##
+		...##
+		...#.
+		...#.
+		.....
+
+		.....
+		#....
+		#....
+		#...#
+		#.#.#
+		#.###
+		#####
+
+		.....
+		.....
+		#.#..
+		###..
+		###.#
+		###.#
+		#####
+
+		.....
+		.....
+		.....
+		#....
+		#.#..
+		#.#.#
+		#####
+		""", 3)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
