c25a762d2375b40a2931771fc48495b7f8524c0e
smabuk <2011834+smabuk@users.noreply.github.com>
2025-12-02 15:51:27 +0000

    2025 Day02 Performance release

--- Solutions/2025/Day02.cs
+++ Solutions/2025/Day02.cs
@@ -11,39 +11,91 @@ public partial class Day02 {
 
 	[Init]
 	public static void LoadInstructions(string[] input)
-		=> _ranges = [.. input[0].Split(',').Select(ParseToLongRange)];
+		=> _ranges = [.. input[0].Split(',').Select(ParseToRange)];
+
+	private static LongRange ParseToRange(string s)
+	{
+		string[] parts = s.Split('-');
+		return new LongRange(parts[0].As<long>(), parts[1].As<long>());
+	}
 
 	public static long Part1()
 	{
 		return _ranges
 			.SelectMany(range => range.Values)
-			.Select(productId => ( ProductId: productId, IdString: productId.ToString(), productId.ToString().Length))
-			.Where(idInfo => idInfo.Length.IsEven())
-			.Where(idInfo => idInfo.IdString[0..(idInfo.Length / 2)] == idInfo.IdString[(idInfo.Length / 2)..])
-			.Sum(idInfo => idInfo.ProductId);
+			.Where(productId => int.IsEvenInteger(QuickLength(productId)))
+			.Where(productId => productId / Pow10[QuickLength(productId) / 2] == productId % Pow10[QuickLength(productId) / 2])
+			.Sum();
 	}
 
 	public static long Part2()
 	{
 		return _ranges
 			.SelectMany(range => range.Values)
-			.Where(productId => IsInvalidProductIdForPart2(productId.ToString()))
+			.Where(IsInvalidProductIdUsingLongs)
 			.Sum();
 	}
 
-	private static bool IsInvalidProductIdForPart2(string productId)
+	// Twice as fast as the string-based approach
+	private static bool IsInvalidProductIdUsingLongs(long productId)
 	{
-		return Enumerable.Range(1, productId.Length / 2)
-			.Where(multiple => productId.Length % multiple == 0)
-			.Where(multiple => productId[0] == productId[multiple])
-			.Any(multiple => {
-				string firstchunk = productId[0..multiple];
-				return productId.Chunk(multiple).Skip(1).All(chunk => chunk.SequenceEqual(firstchunk));
+		int length = QuickLength(productId);
+
+		return Enumerable.Range(1, length / 2)
+			.Where(patternLength => length % patternLength == 0)
+			// Quick first-digit check: extract first digit of productId and first digit at pattern position
+			.Where(patternLength => {
+				int firstDigitPosition = length - 1;
+				int patternDigitPosition = length - patternLength - 1;
+				long firstDigit = productId / Pow10[firstDigitPosition] % 10;
+				long patternDigit = productId / Pow10[patternDigitPosition] % 10;
+				return firstDigit == patternDigit;
+			})
+			.Any(patternLength => {
+				int repetitions = length / patternLength;
+				long divisor = Pow10[patternLength];
+				long firstPattern = productId / Pow10[length - patternLength];
+
+				return Enumerable.Range(1, repetitions - 1)
+					.All(rep => {
+						int position = length - ((rep + 1) * patternLength);
+						long currentPattern = productId / Pow10[position] % divisor;
+						return currentPattern == firstPattern;
+					});
 			});
 	}
 
-	private static LongRange ParseToLongRange(string s) {
-		string[] parts = s.Split('-');
-		return new LongRange(parts[0].As<long>(), parts[1].As<long>());
+	//private static bool IsInvalidProductIdUsingStrings(string productId)
+	//{
+	//	return Enumerable.Range(1, productId.Length / 2)
+	//		.Where(multiple => productId.Length % multiple == 0)
+	//		.Where(multiple => productId[0] == productId[multiple])
+	//		.Any(multiple => {
+	//			string firstchunk = productId[0..multiple];
+	//			return productId.Chunk(multiple).Skip(1).All(chunk => chunk.SequenceEqual(firstchunk));
+	//		});
+	//}
+
+	static int QuickLength(long value)
+	{
+		return value switch
+		{
+			< 10 => 1,
+			< 100 => 2,
+			< 1_000 => 3,
+			< 10_000 => 4,
+			< 100_000 => 5,
+			< 1_000_000 => 6,
+			< 10_000_000 => 7,
+			< 100_000_000 => 8,
+			< 1_000_000_000 => 9,
+			< 10_000_000_000 => 10,
+			_ => throw new ArgumentOutOfRangeException(nameof(value))
+		};
 	}
+
+	// Precomputed powers of 10 for speed (supports up to 10 digits)
+	private static readonly int[] Pow10 = [
+		1, 10, 100, 1_000, 10_000, 100_000,1_000_000, 10_000_000, 100_000_000, 1_000_000_000
+	];
 }
