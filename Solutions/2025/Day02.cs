7b77dbccc73e0729abde6ee13f7715d26dbbb30c
smabuk <2011834+smabuk@users.noreply.github.com>
2025-12-02 05:42:30 +0000

    2025 Day02 Part1 Solved

---
+++ Solutions/2025/Day02.cs
@@ -0,0 +1,60 @@
+﻿namespace AdventOfCode.Solutions._2025;
+
+/// <summary>
+/// Day 02: Gift Shop
+/// https://adventofcode.com/2025/day/02
+/// </summary>
+[Description("Gift Shop")]
+public partial class Day02 {
+
+	private static List<LongRange> _ranges = [];
+
+	[Init]
+	public static void LoadInstructions(string[] input)
+	{
+		_ranges = [.. input[0].TrimmedSplit(',').Select(s => ParseToLongRange(s))];
+	}
+
+	public static long Part1()
+	{
+		long sum = 0;
+
+		foreach (LongRange productIdRange in _ranges) {
+			for (long l = productIdRange.Start; l <= productIdRange.End; l++) {
+				if (IsInvalidProductId(l.ToString())) {
+					sum += l;
+				}
+			}
+		}
+
+		return sum;
+	}
+
+	public static string Part2() => NO_SOLUTION_WRITTEN_MESSAGE;
+
+
+	private static bool IsInvalidProductId(string productId)
+	{
+		int length = productId.Length;
+
+		if (length.IsOdd()) {
+			return false;
+		}
+
+		length /= 2;
+
+		for (int i = 0; i < length; i++) {
+			if (productId[i] != productId[i+length]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+
+	private static LongRange ParseToLongRange(string s) {
+		string[] parts = s.TrimmedSplit('-');
+		return new LongRange(parts[0].As<long>(), parts[1].As<long>());
+	}
+}
---
+++ Tests/2025/Tests_02.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode.Tests.Year2025;
+
+public class Tests_02_Gift_Shop(ITestOutputHelper testOutputHelper)
+{
+	const int DAY = 02;
+
+	private const string TEST_DATA = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+
+	[Theory]
+	[InlineData(TEST_DATA, 1227775554)]
+	public void Part1(string input, long expected)
+	{
+		_ = long.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input, new Action<string[], bool>(Callback)), out long actual);
+		actual.ShouldBe(expected);
+	}
+
+	private void Callback(string[] lines, bool _)
+	{
+		if (lines is null or []) {
+			return;
+		}
+
+		testOutputHelper.WriteLine(string.Join(Environment.NewLine, lines));
+	}
+
+}
