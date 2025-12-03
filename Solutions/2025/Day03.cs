78e584a22b4941357bb49236b4231a7eca541854
smabuk <2011834+smabuk@users.noreply.github.com>
2025-12-03 05:44:46 +0000

    2025 Day03 Part1 Solved

---
+++ Solutions/2025/Day03.cs
@@ -0,0 +1,49 @@
+﻿namespace AdventOfCode.Solutions._2025;
+
+/// <summary>
+/// Day 03: Lobby
+/// https://adventofcode.com/2025/day/03
+/// </summary>
+[Description("Lobby")]
+public partial class Day03 {
+
+	private static IEnumerable<Bank> _batteryBanks = [];
+
+	[Init]
+	public static void LoadBatteryBanks(string[] input) => _batteryBanks = [.. input.As<Bank>()];
+
+	public static int Part1() => _batteryBanks.Sum(bank => bank.Joltage);
+
+	public static string Part2() => NO_SOLUTION_WRITTEN_MESSAGE;
+
+
+	private sealed record Bank(List<Battery> Batteries) : IParsable<Bank>
+	{
+		public int Joltage {
+			get {
+				int highJoltage1 = Batteries.Take(Batteries.Count - 1).Max(b => b.Joltage);
+				int maxVoltage = int.MinValue;
+				for (int index = 0; index < Batteries.Count - 1; index++) {
+					if (Batteries[index].Joltage == highJoltage1) {
+						int highJoltage2 = Batteries.Skip(index + 1).Max(b => b.Joltage);
+						int voltage = (highJoltage1 * 10) + highJoltage2;
+						if (voltage > maxVoltage) {
+							maxVoltage = voltage;
+						}
+					}
+				}
+
+				return maxVoltage;
+			}
+		}
+
+		public static Bank Parse(string s, IFormatProvider? provider)
+			=> new([.. s.AsDigits<int>().Select(dig => new Battery(dig))]);
+
+		public static Bank Parse(string s) => Parse(s, null);
+		public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Bank result)
+			=> ISimpleParsable<Bank>.TryParse(s, provider, out result);
+	}
+
+	private record Battery(int Joltage);
+}
---
+++ Tests/2025/Tests_03.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode.Tests.Year2025;
+
+public class Tests_03_Lobby
+{
+	const int DAY = 03;
+
+	private const string TEST_DATA =
+		"""
+		987654321111111
+		811111111111119
+		234234234234278
+		818181911112111
+		""";
+
+
+	[Theory]
+	[InlineData("987654321111111", 98)]
+	[InlineData("811111111111119", 89)]
+	[InlineData("234234234234278", 78)]
+	[InlineData("818181911112111", 92)]
+	[InlineData(TEST_DATA, 357)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolutionRouter.SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
