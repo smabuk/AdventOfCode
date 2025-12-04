af493a94ab52161220dfea7cbfba552092400983
smabuk <2011834+smabuk@users.noreply.github.com>
2025-12-04 05:30:04 +0000

    2025 Day04 Part1 Solved

---
+++ Solutions/2025/Day04.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.Solutions._2025;
+
+/// <summary>
+/// Day 04: Printing Department
+/// https://adventofcode.com/2025/day/04
+/// </summary>
+[Description("Printing Department")]
+[GenerateVisualiser]
+public partial class Day04 {
+
+	[Init]
+	public static void LoadDiagram(string[] input)
+	{
+		_diagram = input.To2dArray();
+		VisualiseGrid(_diagram, "Initial");
+	}
+
+	private static char[,] _diagram = default!;
+
+	const char PAPER_ROLL = '@';
+
+	public static int Part1()
+		=> _diagram
+		.ForEachCell()
+		.Where(position =>
+			position.Value is PAPER_ROLL &&
+			position.Index.AllAdjacent().Count(cell =>
+				_diagram.IsInBounds(cell) && _diagram[cell.X, cell.Y] is PAPER_ROLL) < 4)
+		.Count();
+
+	public static string Part2() => NO_SOLUTION_WRITTEN_MESSAGE;
+
+}
---
+++ Tests/2025/Tests_04.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode.Tests.Year2025;
+
+[SupportTestOutput]
+public partial class Tests_04_Printing_Department
+{
+	const int DAY = 04;
+
+	private const string TEST_DATA =
+		"""
+		..@@.@@@@.
+		@@@.@.@.@@
+		@@@@@.@.@@
+		@.@@@@..@.
+		@@.@@@@.@@
+		.@@@@@@@.@
+		.@.@.@.@@@
+		@.@@@.@@@@
+		.@@@@@@@@.
+		@.@.@@@.@.
+		""";
+
+	[Theory]
+	[InlineData(TEST_DATA, 13)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
