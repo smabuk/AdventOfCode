204035ee4c889af5b897bbe7245f42c45f40ea1c
smabuk <2011834+smabuk@users.noreply.github.com>
2025-12-06 07:13:15 +0000

    2025 Day06 Solved

--- Solutions/2025/Day06.cs
+++ Solutions/2025/Day06.cs
@@ -5,35 +5,67 @@
 /// https://adventofcode.com/2025/day/06
 /// </summary>
 [Description("Trash Compactor")]
-[GenerateVisualiser]
-public partial class Day06 {
+public partial class Day06
+{
+	private const string OP_ADD = "+";
+	private const string OP_MUL = "*";
 
-	[Init]
-	public static void LoadInstructions(string[] input)
+	public static long Part1(string[] input)
 	{
-		numbers = input[..^1].AsNumbers<long>().To2dArray();
-		operators = [.. input[^1].TrimmedSplit()];
-	}
+		long[,] homeworkAsNumbers = input[..^1].AsNumbers<long>().To2dArray();
+		string[] operators = [.. input[^1].TrimmedSplit()];
 
-	private static long[,] numbers = default!;
-	private static string[] operators = [];
-
-	public static long Part1()
-	{
-		long sum = 0;
-
-		for (int i = 0; i < numbers.ColsCount(); i++) {
-			string op = operators[i];
-			sum += op switch
+		return Enumerable.Range(0, homeworkAsNumbers.ColsCount())
+			.Sum(i => operators[i] switch
 			{
-				"+" => numbers.Col(i).Sum(),
-				"*" => numbers.Col(i).Aggregate(1L, (long curr, long val) => curr * val),
+				OP_ADD => homeworkAsNumbers.Col(i).Sum(),
+				OP_MUL => homeworkAsNumbers.Col(i).Aggregate(1L, (curr, val) => curr * val),
 				_ => 0
-			};
-		}
-
-		return sum;
+			});
 	}
 
-	public static string Part2() => NO_SOLUTION_WRITTEN_MESSAGE;
+	public static long Part2(string[] input)
+	{
+		char[,] homeworkAsChars = input[..^1].To2dArray();
+		string[] operators = [.. input[^1].TrimmedSplit()];
+
+		List<int> problemStarts = [.. homeworkAsChars.FindProblemStarts()];
+
+		return problemStarts
+			.Take(problemStarts.Count - 1)
+			.Select((problemStart, i)
+				=> Enumerable.Sequence(problemStart, problemStarts[i + 1] - 2, 1)
+					.Aggregate(operators[i] == OP_MUL ? 1L : 0, (curr, idx) =>
+					{
+						long cephalapod_number = string.Join("", homeworkAsChars.Col(idx)).As<long>();
+						return operators[i] switch
+						{
+							OP_ADD => curr + cephalapod_number,
+							OP_MUL => curr * cephalapod_number,
+							_ => 0
+						};
+					}))
+			.Sum();
+	}
+}
+
+file static class Day06Helpers
+{
+	extension(char[,] homework)
+	{
+		public IEnumerable<int> FindProblemStarts()
+		{
+			const char EMPTY = ' ';
+			yield return 0;
+
+			int i = 0;
+			for (i = 0; i < homework.ColsCount(); i++) {
+				if (homework.Col(i).All(x => x is EMPTY)) {
+					yield return (i + 1);
+				}
+			}
+
+			yield return (i + 1);
+		}
+	}
 }
--- Tests/2025/Tests_06.cs
+++ Tests/2025/Tests_06.cs
@@ -1,14 +1,15 @@
-﻿namespace AdventOfCode.Tests.Year2025;
+﻿using static AdventOfCode.Solutions.SolutionRouter;
 
-[SupportTestOutput]
+namespace AdventOfCode.Tests.Year2025;
+
 public partial class Tests_06_Trash_Compactor
 {
 	const int DAY = 06;
 
 	private const string TEST_DATA =
 		"""
-		123 328  51 64
-		 45 64  387 23
+		123 328  51 64.
+		 45 64  387 23.
 		  6 98  215 314
 		*   +   *   +
 		""";
@@ -17,7 +18,17 @@ public partial class Tests_06_Trash_Compactor
 	[InlineData(TEST_DATA, 4277556)]
 	public void Part1(string input, int expected)
 	{
+		input = input.Replace('.', ' ');
 		_ = int.TryParse(SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+
+	[Theory]
+	[InlineData(TEST_DATA, 3263827)]
+	public void Part2(string input, int expected)
+	{
+		input = input.Replace('.', ' ');
+		_ = int.TryParse(SolveProblem(YEAR, DAY, PART2, input), out int actual);
 		actual.ShouldBe(expected);
 	}
 }
