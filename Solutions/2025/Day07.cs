75fb1653bfcef0dcd968bc6f145ab6fcd903c243
smabuk <2011834+smabuk@users.noreply.github.com>
2025-12-07 06:44:16 +0000

    2025 Day07 refactored

--- Solutions/2025/Day07.cs
+++ Solutions/2025/Day07.cs
@@ -8,14 +8,14 @@
 [GenerateVisualiser]
 public partial class Day07 {
 
-	private const char BEAM = '|';
-	private const char SPACE = '.';
+	private const char BEAM     = '|';
+	private const char SPACE    = '.';
 	private const char SPLITTER = '^';
-	private const char START = 'S';
-	private static readonly Point SPLIT_LEFT = new(-1, 1);
-	private static readonly Point SPLIT_RIGHT = new(1, 1);
-	private static readonly Point NEXT = new(0, 1);
-	private static readonly Point RIGHT = new(1, 0);
+	private const char START    = 'S';
+	private static readonly Point SPLIT_LEFT  = new(-1, 0);
+	private static readonly Point SPLIT_RIGHT = new( 1, 0);
+	private static readonly Point NEXT_ROW    = new( 0, 1);
+	private static readonly Point[] SPLIT_LEFT_AND_RIGHT = [SPLIT_LEFT, SPLIT_RIGHT];
 
 	public static int Part1(string[] input)
 	{
@@ -49,39 +49,38 @@ public partial class Day07 {
 
 	public static long Part2(string[] input)
 	{
-
 		Grid<char> diagram = input.To2dGrid();
 		Grid<long> timelineCounts = new(diagram.ColsCount, diagram.RowsCount);
 
 		Point tachyonStart = new(diagram.Row(0).Index().First(x => x.Item == START).Index, 1);
 		timelineCounts[tachyonStart] = 1;
 
-		for (int rowIdx = 1; rowIdx < diagram.RowsCount - 1; rowIdx++) {
-			for (Point current = new(0, rowIdx); current.Y < diagram.ColsCount; current += RIGHT) {
-
-				long currentTimelines = timelineCounts[current];
-				if (currentTimelines == 0) {
-					continue;
-				}
-
-				char nextCell = diagram[current + NEXT];
-
-				if (nextCell is SPLITTER) {
-					if (current.X > 0) {
-						timelineCounts[current + SPLIT_LEFT] += currentTimelines;
-					}
-
-					if (current.X < diagram.ColsCount - 1) {
-						timelineCounts[current + SPLIT_RIGHT] += currentTimelines;
-					}
-				} else if (nextCell is SPACE) {
-					timelineCounts[current + NEXT] += currentTimelines;
-				}
+		foreach (Cell<char> cell in diagram.ForEachCell().Where(cell => cell.Row > 0 && cell.Row < diagram.ColsCount)) {
+			long currentTimelines = timelineCounts[cell.Index];
+			if (currentTimelines == 0) {
+				continue;
 			}
+
+			Point nextCell = cell.Index + NEXT_ROW;
+
+			switch (diagram[nextCell]) {
+				case SPLITTER:
+					foreach (Point split in SPLIT_LEFT_AND_RIGHT) {
+						if (timelineCounts.IsInBounds(nextCell + split)) {
+							timelineCounts[nextCell + split] += currentTimelines;
+						}
+					}
+
+					break;
+				case SPACE:
+					timelineCounts[nextCell] += currentTimelines;
+					break;
+				default:
+					break;
+			}
+
 		}
 
-		long timelines = timelineCounts.Row(diagram.RowsCount - 1).Sum();
-
-		return timelines;
+		return timelineCounts.Row(diagram.RowsCount - 1).Sum();
 	}
 }
