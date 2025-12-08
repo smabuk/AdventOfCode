b1e518f8d1a022e7a1a41eef4d0f3a55572cec0a
smabuk <2011834+smabuk@users.noreply.github.com>
2025-12-08 13:07:35 +0000

    Update Smab.Helpers to version 1.10.5
    Updated the `Smab.Helpers` package in `Directory.Packages.props`
    from version 1.10.2 to 1.10.5. This update may include bug fixes,
    new features, or other improvements provided in the newer version.

--- Solutions/2025/Day08.cs
+++ Solutions/2025/Day08.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode.Solutions._2025;
+﻿using JunctionBox = Smab.Helpers.Point3d;
+
+namespace AdventOfCode.Solutions._2025;
 
 /// <summary>
 /// Day 08: Playground
@@ -11,27 +13,27 @@ public partial class Day08
 	[Init]
 	public static void LoadJunctionBoxes(string[] input)
 	{
-		_junctionBoxes = [.. input.Select(n => n.As<Point3d>())];
+		_junctionBoxes = [.. input.Select(n => n.As<JunctionBox>())];
 
 		_distances
 			= _junctionBoxes
 			.Combinations(2)
-			.ToDictionary(jbs => jbs.First().EuclideanDistance(jbs.Last()), jbs => (jbs.First(), jbs.Last()));
+			.ToDictionary(jbs => jbs[0].EuclideanDistance(jbs[1]), jbs => (jbs[0], jbs[1]));
 
 		_sortedDistances = [.. _distances.Keys.OrderBy(d => d)];
 	}
 
-	private static List<Point3d> _junctionBoxes = [];
-	private static Dictionary<double, (Point3d First, Point3d Second)> _distances = [];
+	private static List<JunctionBox> _junctionBoxes = [];
+	private static Dictionary<double, (JunctionBox First, JunctionBox Second)> _distances = [];
 	private static List<double> _sortedDistances = [];
 
 	public static long Part1(string[] _, object[]? args)
 	{
 		int noOfPairs = GetArgument(args, 1, 1000);
-		List<HashSet<Point3d>> circuits = [];
+		List<HashSet<JunctionBox>> circuits = [];
 
 		for (int i = 0; i < noOfPairs;) {
-			(Point3d first, Point3d second) = _distances[_sortedDistances[i++]];
+			(JunctionBox first, JunctionBox second) = _distances[_sortedDistances[i++]];
 			circuits = CalculateCircuits(circuits, first, second);
 		}
 
@@ -44,8 +46,8 @@ public partial class Day08
 
 	public static long Part2()
 	{
-		List<HashSet<Point3d>> circuits = [];
-		(Point3d First, Point3d Second) lastPair = default;
+		List<HashSet<JunctionBox>> circuits = [];
+		(JunctionBox First, JunctionBox Second) lastPair = default;
 
 		int i = 0;
 		while (!(circuits.Count == 1 && circuits[0].Count == _junctionBoxes.Count)) {
@@ -56,33 +58,29 @@ public partial class Day08
 		return lastPair.First.X * lastPair.Second.X;
 	}
 
-	private static List<HashSet<Point3d>> CalculateCircuits(List<HashSet<Point3d>> inputCircuits, Point3d first, Point3d second)
+	private static List<HashSet<JunctionBox>> CalculateCircuits(List<HashSet<JunctionBox>> inputCircuits, JunctionBox first, JunctionBox second)
 	{
 		const int NOT_FOUND = -1;
 
-		List<HashSet<Point3d>> circuits = [.. inputCircuits];
+		List<HashSet<JunctionBox>> circuits = [.. inputCircuits];
 
-		int firstCircuitIndex = NOT_FOUND;
-		int secondCircuitIndex = NOT_FOUND;
+		int firstCircuitIndex = circuits.FindIndex(c => c.Contains(first));
+		int secondCircuitIndex = circuits.FindIndex(c => c.Contains(second));
 
-		for (int i = 0; i < circuits.Count; i++) {
-			if (circuits[i].Contains(first)) {
-				firstCircuitIndex = i;
-			}
-			if (circuits[i].Contains(second)) {
-				secondCircuitIndex = i;
-			}
-		}
-
-		if (firstCircuitIndex == NOT_FOUND && secondCircuitIndex == NOT_FOUND) {        // Neither in any circuit - create new circuit
-			circuits.Add([first, second]);
-		} else if (firstCircuitIndex != NOT_FOUND && secondCircuitIndex == NOT_FOUND) { // Only first is in a circuit - add second to it
-			_ = circuits[firstCircuitIndex].Add(second);
-		} else if (firstCircuitIndex == NOT_FOUND && secondCircuitIndex != NOT_FOUND) { // Only second is in a circuit - add first to it
-			_ = circuits[secondCircuitIndex].Add(first);
-		} else if (firstCircuitIndex != secondCircuitIndex) {                           // Both in different circuits - merge them
-			circuits[firstCircuitIndex].UnionWith(circuits[secondCircuitIndex]);
-			circuits.RemoveAt(secondCircuitIndex);
+		switch (firstCircuitIndex, secondCircuitIndex) {
+			case (NOT_FOUND, NOT_FOUND):
+				circuits.Add([first, second]);
+				break;
+			case (NOT_FOUND, int secondIdx):
+				_ = circuits[secondIdx].Add(first);
+				break;
+			case (int firstIdx, NOT_FOUND):
+				_ = circuits[firstIdx].Add(second);
+				break;
+			case (int firstIdx, int secondIdx) when firstIdx != secondIdx:
+				circuits[firstIdx].UnionWith(circuits[secondIdx]);
+				circuits.RemoveAt(secondIdx);
+				break;
 		}
 
 		return circuits;
@@ -91,11 +89,11 @@ public partial class Day08
 
 file static partial class Day08Extensions
 {
-	extension(Point3d point)
+	extension(JunctionBox junctionBox)
 	{
-		public double EuclideanDistance(Point3d other)
+		public double EuclideanDistance(JunctionBox other)
 		{
-			double x1 = point.X, y1 = point.Y, z1 = point.Z;
+			double x1 = junctionBox.X, y1 = junctionBox.Y, z1 = junctionBox.Z;
 			double x2 = other.X, y2 = other.Y, z2 = other.Z;
 			return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
 		}
