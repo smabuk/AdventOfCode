3fa1a615da07e3e6077539afff34d178e224dd93
smabuk <2011834+smabuk@users.noreply.github.com>
2025-12-09 10:31:36 +0000

    2025 Day09 Part1 Solved

---
+++ Solutions/2025/Day09.cs
@@ -0,0 +1,42 @@
+﻿using static AdventOfCode.Solutions._2025.Day09;
+
+namespace AdventOfCode.Solutions._2025;
+
+/// <summary>
+/// Day 09: Movie Theater
+/// https://adventofcode.com/2025/day/09
+/// </summary>
+[Description("Movie Theater")]
+[GenerateVisualiser]
+public partial class Day09 {
+
+	[Init]
+	public static void LoadTiles(string[] input) => _redTiles = [.. input.As<Tile>()];
+	private static List<Tile> _redTiles = [];
+
+	public static long Part1() => _redTiles.Combinations(2).Max(tiles => tiles.Area());
+	public static string Part2() => NO_SOLUTION_WRITTEN_MESSAGE;
+
+	[GenerateIParsable]
+	internal sealed partial record Tile(Point Position)
+	{
+		public static Tile Parse(string s) => new(Point.Parse(s));
+	}
+}
+
+file static partial class Day09TileExtensions
+{
+	extension(Tile tile)
+	{
+		public long X => tile.Position.X;
+		public long Y => tile.Position.Y;
+
+		public long Area(Tile tile2)
+			=> (Math.Abs(tile.X - tile2.X) + 1) * (Math.Abs(tile.Y - tile2.Y) + 1);
+	}
+
+	extension(Tile[] tiles)
+	{
+		public long Area() => tiles[0].Area(tiles[1]);
+	}
+}
---
+++ Tests/2025/Tests_09.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode.Tests.Year2025;
+
+public partial class Tests_09_Movie_Theater
+{
+	const int DAY = 09;
+
+	private const string TEST_DATA =
+		"""
+		7,1
+		11,1
+		11,7
+		9,7
+		9,5
+		2,5
+		2,3
+		7,3
+		""";
+
+	[Theory]
+	[InlineData(TEST_DATA, 50)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+
+}
