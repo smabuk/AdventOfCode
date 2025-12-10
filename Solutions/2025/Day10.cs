74b0883460259bb6c1c3e3e753535f2b94f9d685
smabuk <2011834+smabuk@users.noreply.github.com>
2025-12-10 06:37:50 +0000

    2025 Day10 Part1 Solved

---
+++ Solutions/2025/Day10.cs
@@ -0,0 +1,82 @@
+﻿
+namespace AdventOfCode.Solutions._2025;
+
+/// <summary>
+/// Day 10: Factory
+/// https://adventofcode.com/2025/day/10
+/// </summary>
+[Description("Factory")]
+//[GenerateVisualiser]
+public partial class Day10
+{
+
+	[Init]
+	public static void LoadMachines(string[] input) => _machines = [.. input.Select(Machine.Parse)];
+	private static List<Machine> _machines = [];
+
+	public static int Part1()
+	{
+		int fewestTotalPresses = 0;
+		foreach (Machine machine in _machines) {
+			int presses = 0;
+			Machine newMachine = machine;
+
+			while (newMachine.IsNotStarted) {
+				presses++;
+
+				IEnumerable<Button[]> buttons = machine.Buttons.Permute(presses);
+				foreach (Button[] buttonCombo in buttons) {
+					newMachine = machine;
+					foreach (Button button in buttonCombo) {
+						newMachine = newMachine.PressButton(button);
+					}
+
+					if (newMachine.IsStarted) {
+						fewestTotalPresses += presses;
+						break;
+					}
+				}
+			}
+		}
+
+		return fewestTotalPresses;
+	}
+
+	public static string Part2() => NO_SOLUTION_WRITTEN_MESSAGE;
+
+	private sealed partial record Machine(string DesiredLightState, List<Button> Buttons, int[] Joltages)
+	{
+		public string CurrentLightState { get; init; } = new('.', DesiredLightState.Length);
+		public bool IsStarted => CurrentLightState == DesiredLightState;
+		public bool IsNotStarted => CurrentLightState != DesiredLightState;
+
+		public Machine PressButton(Button button)
+		{
+			char[] newLightState = CurrentLightState.ToCharArray();
+			foreach (int lightIndex in button.Lights) {
+				newLightState[lightIndex] = newLightState[lightIndex] == '.' ? '#' : '.';
+			}
+
+			return this with { CurrentLightState = new string(newLightState) };
+		}
+
+		public static Machine Parse(string s)
+		{
+			string[] parts = s.TrimmedSplit(['[', ']', '{', '}']);
+
+			return new Machine(
+				parts[0],
+				[.. parts[1].TrimmedSplit(' ').Select(Button.Parse)],
+				[.. parts[2].TrimmedSplit(',').Select(int.Parse)]
+			);
+		}
+
+		public override string ToString() => $"[{CurrentLightState}] ({string.Join(") (", Buttons)}) {{{string.Join(',', Joltages)}}}";
+	}
+
+	private sealed partial record Button(int[] Lights)
+	{
+		public static Button Parse(string s) => new([.. s.AsNumbers<int>()]);
+		public override string ToString() => $"({string.Join(',', Lights)})";
+	}
+}
---
+++ Tests/2025/Tests_10.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode.Tests.Year2025;
+
+[SupportTestOutput]
+public partial class Tests_10_Factory
+{
+	const int DAY = 10;
+
+	private const string TEST_DATA =
+		"""
+		[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+		[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+		[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+		""";
+
+	[Theory]
+	[InlineData(TEST_DATA, 7)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
