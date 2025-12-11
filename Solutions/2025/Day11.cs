b57c2122628aa72e1189bf8551428b837a355291
smabuk <2011834+smabuk@users.noreply.github.com>
2025-12-11 10:00:19 +0000

    2025 Day11 Part1 Solved

---
+++ Solutions/2025/Day11.cs
@@ -0,0 +1,66 @@
+﻿using static AdventOfCode.Solutions._2025.Day11;
+
+namespace AdventOfCode.Solutions._2025;
+
+/// <summary>
+/// Day 11: Reactor
+/// https://adventofcode.com/2025/day/11
+/// </summary>
+[Description("Reactor")]
+[GenerateVisualiser]
+public partial class Day11 {
+
+	[Init]
+	public static void LoadServerRack(string[] input)
+		=> _serverRack = input.Select(i => i.AsConnection()).ToDictionary();
+	private static Dictionary<Device, HashSet<Device>> _serverRack = [];
+
+	public static int Part1() => _serverRack.FindAllPaths(new("you"), new("out")).Count();
+
+	public static string Part2() => NO_SOLUTION_WRITTEN_MESSAGE;
+
+	[GenerateIParsable]
+	internal sealed partial record Device(string Name)
+	{
+		public static Device Parse(string s) => new(s);
+	}
+}
+
+file static partial class Day11Extensions
+{
+	extension(Dictionary<Device, HashSet<Device>> rack)
+	{
+		public IEnumerable<List<Device>> FindAllPaths(Device start, Device target)
+		{
+			List<List<Device>> paths = [];
+			Queue<List<Device>> queue = new();
+			queue.Enqueue([start]);
+			while (queue.Count > 0)
+			{
+				List<Device> path = queue.Dequeue();
+				Device lastDevice = path.Last();
+				if (lastDevice.Equals(target))
+				{
+					paths.Add(path);
+					continue;
+				}
+
+				foreach (Device? connectedTo in rack[lastDevice].Where(connectedDevice => path.DoesNotContain(connectedDevice))) {
+					List<Device> newPath = [.. path, connectedTo];
+					queue.Enqueue(newPath);
+				}
+			}
+
+			return paths;
+		}
+	}
+
+	extension(string s)
+	{
+		public KeyValuePair<Device, HashSet<Device>> AsConnection()
+		{
+			Device[] devices = [.. s.TrimmedSplit([':', ' ']).Select(Device.Parse)];
+			return new(devices[0], [.. devices[1..]]);
+		}
+	}
+}
---
+++ Tests/2025/Tests_11.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode.Tests.Year2025;
+
+[SupportTestOutput]
+public partial class Tests_11_Reactor
+{
+	const int DAY = 11;
+
+	private const string TEST_DATA =
+		"""
+		aaa: you hhh
+		you: bbb ccc
+		bbb: ddd eee
+		ccc: ddd eee fff
+		ddd: ggg
+		eee: out
+		fff: out
+		ggg: out
+		hhh: ccc fff iii
+		iii: out
+		""";
+
+	[Theory]
+	[InlineData(TEST_DATA, 5)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
