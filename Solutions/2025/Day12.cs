772faa0ef0a946fa380637bb4b2461d6b114cdd8
smabuk <2011834+smabuk@users.noreply.github.com>
2025-12-12 06:11:51 +0000

    2025 Day12 Part1 Parsing down

---
+++ Solutions/2025/Day12.cs
@@ -0,0 +1,57 @@
+﻿namespace AdventOfCode.Solutions._2025;
+
+/// <summary>
+/// Day 12: Christmas Tree Farm
+/// https://adventofcode.com/2025/day/12
+/// </summary>
+[Description("Christmas Tree Farm")]
+//[GenerateVisualiser]
+public partial class Day12 {
+
+	private const char EMPTY = '.';
+
+	[Init]
+	public static void LoadChristmasTreeFarm(string[] input)
+	{
+		int sectionStart = input.Index().First(line => line.Item.Contains('x')).Index;
+		_shapes = [.. input
+			.Take(sectionStart)
+			.SplitBy(string.IsNullOrWhiteSpace)
+			.Select(shapeLines => PresentShape.Parse(string.Join(Environment.NewLine, shapeLines)))];
+		_regions = [.. input.Skip(sectionStart).As<Region>()];
+	}
+
+	private static List<PresentShape> _shapes = [];
+	private static List<Region> _regions = [];
+
+	public static int Part1()
+	{
+		int count = _regions.Count;
+
+
+
+		return count;
+	}
+
+	public static string Part2() => NO_SOLUTION_WRITTEN_MESSAGE;
+
+	[GenerateIParsable]
+	private sealed partial record Region(int Width, int Length, int[] QuantitiesOfShapes)
+	{
+		public static Region Parse(string s)
+		{
+			string[] parts = s.TrimmedSplit([' ', 'x', ':']);
+			return new Region(int.Parse(parts[0]), int.Parse(parts[1]), [.. parts[2..].As<int>()]);
+		}
+	}
+
+	[GenerateIParsable] private sealed partial record PresentShape(int Index, Grid<char> Shape)
+	{
+		public static PresentShape Parse(string s)
+		{
+			string[] parts = s.TrimmedSplit(Environment.NewLine);
+			int index = int.Parse(parts[0][..^1]);
+			return new PresentShape(index, parts[1..].To2dGrid());
+		}
+	}
+}
---
+++ Tests/2025/Tests_12.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode.Tests.Year2025;
+
+[SupportTestOutput]
+public partial class Tests_12_Christmas_Tree_Farm
+{
+	const int DAY = 12;
+
+	private const string TEST_DATA =
+		"""
+		0:
+		###
+		##.
+		##.
+
+		1:
+		###
+		##.
+		.##
+
+		2:
+		.##
+		###
+		##.
+
+		3:
+		##.
+		###
+		##.
+
+		4:
+		###
+		#..
+		###
+
+		5:
+		###
+		.#.
+		###
+
+		4x4: 0 0 0 0 2 0
+		12x5: 1 0 1 0 2 2
+		12x5: 1 0 1 0 3 2
+		""";
+
+	[Theory]
+	[InlineData(TEST_DATA, 2)]
+	public void Part1(string input, int expected)
+	{
+		_ = int.TryParse(SolveProblem(YEAR, DAY, PART1, input), out int actual);
+		actual.ShouldBe(expected);
+	}
+}
